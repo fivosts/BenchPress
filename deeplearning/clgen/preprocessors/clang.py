c11834a4c8b1a6eab8c3092a586bf9b0664966fd
Chris Cummins <chrisc.101@gmail.com>
2018-05-23 23:52:53 +0000

    Add a clang module.
    [Exported from 29bd245b7de4d4737d16c59d78603d0ff95b5a74]

--- deeplearning/clgen/preprocessors/clang.py
+++ deeplearning/clgen/preprocessors/clang.py
@@ -1,63 +1,19 @@
-# Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-#
-# clgen is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# clgen is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with clgen.  If not, see <https://www.gnu.org/licenses/>.
-"""This file contains utility code for working with clang.
-
-This module does not expose any preprocessor functions for CLgen. It contains
-wrappers around Clang binaries, which preprocessor functions can use to
-implement specific behavior. See deeplearning.clgen.preprocessors.cxx.Compile()
-for an example.
-"""
-import json
-import re
+"""This file contains utility code for working with clang."""
 import subprocess
-import sys
-import tempfile
-import typing
+from typing import List
 
-from compilers.llvm import clang_format
-from compilers.llvm import llvm
+from absl import flags
+from absl import logging
+
+from config import getconfig
 from deeplearning.clgen import errors
-from labm8 import app
-from labm8 import bazelutil
 
-FLAGS = app.FLAGS
 
-_LLVM_REPO = 'llvm_mac' if sys.platform == 'darwin' else 'llvm_linux'
-# Path to clang binary.
-CLANG = bazelutil.DataPath(f'{_LLVM_REPO}/bin/clang')
-# The marker used to mark stdin from clang pre-processor output.
-CLANG_STDIN_MARKER = re.compile(r'# \d+ "<stdin>" 2')
-# Options to pass to clang-format.
-# See: http://clang.llvm.org/docs/ClangFormatStyleOptions.html
-CLANG_FORMAT_CONFIG = {
-    'BasedOnStyle': 'Google',
-    'ColumnLimit': 5000,
-    'IndentWidth': 2,
-    'AllowShortBlocksOnASingleLine': False,
-    'AllowShortCaseLabelsOnASingleLine': False,
-    'AllowShortFunctionsOnASingleLine': False,
-    'AllowShortLoopsOnASingleLine': False,
-    'AllowShortIfStatementsOnASingleLine': False,
-    'DerivePointerAlignment': False,
-    'PointerAlignment': 'Left',
-    'BreakAfterJavaFieldAnnotations': True,
-    'BreakBeforeInheritanceComma': False,
-    'BreakBeforeTernaryOperators': False,
-    'AlwaysBreakAfterReturnType': 'None',
-    'AlwaysBreakAfterDefinitionReturnType': 'None',
-}
+FLAGS = flags.FLAGS
+
+_config = getconfig.GetGlobalConfig()
+CLANG = _config.paths.cc
+CLANG_FORMAT = _config.paths.clang_format
 
 
 def StripPreprocessorLines(src: str) -> str:
@@ -70,9 +26,10 @@ def StripPreprocessorLines(src: str) -> str:
     The output with preprocessor output stripped.
   """
   lines = src.split('\n')
+  i = 0
   # Determine when the final included file ends.
   for i in range(len(lines) - 1, -1, -1):
-    if CLANG_STDIN_MARKER.match(lines[i]):
+    if lines[i] == '# 1 "<stdin>" 2':
       break
   else:
     return ''
@@ -80,9 +37,7 @@ def StripPreprocessorLines(src: str) -> str:
   return '\n'.join([line for line in lines[i:] if not line.startswith('#')])
 
 
-def Preprocess(src: str,
-               cflags: typing.List[str],
-               timeout_seconds: int = 60,
+def Preprocess(src: str, cflags: List[str], timeout_seconds: int = 60,
                strip_preprocessor_lines: bool = True):
   """Run input code through the compiler frontend to inline macros.
 
@@ -99,96 +54,21 @@ def Preprocess(src: str,
     The preprocessed code.
 
   Raises:
-    ClangException: In case of an error.
-    ClangTimeout: If clang does not complete before timeout_seconds.
+
   """
-  cmd = [
-      'timeout', '-s9',
-      str(timeout_seconds),
-      str(CLANG), '-E', '-c', '-', '-o', '-'
-  ] + cflags
-  app.Log(2, '$ %s', ' '.join(cmd))
-  process = subprocess.Popen(cmd,
-                             stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.PIPE,
-                             universal_newlines=True)
+  builtin_cflags = ['-E', '-c', '-', '-o', '-']
+  cmd = ['timeout', '-s9', str(timeout_seconds),
+         CLANG] + builtin_cflags + cflags
+  logging.debug('$ %s', ' '.join(cmd))
+  process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, universal_newlines=True)
   stdout, stderr = process.communicate(src)
   if process.returncode == 9:
     raise errors.ClangTimeout(
-        f'Clang preprocessor timed out after {timeout_seconds}s')
+      f'Clang preprocessor timed out after {timeout_seconds}s')
   elif process.returncode != 0:
     raise errors.ClangException(stderr)
   if strip_preprocessor_lines:
     return StripPreprocessorLines(stdout)
   else:
     return stdout
-
-
-def CompileLlvmBytecode(src: str,
-                        suffix: str,
-                        cflags: typing.List[str],
-                        timeout_seconds: int = 60) -> str:
-  """Compile input code into textual LLVM byte code.
-
-  Args:
-    src: The source code to compile.
-    suffix: The suffix to append to the source code temporary file. E.g. '.c'
-      for a C program.
-    cflags: A list of flags to be passed to clang.
-    timeout_seconds: The number of seconds to allow before killing clang.
-
-  Returns:
-    The textual LLVM byte code.
-
-  Raises:
-    ClangException: In case of an error.
-    ClangTimeout: If clang does not complete before timeout_seconds.
-  """
-  builtin_cflags = ['-S', '-emit-llvm', '-o', '-']
-  with tempfile.NamedTemporaryFile(
-      'w', prefix='phd_deeplearning_clgen_preprocessors_clang_',
-      suffix=suffix) as f:
-    f.write(src)
-    f.flush()
-    cmd = ['timeout', '-s9',
-           str(timeout_seconds),
-           str(CLANG), f.name] + builtin_cflags + cflags
-    app.Log(2, '$ %s', ' '.join(cmd))
-    process = subprocess.Popen(cmd,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE,
-                               universal_newlines=True)
-    stdout, stderr = process.communicate()
-  if process.returncode == 9:
-    raise errors.ClangTimeout(f'Clang timed out after {timeout_seconds}s')
-  elif process.returncode != 0:
-    raise errors.ClangException(stderr)
-  return stdout
-
-
-def ClangFormat(text: str, suffix: str, timeout_seconds: int = 60) -> str:
-  """Run clang-format on a source to enforce code style.
-
-  Args:
-    text: The source code to run through clang-format.
-    suffix: The suffix to append to the source code temporary file. E.g. '.c'
-      for a C program.
-    timeout_seconds: The number of seconds to allow clang-format to run for.
-
-  Returns:
-    The output of clang-format.
-
-  Raises:
-    ClangFormatException: In case of an error.
-    ClangTimeout: If clang-format does not complete before timeout_seconds.
-  """
-  try:
-    return clang_format.Exec(
-        text, suffix, ['-style={}'.format(json.dumps(CLANG_FORMAT_CONFIG))],
-        timeout_seconds)
-  except llvm.LlvmTimeout:
-    raise errors.ClangTimeout(
-        f'Clang-format timed out after {timeout_seconds}s')
-  except clang_format.ClangFormatException as e:
-    raise errors.ClangFormatException(str(e))
--- deeplearning/clgen/preprocessors/clang_test.py
+++ deeplearning/clgen/preprocessors/clang_test.py
@@ -1,28 +1,16 @@
-# Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-#
-# clgen is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# clgen is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with clgen.  If not, see <https://www.gnu.org/licenses/>.
 """Unit tests for //deeplearning/clgen/preprocessors/clang.py."""
 import subprocess
+import sys
 
 import pytest
+from absl import app
+from absl import flags
 
 from deeplearning.clgen import errors
 from deeplearning.clgen.preprocessors import clang
-from labm8 import app
-from labm8 import test
 
-FLAGS = app.FLAGS
+
+FLAGS = flags.FLAGS
 
 
 class MockProcess():
@@ -31,11 +19,8 @@ class MockProcess():
   def __init__(self, returncode):
     self.returncode = returncode
 
-  def communicate(self, *args):
+  def communicate(self, input):
     return '', ''
-
-
-# StripPreprocessorLines() tests.
 
 
 def test_StripPreprocessorLines_empty_input():
@@ -71,9 +56,6 @@ This will # not be stripped.
 This will # not be stripped."""
 
 
-# Preprocess() tests.
-
-
 def test_Preprocess_process_command(mocker):
   """Test the process comand which is run."""
   mock_Popen = mocker.patch('subprocess.Popen')
@@ -104,11 +86,6 @@ def test_Preprocess_ClangException(mocker):
     clang.Preprocess('', [])
 
 
-def test_Preprocess_empty_file():
-  """Test the preprocessor output with an empty file."""
-  assert clang.Preprocess('', []) == '\n'
-
-
 def test_Preprocess_simple_c_program():
   """Test that a simple C program is unchanged."""
   assert clang.Preprocess("""
@@ -120,8 +97,7 @@ int main(int argc, char** argv) { return 0; }
 
 def test_Preprocess_inlined_cflag():
   """Test pre-processing with a custom define in the command line."""
-  assert clang.Preprocess(
-      """
+  assert clang.Preprocess("""
 int main(MY_TYPE argc, char** argv) { return 0; }
 """, ['-DMY_TYPE=int']) == """
 int main(int argc, char** argv) { return 0; }
@@ -130,8 +106,7 @@ int main(int argc, char** argv) { return 0; }
 
 def test_Preprocess_inlined_define():
   """Test pre-processing with a #define in the source code."""
-  assert clang.Preprocess(
-      """
+  assert clang.Preprocess("""
 #define MY_TYPE int
 int main(MY_TYPE argc, char** argv) { return 0; }
 """, ['-DMY_TYPE=int']) == """
@@ -142,8 +117,7 @@ int main(int argc, char** argv) { return 0; }
 
 def test_Preprocess_undefined_data_type():
   """Test that an undefined data type does not cause an error."""
-  assert clang.Preprocess(
-      """
+  assert clang.Preprocess("""
 int main(MY_TYPE argc, char** argv) { return 0; }
 """, []) == """
 int main(MY_TYPE argc, char** argv) { return 0; }
@@ -152,8 +126,7 @@ int main(MY_TYPE argc, char** argv) { return 0; }
 
 def test_Preprocess_undefined_variable():
   """Test that an undefined variable does not cause an error."""
-  assert clang.Preprocess(
-      """
+  assert clang.Preprocess("""
 int main(int argc, char** argv) { return UNDEFINED_VARIABLE; }
 """, []) == """
 int main(int argc, char** argv) { return UNDEFINED_VARIABLE; }
@@ -162,8 +135,7 @@ int main(int argc, char** argv) { return UNDEFINED_VARIABLE; }
 
 def test_Preprocess_undefined_function():
   """Test that an undefined function does not cause an error."""
-  assert clang.Preprocess(
-      """
+  assert clang.Preprocess("""
 int main(int argc, char** argv) { return UNDEFINED_FUNCTION(0); }
 """, []) == """
 int main(int argc, char** argv) { return UNDEFINED_FUNCTION(0); }
@@ -173,166 +145,41 @@ int main(int argc, char** argv) { return UNDEFINED_FUNCTION(0); }
 def test_Preprocess_invalid_preprocessor_directive():
   """Test that an invalid preprocessor directive raises an error."""
   with pytest.raises(errors.ClangException) as e_info:
-    clang.Preprocess(
-        """
+    clang.Preprocess("""
 #this_is_not_a_valid_directive
 int main(int argc, char** argv) { return 0; }
 """, [])
   assert "invalid preprocessing directive" in str(e_info.value)
 
 
+def test_Preprocess_empty_file():
+  """Test the preprocessor output with an empty file."""
+  assert clang.Preprocess('', []) == '\n'
+
+
 def test_Preprocess_no_strip_processor_lines():
   """Test that stdin marker is preserved if no strip_preprocessor_lines."""
   assert '# 1 "<stdin>" 2' in clang.Preprocess("""
 int main(int argc, char** argv) { return 0; }
-""", [],
-                                               strip_preprocessor_lines=False)
+""", [], strip_preprocessor_lines=False)
 
 
+@pytest.mark.skip()
 def test_Preprocess_include_stdio_strip():
   """Test that an included file is stripped."""
-  out = clang.Preprocess(
-      """
+  assert clang.Preprocess("""
 #include <stdio.h>
 int main(int argc, char** argv) { return NULL; }
-""", [])
-  # NULL expands to either "((void *)0)" or "((void*)0)". Accept either.
-  assert out == """\
-int main(int argc, char** argv) { return ((void *)0); }
-""" or out == """\
-int main(int argc, char** argv) { return ((void*)0); }
+""", []) == """
+int main(int argc, char** argv) { return 0; }
 """
 
 
-# ClangFormat() tests.
-
-
-def test_ClangFormat_process_command(mocker):
-  """Test the clang-format comand which is run."""
-  mock_Popen = mocker.patch('subprocess.Popen')
-  mock_Popen.return_value = MockProcess(0)
-  clang.ClangFormat('', '.cpp')
-  subprocess.Popen.assert_called_once()
-  cmd = subprocess.Popen.call_args_list[0][0][0]
-  assert cmd[:3] == ['timeout', '-s9', '60']
-
-
-def test_ClangFormat_ClangTimeout(mocker):
-  """Test that ClangTimeout is raised if clang-format returns with SIGKILL."""
-  mock_Popen = mocker.patch('subprocess.Popen')
-  mock_Popen.return_value = MockProcess(9)
-  with pytest.raises(errors.ClangTimeout):
-    clang.ClangFormat('', '.cpp')
-  # ClangTimeout inherits from ClangException.
-  with pytest.raises(errors.ClangException):
-    clang.ClangFormat('', '.cpp')
-
-
-def test_ClangFormat_ClangException(mocker):
-  """Test that ClangException is raised if clang-format returns non-zero."""
-  mock_Popen = mocker.patch('subprocess.Popen')
-  mock_Popen.return_value = MockProcess(1)
-  with pytest.raises(errors.ClangFormatException):
-    clang.ClangFormat('', '.cpp')
-
-
-def test_ClangFormat_empty_file():
-  """Test the preprocessor output with an empty file."""
-  assert clang.ClangFormat('', '.cpp') == ''
-
-
-# CompileLlvmBytecode() tests.
-
-
-def test_CompileLlvmBytecode_command(mocker):
-  """Test the clang comand which is run."""
-  mock_Popen = mocker.patch('subprocess.Popen')
-  mock_Popen.return_value = MockProcess(0)
-  clang.CompileLlvmBytecode('', '.c', ['-foo'])
-  subprocess.Popen.assert_called_once()
-  cmd = subprocess.Popen.call_args_list[0][0][0]
-  assert cmd[:3] == ['timeout', '-s9', '60']
-  assert cmd[5:] == ['-S', '-emit-llvm', '-o', '-', '-foo']
-
-
-def test_CompileLlvmBytecode_ClangTimeout(mocker):
-  """Test that ClangTimeout is raised if clang returns with SIGKILL."""
-  mock_Popen = mocker.patch('subprocess.Popen')
-  mock_Popen.return_value = MockProcess(9)
-  with pytest.raises(errors.ClangTimeout):
-    clang.CompileLlvmBytecode('', '.c', [])
-  # ClangTimeout inherits from ClangException.
-  with pytest.raises(errors.ClangException):
-    clang.CompileLlvmBytecode('', '.c', [])
-
-
-def test_CompileLlvmBytecode_ClangException(mocker):
-  """Test that ClangException is raised if clang returns non-zero."""
-  mock_Popen = mocker.patch('subprocess.Popen')
-  mock_Popen.return_value = MockProcess(1)
-  with pytest.raises(errors.ClangException):
-    clang.CompileLlvmBytecode('', '.c', [])
-
-
-def test_CompileLlvmBytecode_empty_c_file():
-  """Test that bytecode is produced for an empty C program."""
-  bc = clang.CompileLlvmBytecode('', '.c', [])
-  assert 'source_filename =' in bc
-  assert 'target datalayout =' in bc
-  assert 'target triple =' in bc
-
-
-def test_CompileLlvmBytecode_small_c_program():
-  """Test that bytecode is produced for a small C program."""
-  bc = clang.CompileLlvmBytecode('int main() {}', '.c', [])
-  # The full bytecode output is too fragile to compare against, e.g. it contains
-  # the file path, target layout, exact LLVM build version, etc. Instead we just
-  # check for some expected lines.
-  assert 'source_filename =' in bc
-  assert "define i32 @main() #0 {" in bc
-  assert "ret i32 0" in bc
-
-
-def test_CompileLlvmBytecode_small_cl_program():
-  """Test that bytecode is produced for a small OpenCL program."""
-  bc = clang.CompileLlvmBytecode('kernel void A() {}', '.cl', [])
-  assert 'source_filename =' in bc
-  assert 'target datalayout =' in bc
-  assert 'target triple =' in bc
-
-
-def test_CompileLlvmBytecode_c_syntax_error():
-  """Test that a ClangException is raised for a C program with syntax error."""
-  with pytest.raises(errors.ClangException):
-    clang.CompileLlvmBytecode('int main@@()! ##', '.c', [])
-
-
-# Benchmarks.
-
-HELLO_WORLD_C = """
-#include <stdio.h>
-
-int main(int argc, char** argv) {
-  printf("Hello, world!\\n");
-  return 0;
-}
-"""
-
-
-def test_benchmark_Preprocess_c_hello_world(benchmark):
-  """Benchmark Preprocess for a "hello world" C program."""
-  benchmark(clang.Preprocess, HELLO_WORLD_C, [])
-
-
-def test_benchmark_ClangFormat_c_hello_world(benchmark):
-  """Benchmark ClangFormat for a "hello world" C program."""
-  benchmark(clang.ClangFormat, HELLO_WORLD_C, '.cpp')
-
-
-def test_benchmark_CompileLlvmBytecode_c_hello_world(benchmark):
-  """Benchmark CompileLlvmBytecode for a "hello world" C program."""
-  benchmark(clang.CompileLlvmBytecode, HELLO_WORLD_C, '.c', [])
+def main(argv):
+  """Main entry point."""
+  del argv
+  sys.exit(pytest.main([__file__, '-vv']))
 
 
 if __name__ == '__main__':
-  test.Main()
+  app.run(main)
