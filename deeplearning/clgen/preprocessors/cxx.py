fc99a472dc11a0be9be1bd58c61c1042150427ed
Chris Cummins <chrisc.101@gmail.com>
2018-05-24 01:52:31 +0000

    Work in progress on preprocess pipeline.
    [Exported from a00da11eb373fba567618cad83ddbe8d9a918fa9]

--- deeplearning/clgen/preprocessors/cxx.py
+++ deeplearning/clgen/preprocessors/cxx.py
@@ -1,123 +1,24 @@
-# Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-#
-# clgen is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# clgen is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with clgen.  If not, see <https://www.gnu.org/licenses/>.
-"""Preprocessor functions for C++."""
 import re
-import sys
 
-from compilers.llvm import clang as clanglib
-from compilers.llvm import llvm
-from deeplearning.clgen import errors
 from deeplearning.clgen.preprocessors import clang
-from deeplearning.clgen.preprocessors import normalizer
-from deeplearning.clgen.preprocessors import public
-from labm8 import app
-from labm8 import bazelutil
 
-FLAGS = app.FLAGS
-
-_UNAME = 'mac' if sys.platform == 'darwin' else 'linux'
-LIBCXX_HEADERS = bazelutil.DataPath(f'libcxx_{_UNAME}/include/c++/v1')
-CLANG_HEADERS = bazelutil.DataPath(f'libcxx_{_UNAME}/lib/clang/6.0.0/include')
 
 C_COMMENT_RE = re.compile(
-    r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
-    re.DOTALL | re.MULTILINE)
-
-# Flags to compile C++ files with. I've replicated the default search path,
-# but substituted the sandboxed header locations in place of the defaults.
-#   bazel-phd/bazel-out/*-py3-opt/bin/deeplearning/clgen/preprocessors/\
-#     cxx_test.runfiles/llvm_mac/bin/clang -xc++ -E - -v
-CLANG_ARGS = [
-    '-xc++', '-isystem',
-    str(LIBCXX_HEADERS), '-isystem', '/usr/local/include', '-isystem',
-    str(CLANG_HEADERS), '-isystem', '/usr/include', '-Wno-ignored-pragmas',
-    '-ferror-limit=1', '-Wno-implicit-function-declaration',
-    '-Wno-incompatible-library-redeclaration', '-Wno-macro-redefined',
-    '-Wno-unused-parameter', '-Wno-long-long', '-Wcovered-switch-default',
-    '-Wdelete-non-virtual-dtor', '-Wstring-conversion',
-    '-DLLVM_BUILD_GLOBAL_ISEL', '-D__STDC_CONSTANT_MACROS',
-    '-D__STDC_FORMAT_MACROS', '-D__STDC_LIMIT_MACROS',
-    '-D_LIBCPP_HAS_C_ATOMIC_IMP'
-]
+  r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+  re.DOTALL | re.MULTILINE)
 
 
-@public.clgen_preprocessor
-def ClangPreprocess(text: str) -> str:
-  try:
-    return clang.StripPreprocessorLines(clanglib.Preprocess(text, CLANG_ARGS))
-  except llvm.LlvmError as e:
-    raise errors.ClangException(str(e.stderr[:1024]))
+def ClangPreprocess(src: str) -> str:
+  return clang.Preprocess(src, ['-x', 'c++'])
 
 
-@public.clgen_preprocessor
-def Compile(text: str) -> str:
-  """A preprocessor which attempts to compile the given code.
-
-  Args:
-    text: Code to compile.
-
-  Returns:
-    The input code, unmodified.
-  """
-  clang.CompileLlvmBytecode(text, '.cpp', CLANG_ARGS)
-  return text
-
-
-@public.clgen_preprocessor
-def ClangFormat(text: str) -> str:
-  """Run clang-format on a source to enforce code style.
-
-  Args:
-    text: The source code to run through clang-format.
-
-  Returns:
-    The output of clang-format.
-
-  Raises:
-    ClangFormatException: In case of an error.
-    ClangTimeout: If clang-format does not complete before timeout_seconds.
-  """
-  return clang.ClangFormat(text, '.cpp')
-
-
-@public.clgen_preprocessor
-def NormalizeIdentifiers(text: str) -> str:
-  """Normalize identifiers in C++ source code.
-
-  Args:
-    text: The source code to rewrite.
-
-  Returns:
-    Source code with identifier names normalized.
-
-  Raises:
-    RewriterException: If rewriter found nothing to rewrite.
-    ClangTimeout: If rewriter fails to complete within timeout_seconds.
-  """
-  return normalizer.NormalizeIdentifiers(text, '.cpp', CLANG_ARGS)
-
-
-@public.clgen_preprocessor
 def StripComments(text: str) -> str:
   """Strip C/C++ style comments.
 
-  Written by @markus-jarderot https://stackoverflow.com/a/241506/1318051
+  written by @markus-jarderot https://stackoverflow.com/a/241506/1318051
   """
 
   def Replacer(match):
-    """Regex replacement callback."""
     s = match.group(0)
     if s.startswith('/'):
       return " "  # note: a space and not an empty string
--- deeplearning/clgen/preprocessors/cxx_test.py
+++ deeplearning/clgen/preprocessors/cxx_test.py
@@ -1,34 +1,14 @@
-# Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-#
-# clgen is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# clgen is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with clgen.  If not, see <https://www.gnu.org/licenses/>.
-"""Unit tests for :cxx_test.py."""
+"""Unit tests for ///cxx_test."""
+import sys
 
 import pytest
+from absl import app
+from absl import flags
 
-from deeplearning.clgen import errors
 from deeplearning.clgen.preprocessors import cxx
-from labm8 import app
-from labm8 import test
-
-FLAGS = app.FLAGS
-
-# ClangPreprocess() tests.
 
 
-def test_ClangPreprocess_empty_input():
-  """Test that ClangPreprocess accepts an empty input."""
-  assert cxx.ClangPreprocess('') == '\n'
+FLAGS = flags.FLAGS
 
 
 def test_ClangPreprocess_small_cxx_program():
@@ -48,197 +28,13 @@ int foo() { return foobar<int>(10); }
 """
 
 
-def test_ClangPreprocess_missing_include():
-  """Test that ClangPreprocessor error is raised on missing #include."""
-  with pytest.raises(errors.ClangException) as e_info:
-    cxx.ClangPreprocess('#include "my-missing-file.h"')
-  assert "'my-missing-file.h' file not found" in str(e_info.value)
-
-
-# Compile() tests.
-
-
-def test_Compile_empty_input():
-  """Test that Compile accepts an empty input."""
-  assert cxx.Compile('') == ''
-
-
-def test_Compile_small_cxx_program():
-  """Test Compile on a small C++ program."""
-  assert cxx.Compile("""
-#define FOO T
-template<typename FOO>
-FOO foobar(const T& a) {return a;}
-
-int foo() { return foobar<int>(10); }
-""") == """
-#define FOO T
-template<typename FOO>
-FOO foobar(const T& a) {return a;}
-
-int foo() { return foobar<int>(10); }
-"""
-
-
-def test_Compile_user_define():
-  """Test that Compile accepts a program with a custom #define."""
-  assert cxx.Compile("""
-#define FLOAT_T float
-int A(FLOAT_T* a) {}
-""") == """
-#define FLOAT_T float
-int A(FLOAT_T* a) {}
-"""
-
-
-def test_Compile_syntax_error():
-  """Test that Compile rejects a program with invalid syntax."""
-  with pytest.raises(errors.ClangException) as e_info:
-    cxx.Compile("int mainA2@@1!!!#")
-  assert 'error: ' in str(e_info.value)
-
-
-def test_Compile_undefined_variable():
-  """Test that Compile rejects a program with an undefined variable."""
-  with pytest.raises(errors.ClangException) as e_info:
-    cxx.Compile("""
-int main(int argc, char** argv) {
-  undefined_variable;
-}
-""")
-  assert 'use of undeclared identifier' in str(e_info.value)
-
-
-def test_Compile_undefined_function():
-  """Test that Compile rejects a program with an undefined function."""
-  with pytest.raises(errors.ClangException) as e_info:
-    cxx.Compile("""
-int main(int argc, char** argv) {
-  undefined_function(argc);
-}
-""")
-  assert 'use of undeclared identifier' in str(e_info.value)
-
-
-def test_Compile_cxx_header():
-  """Test that Compile accepts a program using a C++ header."""
-  src = """
-#include <cassert>
-int main(int argc, char** argv) { return 0; }
-"""
-  assert src == cxx.Compile(src)
-
-
-def test_Compile_c99_header():
-  """Test that Compile accepts a program using a C99 header."""
-  src = """
-#include <assert.h>
-int main(int argc, char** argv) { return 0; }
-"""
-  assert src == cxx.Compile(src)
-
-
-# ClangFormat() tests.
-
-
-def test_ClangFormat_simple_c_program():
-  """Test that a simple C program is unchanged."""
-  assert cxx.ClangFormat("""
-int main(int argc, char** argv) { return 0; }
-""") == """
-int main(int argc, char** argv) {
-  return 0;
-}
-"""
-
-
-def test_ClangFormat_pointer_alignment():
-  """Test that pointers are positioned left."""
-  assert cxx.ClangFormat("""
-int * A(int* a, int * b, int *c);
-""") == """
-int* A(int* a, int* b, int* c);
-"""
-
-
-def test_ClangFormat_undefined_data_type():
-  """Test that an undefined data type does not cause an error."""
-  assert cxx.ClangFormat("""
-int main(MY_TYPE argc, char** argv) { return 0; }
-""") == """
-int main(MY_TYPE argc, char** argv) {
-  return 0;
-}
-"""
-
-
-def test_ClangFormat_undefined_variable():
-  """Test that an undefined variable does not cause an error."""
-  assert cxx.ClangFormat("""
-int main(int argc, char** argv) { return UNDEFINED_VARIABLE; }
-""") == """
-int main(int argc, char** argv) {
-  return UNDEFINED_VARIABLE;
-}
-"""
-
-
-def test_ClangFormat_undefined_function():
-  """Test that an undefined function does not cause an error."""
-  assert cxx.ClangFormat("""
-int main(int argc, char** argv) { return UNDEFINED_FUNCTION(0); }
-""") == """
-int main(int argc, char** argv) {
-  return UNDEFINED_FUNCTION(0);
-}
-"""
-
-
-def test_ClangFormat_invalid_preprocessor_directive():
-  """Test that an invalid preprocessor directive does not raise an error."""
-  assert cxx.ClangFormat("""
-#this_is_not_a_valid_directive
-int main(int argc, char** argv) { return 0; }
-""") == """
-#this_is_not_a_valid_directive
-int main(int argc, char** argv) {
-  return 0;
-}
-"""
-
-
-# NormalizeIdentifiers() tests.
-
-
-def test_NormalizeIdentifiers_small_cxx_program():
-  """Test that rewriter performs as expected for a small C++ program."""
-  assert """
-#include <iostream>
-
-int A(int a, char** b) {
-  int c = 2 * a;
-  std::cout << c << ' args' << std::endl;
-}
-""" == cxx.NormalizeIdentifiers("""
-#include <iostream>
-
-int main(int argc, char** argv) {
-  int foo = 2 * argc;
-  std::cout << foo << ' args' << std::endl;
-}
-""")
-
-
-# StripComments() tests.
-
-
 def test_StripComments_empty_input():
   """Test StripComments on an empty input."""
   assert cxx.StripComments('') == ''
 
 
-def test_StripComments_only_comment():
-  """Test StripComments on an input containing only comments."""
+def test_StripComments_only_commend():
+  """Test StripComments on an empty input."""
   assert cxx.StripComments('// Just a comment') == ' '
   assert cxx.StripComments('/* Just a comment */') == ' '
 
@@ -260,32 +56,11 @@ int main(int argc, char** argv) {
 """
 
 
-# Benchmarks.
-
-HELLO_WORLD_CXX = """
-#include <iostream>
-
-int main(int argc, char** argv) {
-  std::cout << "Hello, world!" << std::endl;
-  return 0;
-}
-"""
-
-
-def test_benchmark_ClangPreprocess_hello_world(benchmark):
-  """Benchmark ClangPreprocess on a "hello world" C++ program."""
-  benchmark(cxx.ClangPreprocess, HELLO_WORLD_CXX)
-
-
-def test_benchmark_Compile_hello_world(benchmark):
-  """Benchmark Compile on a "hello world" C++ program."""
-  benchmark(cxx.Compile, HELLO_WORLD_CXX)
-
-
-def test_benchmark_StripComments_hello_world(benchmark):
-  """Benchmark StripComments on a "hello world" C++ program."""
-  benchmark(cxx.StripComments, HELLO_WORLD_CXX)
+def main(argv):
+  """Main entry point."""
+  del argv
+  sys.exit(pytest.main([__file__, '-v']))
 
 
 if __name__ == '__main__':
-  test.Main()
+  app.run(main)
