53ea047bdd17fc5e994c1214001faef4e8fabf04
Chris Cummins <chrisc.101@gmail.com>
2018-05-29 11:39:25 +0000

    Add training telemetry tests.
    [Exported from a62dacc352569182b79110d1cde1563316d5f5ee]

--- deeplearning/clgen/telemetry_test.py
+++ deeplearning/clgen/telemetry_test.py
@@ -1,38 +1,24 @@
-# Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-#
-# clgen is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# clgen is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with clgen.  If not, see <https://www.gnu.org/licenses/>.
 """Unit tests for //deeplearning/clgen/telemetry.py."""
 import pathlib
+import sys
 import tempfile
 
-from deeplearning.clgen import telemetry
-from labm8 import app
-from labm8 import test
+import pytest
+from absl import app
 
-FLAGS = app.FLAGS
+from deeplearning.clgen import telemetry
+
 
 # TrainingLogger tests.
-
 
 def test_TrainingLogger_create_file():
   """Test that EpochEndCallback() produces a file."""
   with tempfile.TemporaryDirectory() as d:
     logger = telemetry.TrainingLogger(pathlib.Path(d))
-    logger.KerasEpochBeginCallback(0, {})
-    logger.KerasEpochEndCallback(0, {'loss': 1})
+    logger.EpochBeginCallback(0, {})
+    logger.EpochEndCallback(0, {'loss': 1})
     # Note that one is added to the epoch number.
-    assert (pathlib.Path(d) / 'epoch_001_telemetry.pbtxt').is_file()
+    assert (pathlib.Path(d) / 'epoch_01_end.pbtxt').is_file()
 
 
 def test_TrainingLogger_EpochTelemetry():
@@ -40,13 +26,20 @@ def test_TrainingLogger_EpochTelemetry():
   with tempfile.TemporaryDirectory() as d:
     logger = telemetry.TrainingLogger(pathlib.Path(d))
     assert not logger.EpochTelemetry()
-    logger.KerasEpochBeginCallback(0, {})
-    logger.KerasEpochEndCallback(0, {'loss': 1})
+    logger.EpochBeginCallback(0, {})
+    logger.EpochEndCallback(0, {'loss': 1})
     assert len(logger.EpochTelemetry()) == 1
-    logger.KerasEpochBeginCallback(1, {})
-    logger.KerasEpochEndCallback(1, {'loss': 1})
+    logger.EpochBeginCallback(1, {})
+    logger.EpochEndCallback(1, {'loss': 1})
     assert len(logger.EpochTelemetry()) == 2
 
 
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError('Unrecognized command line flags.')
+  sys.exit(pytest.main([__file__, '-v']))
+
+
 if __name__ == '__main__':
-  test.Main()
+  app.run(main)
