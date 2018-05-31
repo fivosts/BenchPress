06e226f5f6a3d821608958c1cb08263a63c0b064
Chris Cummins <chrisc.101@gmail.com>
2018-05-31 11:28:34 +0000

    Begin dataset preprocessors.
    [Exported from a3a0a0a9854eb4ba7c47a7d99d7d30bdb9429306]

--- datasets/github/scrape_repos/preprocessors/public.py
+++ datasets/github/scrape_repos/preprocessors/public.py
@@ -1,28 +1,14 @@
-# Copyright 2018, 2019 Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """This file defines the decorator for marking a dataset preprocessor."""
 import pathlib
-import subprocess
 import typing
 
-from labm8 import app
-from labm8 import fs
+from absl import flags
 
-FLAGS = app.FLAGS
+
+FLAGS = flags.FLAGS
 
 # Type hint for a preprocessor function. See @clgen_preprocess for details.
-PreprocessorFunction = typing.Callable[[str], typing.List[str]]
+PreprocessorFunction = typing.Callable[[str], str]
 
 
 def dataset_preprocessor(func: PreprocessorFunction) -> PreprocessorFunction:
@@ -46,50 +32,17 @@ def dataset_preprocessor(func: PreprocessorFunction) -> PreprocessorFunction:
       'def func(text: str) -> str:'.
   """
   expected_type_hints = {
-      'import_root': pathlib.Path,
-      'file_relpath': str,
-      'all_file_relpaths': typing.List[str],
-      'text': str,
-      'return': typing.List[str],
+    'import_root': pathlib.Path,
+    'file_relpath': str,
+    'text': str,
+    'return': str,
   }
   if typing.get_type_hints(func) != expected_type_hints:
     return_type = expected_type_hints.pop('return').__name__
     expected_args = ', '.join(
         [f'{k}: {v.__name__}' for k, v in expected_type_hints.items()])
-    raise TypeError(f'Preprocessor {func.__name__} does not have signature '
-                    f'"def {func.__name__}({expected_args}) -> {return_type}".')
+    raise TypeError(
+        f'Preprocessor {func.__name__} does not have signature '
+        f'"def {func.__name__}({expected_args}) -> {return_type}".')
   func.__dict__['is_dataset_preprocessor'] = True
   return func
-
-
-def GetAllFilesRelativePaths(root_dir: pathlib.Path,
-                             follow_symlinks: bool = False) -> typing.List[str]:
-  """Get relative paths to all files in the root directory.
-
-  Follows symlinks.
-
-  Args:
-    root_dir: The directory to find files in.
-    follow_symlinks: If true, follow symlinks.
-
-  Returns:
-    A list of paths relative to the root directory.
-
-  Raises:
-    EmptyCorpusException: If the content files directory is empty.
-  """
-  with fs.chdir(root_dir):
-    cmd = ['find']
-    if follow_symlinks:
-      cmd.append('-L')
-    cmd += ['.', '-type', 'f']
-    try:
-      find_output = subprocess.check_output(cmd).decode('utf-8').strip()
-    except UnicodeDecodeError:
-      # Unicode error could happen with special characters in paths.
-      return []
-  if find_output:
-    # Strip the leading './' from paths.
-    return [x[2:] for x in find_output.split('\n')]
-  else:
-    return []
