d0c9a2b9be18c44b3724561fe297e46e68dc6533
Chris Cummins <chrisc.101@gmail.com>
2019-01-03 16:29:59 +0000

    Fix invalid file name.
    [Exported from dc1cb867bd8eade1248951df7fa5b220efb64e4d]

--- deeplearning/clgen/tests/data/tiny/config_test.py
+++ deeplearning/clgen/tests/data/tiny/config_test.py
@@ -1,27 +1,14 @@
-# Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-#
-# clgen is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# clgen is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with clgen.  If not, see <https://www.gnu.org/licenses/>.
 """Test that //deeplearning/clgen/tests/data/tiny/config.pbtxt is valid."""
+import sys
 import tempfile
+
+import pytest
+from absl import app
 
 from deeplearning.clgen import clgen
 from deeplearning.clgen.proto import clgen_pb2
 from labm8 import bazelutil
 from labm8 import pbutil
-from labm8 import test
-
-MODULE_UNDER_TEST = 'deeplearning.clgen'
 
 
 def test_config_is_valid():
@@ -33,11 +20,17 @@ def test_config_is_valid():
         clgen_pb2.Instance())
     # Change the working directory and corpus path to our bazel run dir.
     config.working_dir = d
-    config.model.corpus.local_directory = str(
-        bazelutil.DataPath(
-            'phd/deeplearning/clgen/tests/data/tiny/corpus.tar.bz2'))
+    config.model.corpus.local_directory = str(bazelutil.DataPath(
+        'phd/deeplearning/clgen/tests/data/tiny/corpus.tar.bz2'))
     clgen.Instance(config)
 
 
+def main(argv):
+  """Main entry point."""
+  if len(argv) > 1:
+    raise app.UsageError('Unrecognized command line flags.')
+  sys.exit(pytest.main([__file__, '-vv']))
+
+
 if __name__ == '__main__':
-  test.Main()
+  app.run(main)
