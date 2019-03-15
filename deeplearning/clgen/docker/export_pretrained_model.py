61c29727d19c00e2f4c3d0f2128dc4de16fca9fb
Chris Cummins <chrisc.101@gmail.com>
2019-03-15 17:20:20 +0000

    Add script to export docker image.
    [Exported from 7a6a0cf721515f122d48e424dc3b14587ddbdc04]

--- deeplearning/clgen/docker/export_pretrained_model.py
+++ deeplearning/clgen/docker/export_pretrained_model.py
@@ -2,9 +2,7 @@
 import pathlib
 import subprocess
 import tempfile
-import typing
 
-from research.cummins_2017_cgo import generative_model
 from deeplearning.clgen import clgen
 from labm8 import app
 from labm8 import fs
@@ -14,45 +12,33 @@ FLAGS = app.FLAGS
 
 app.DEFINE_string('export_instance_config', None,
                   'Path of CLgen instance proto to export.')
-app.DEFINE_string('docker_base_image', 'chriscummins/clgen:latest',
+app.DEFINE_string('docker_base_image', 'chriscummins/clgen:190315',
                   'The name of the base docker image to build using.')
-
-
-def ExportInstance(instance: clgen.Instance,
-                   export_dir: pathlib.Path,
-                   docker_base_image: typing.Optional[str] = None) -> None:
-  """Export a self-contained CLgen instance to a directory.
-
-  Args:
-    instance: The instance to export.
-    export_dir: The directory to export files to.
-    docker_base_image: The name of the base docker image for the generated
-      Dockerfile.
-  """
-  config = instance.ToProto()
-
-  instance.ExportPretrainedModel(export_dir / 'model')
-  config.pretrained_model = '/clgen/model'
-
-  config.working_dir = '/clgen'
-  pbutil.ToFile(config, export_dir / 'config.pbtxt')
-
-  fs.Write(
-      export_dir / 'Dockerfile', f"""
-FROM {docker_base_image}
-MAINTAINER Chris Cummins <chrisc.101@gmail.com>
-
-ADD . /clgen
-""".encode('utf-8'))
 
 
 def main():
   """Main entry point."""
-  instance = generative_model.CreateInstanceFromFlags()
+  config = clgen.ConfigFromFlags()
+  instance = clgen.Instance(config)
 
   with tempfile.TemporaryDirectory(prefix='deeplearning_clgen_docker_') as d:
-    ExportInstance(instance, pathlib.Path(d), FLAGS.docker_base_image)
-    subprocess.check_call(['docker', 'build', d])
+    export_dir = pathlib.Path(d)
+    config = instance.ToProto()
+
+    instance.ExportPretrainedModel(export_dir / 'model')
+    config.pretrained_model = '/clgen/model'
+
+    config.working_dir = '/clgen'
+    pbutil.ToFile(config, export_dir / 'config.pbtxt')
+
+    fs.Write(
+        export_dir / 'Dockerfile', f"""
+FROM {FLAGS.docker_base_image}
+MAINTAINER Chris Cummins <chrisc.101@gmail.com>
+
+ADD . /clgen
+""".encode('utf-8'))
+    subprocess.check_call(['docker', 'build', str(export_dir)])
 
 
 if __name__ == '__main__':
