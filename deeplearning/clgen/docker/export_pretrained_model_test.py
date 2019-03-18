375178896b9d3011f5add80e2a3625d346a0def3
Chris Cummins <chrisc.101@gmail.com>
2019-03-18 16:29:42 +0000

    Add test for instance export.
    [Exported from 5913668f7717a92c6ddff86c26df5bca03ae70fd]

--- deeplearning/clgen/docker/export_pretrained_model_test.py
+++ deeplearning/clgen/docker/export_pretrained_model_test.py
@@ -1,31 +1,28 @@
 """Unit tests for //deeplearning/clgen/docker:export_pretrained_model."""
-import pathlib
+
+import pytest
 
 from deeplearning.clgen import clgen
 from deeplearning.clgen.docker import export_pretrained_model
 from deeplearning.clgen.proto import clgen_pb2
-from labm8.py import app
-from labm8.py import test
+from labm8 import app
+from labm8 import test
 
 FLAGS = app.FLAGS
 
-pytest_plugins = ["deeplearning.clgen.tests.fixtures"]
 
-
-@test.Fixture(scope="function")
+@pytest.fixture(scope='function')
 def abc_instance(abc_instance_config: clgen_pb2.Instance):
   """Test fixture that yields an instance."""
   return clgen.Instance(abc_instance_config)
 
 
-def test_ExportInstance(abc_instance: clgen.Instance, tempdir: pathlib.Path):
-  export_pretrained_model.ExportInstance(
-    abc_instance, tempdir, "chriscummins/clgen:latest"
-  )
-  assert (tempdir / "Dockerfile").is_file()
-  assert (tempdir / "config.pbtxt").is_file()
-  assert (tempdir / "model").is_dir()
+def test_TODO(abc_instance: clgen.Instance):
+  """TODO: Short summary of test."""
+  export_pretrained_model.ExportInstance(abc_instance,
+                                         'chriscummins/clgen:latest')
+  assert True
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   test.Main()
