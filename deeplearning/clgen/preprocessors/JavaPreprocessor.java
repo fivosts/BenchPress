b36fe40e213b26da4cf959417bf4b30752d51ad7
Chris Cummins <chrisc.101@gmail.com>
2019-06-25 15:57:36 +0000

    Tiny tweaks.
    [Exported from 8587b528a4068cfbf2680e3d5ea58e52edc990b9]

--- deeplearning/clgen/preprocessors/JavaPreprocessor.java
+++ deeplearning/clgen/preprocessors/JavaPreprocessor.java
@@ -34,9 +34,14 @@ import org.eclipse.jface.text.Document;
 public final class JavaPreprocessor {
 
   // Configuration options.
+<<<<<<< HEAD:deeplearning/clgen/preprocessors/JavaPreprocessor.java
   private static final int MIN_CHAR_COUNT = 50;
   private static final int MIN_LINE_COUNT = 4;
   private static final int PREPROCESS_TIMEOUT_SECONDS = 60;
+=======
+  private static final int MIN_CHAR_COUNT = 100;
+  private static final int MIN_LINE_COUNT = 4;
+>>>>>>> 8587b528a... Tiny tweaks.:experimental/deeplearning/deepsmith/java_fuzz/JavaPreprocessor.java
   // End of configuration options.
 
   /** Construct a preprocessor. */
