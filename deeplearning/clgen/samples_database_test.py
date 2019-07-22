3b5faeb338170b2871be790ed75d37575dc1f650
Chris Cummins <chrisc.101@gmail.com>
2019-07-22 16:06:56 +0000

    Add a database for storing CLgen samples.
    [Exported from 11a3cdaad18a97b10300e789dd3f0dce2bcf21ad]

--- deeplearning/clgen/samples_database_test.py
+++ deeplearning/clgen/samples_database_test.py
@@ -1,26 +1,25 @@
 """Unit tests for //deeplearning/clgen:samples_database."""
 import pathlib
-
 import pytest
 
 from deeplearning.clgen import samples_database
 from deeplearning.clgen.proto import model_pb2
-from labm8.py import test
+from labm8 import test
 
 FLAGS = test.FLAGS
 
 
-@test.Fixture(scope="function")
+@pytest.fixture(scope='function')
 def db(tempdir: pathlib.Path) -> samples_database.SamplesDatabase:
-  yield samples_database.SamplesDatabase(f"sqlite:///{tempdir}/db")
+  yield samples_database.SamplesDatabase(f'sqlite:///{tempdir}/db')
 
 
 def test_SamplesDatabaseObserver_add_one(db: samples_database.SamplesDatabase):
   sample_proto = model_pb2.Sample(
-    text="Hello, observer",
-    num_tokens=10,
-    wall_time_ms=5,
-    sample_start_epoch_ms_utc=1000,
+      text='Hello, observer',
+      num_tokens=10,
+      wall_time_ms=5,
+      sample_start_epoch_ms_utc=1000,
   )
 
   with db.Observer() as obs:
@@ -31,5 +30,5 @@ def test_SamplesDatabaseObserver_add_one(db: samples_database.SamplesDatabase):
     assert s.query(samples_database.Sample).one().ToProto() == sample_proto
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   test.Main()
