dfd738e66fe9c734980ba2bcc1752c78ee4fec57
Chris Cummins <chrisc.101@gmail.com>
2019-08-06 15:25:36 +0000

    Replace double quotes with single.
    [Exported from 71f0f800d210ba7f78b5969c488f8135d5391719]

--- labm8/py/text.py
+++ labm8/py/text.py
@@ -79,12 +79,16 @@ def truncate(string, maxchar):
       TruncateError: In case of an error.
   """
   if maxchar < 4:
-    raise TruncateError("Maxchar must be > 3")
+    raise TruncateError('Maxchar must be > 3')
 
   if len(string) <= maxchar:
     return string
   else:
+<<<<<<< HEAD:labm8/py/text.py
     return string[: maxchar - 3] + "..."
+=======
+    return string[:maxchar - 3] + '...'
+>>>>>>> 71f0f800d... Replace double quotes with single.:labm8/text.py
 
 
 def levenshtein(s1, s2):
@@ -252,7 +256,7 @@ def AutoCompletePrefix(prefix: str, trie: nx.DiGraph) -> typing.Set[str]:
     KeyError: If the prefix is not found.
   """
   if not prefix:
-    raise ValueError("Prefix cannot be empty")
+    raise ValueError('Prefix cannot be empty')
 
   ret = set()
 
