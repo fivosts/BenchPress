61e9531f19310996d8be8735ab31ad5ce3e96191
Chris Cummins <chrisc.101@gmail.com>
2019-08-06 15:28:00 +0000

    Add trailing comma.
    [Exported from 14720263ba9738ca5b88876b0f529435fbec1184]

--- labm8/py/text.py
+++ labm8/py/text.py
@@ -313,8 +313,10 @@ def StripSingleLineComments(
 =======
 
 
-def StripSingleLineComments(string: str,
-                            start_comment_re: str = '(#|//)') -> str:
+def StripSingleLineComments(
+    string: str,
+    start_comment_re: str = '(#|//)',
+) -> str:
   """Strip line comments from a string.
 
   Args:
