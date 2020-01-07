84737e6f297e365a99ccabbeebd38b8096a8bd29
Chris Cummins <chrisc.101@gmail.com>
2020-01-07 15:21:51 +0000

    Add a //labm8/sh:app library.
    [Exported from c9c73adc9f840f7ed4b9009d495e290b4e438230]

--- labm8/sh/app.sh
+++ labm8/sh/app.sh
@@ -1,4 +1,4 @@
-# Copyright 2014-2020 Chris Cummins <chrisc.101@gmail.com>.
+# Copyright 2014-2019 Chris Cummins <chrisc.101@gmail.com>.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,22 +25,18 @@
 #       source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
 #       source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
 #       { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=
-#     # --- begin app init ---
+#     --- begin app init ---
 # =============================================================================
 
 # --- begin runfiles.bash initialization v2 ---
 # Copy-pasted from the Bazel Bash runfiles library v2.
 f=bazel_tools/tools/bash/runfiles/runfiles.bash
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null ||
-  source "$0.runfiles/$f" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
-  {
-    echo >&2 "ERROR: cannot find $f"
-    exit 1
-  }
-f=
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+   source "$0.runfiles/$f" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=
 # --- end runfiles.bash initialization v2 ---
 
 # Args:
