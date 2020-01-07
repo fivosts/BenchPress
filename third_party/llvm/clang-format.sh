a2828b356d59d3562c41617492735350c15a254b
Chris Cummins <chrisc.101@gmail.com>
2020-01-07 15:27:35 +0000

    Add LLVM wrapper scripts to //third_party/llvm.
    [Exported from d551645d46ba0cc6095b2ea345beeb142f4bbb58]

--- third_party/llvm/clang-format.sh
+++ third_party/llvm/clang-format.sh
@@ -9,24 +9,17 @@
 
 # --- begin labm8 init ---
 f=phd/labm8/sh/app.sh
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null ||
-  source "$0.runfiles/$f" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
-  {
-    echo >&2 "ERROR: cannot find $f"
-    exit 1
-  }
-f=
-# --- end app init ---
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+   source "$0.runfiles/$f" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=
+# --- begin app init ---
 
 set -e
 if [[ -n $(DataPath llvm_mac/bin/clang-format) ]]; then
-  $(DataPath llvm_mac/bin/clang-format) "$@"
+  $(DataPath llvm_mac/bin/clang-format) $@
 elif [[ -n $(DataPath llvm_linux/bin/clang-format) ]]; then
-  $(DataPath llvm_linux/bin/clang-format) "$@"
-else
-  echo "clang-format not found!" >&2
-  exit 1
+  $(DataPath llvm_linux/bin/clang-format) $@
 fi
--- third_party/llvm/clang.sh
+++ third_party/llvm/clang.sh
@@ -9,24 +9,17 @@
 
 # --- begin labm8 init ---
 f=phd/labm8/sh/app.sh
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null ||
-  source "$0.runfiles/$f" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
-  {
-    echo >&2 "ERROR: cannot find $f"
-    exit 1
-  }
-f=
-# --- end app init ---
+source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null || \
+   source "$0.runfiles/$f" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+   source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null || \
+   { echo>&2 "ERROR: cannot find $f"; exit 1; }; f=
+# --- begin app init ---
 
 set -e
 if [[ -n $(DataPath llvm_mac/bin/clang) ]]; then
-  $(DataPath llvm_mac/bin/clang) "$@"
+  $(DataPath llvm_mac/bin/clang) $@
 elif [[ -n $(DataPath llvm_linux/bin/clang) ]]; then
-  $(DataPath llvm_linux/bin/clang) "$@"
-else
-  echo "clang not found!" >&2
-  exit 1
+  $(DataPath llvm_linux/bin/clang) $@
 fi
