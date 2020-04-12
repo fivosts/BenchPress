d026228baac07bc17ae37f686821e273e5c70b54
fivosts <fivos_ts@hotmail.com>
2020-04-12 03:41:06 +0100

    #1: Refactor Helper constructor, initialize, comment out next inputs for default behavior

--- deeplearning/clgen/models/helper.py
+++ deeplearning/clgen/models/helper.py
@@ -11,18 +11,22 @@ class CustomInferenceHelper(tfa.seq2seq.sampler.TrainingSampler):
   """An inference helper that takes a seed text"""
 
   def __init__(
-    self, input_seed, sequence_length, seed_length, embedding, temperature
+    self, seed_length, embedding, temperature
   ):
     super(CustomInferenceHelper, self).__init__(
-      inputs=input_seed, sequence_length=sequence_length, time_major=False
+      # inputs=input_seed, sequence_length=sequence_length, 
+      time_major=False
     )
 
     self._xlate = embedding
     self._seed_length = seed_length
     self._softmax_temperature = temperature
 
-  def initialize(self, name=None):
-    return super(CustomInferenceHelper, self).initialize(name=name)
+  def initialize(self, input_seed, sequence_length, name=None):
+    return super(CustomInferenceHelper, self).initialize(inputs = input_seed,
+                                                         sequence_length = sequence_length,
+                                                         mask = None
+                                                         )
 
   def sample(self, time, outputs, state, name=None):
     if self._softmax_temperature is not None:
@@ -32,26 +36,26 @@ class CustomInferenceHelper(tfa.seq2seq.sampler.TrainingSampler):
     sample_ids = sampler.sample()
     return sample_ids
 
-  def next_inputs(self, time, outputs, state, sample_ids, name=None):
-    with tf.name_scope(name, "CIHNextInputs", [time, outputs, state]):
-      next_time = time + 1
-      finished = next_time >= self._sequence_length
-      all_finished = math_ops.reduce_all(finished)
-      seed_done = next_time >= self._seed_length
+  # def next_inputs(self, time, outputs, state, sample_ids, name = "CIHNextInputs"):
+  #   with tf.name_scope(name, "CIHNextInputs", [time, outputs, state]):
+  #     next_time = time + 1
+  #     finished = next_time >= self._sequence_length
+  #     all_finished = math_ops.reduce_all(finished)
+  #     seed_done = next_time >= self._seed_length
 
-      def read_from_ta(inp):
-        return inp.read(next_time)
+  #     def read_from_ta(inp):
+  #       return inp.read(next_time)
 
-      next_inputs = tf.case(
-        [
-          (all_finished, lambda: self._zero_inputs),
-          (
-            tf.logical_not(seed_done),
-            lambda: nest.map_structure(read_from_ta, self._input_tas),
-          ),
-        ],
-        default=lambda: tf.stop_gradient(
-          tf.nn.embedding_lookup(self._xlate, sample_ids)
-        ),
-      )
-      return (finished, next_inputs, state)
+  #     next_inputs = tf.case(
+  #       [
+  #         (all_finished, lambda: self._zero_inputs),
+  #         (
+  #           tf.logical_not(seed_done),
+  #           lambda: nest.map_structure(read_from_ta, self._input_tas),
+  #         ),
+  #       ],
+  #       default=lambda: tf.stop_gradient(
+  #         tf.nn.embedding_lookup(self._xlate, sample_ids)
+  #       ),
+  #     )
+  #     return (finished, next_inputs, state)
