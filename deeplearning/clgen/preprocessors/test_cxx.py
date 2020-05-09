bc3e980ad90306c9534b39526770f029e69e6b00
fivosts <fivos_ts@hotmail.com>
2020-05-09 18:28:10 +0100

    Rename preprocessors test files

--- deeplearning/clgen/preprocessors/test_cxx.py
+++ deeplearning/clgen/preprocessors/test_cxx.py
@@ -12,7 +12,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with clgen.  If not, see <https://www.gnu.org/licenses/>.
-"""Unit tests for :cxx_test.py."""
+"""Unit tests for :test_cxx.py."""
 import pytest
 
 from deeplearning.clgen import errors
