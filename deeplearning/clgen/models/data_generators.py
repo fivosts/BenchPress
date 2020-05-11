efcb3c483f33c1ac0d0ca123c8855ee685d62557
fivosts <fivos_ts@hotmail.com>
2020-05-11 21:44:42 +0100

    Added input_fn generator in data_generator

--- deeplearning/clgen/models/data_generators.py
+++ deeplearning/clgen/models/data_generators.py
@@ -293,12 +293,77 @@ class MaskLMBatchGenerator(object):
     )
     return
 
-  def CreateTfDataset() -> "tf.Dataset":
+  def CreateTfDataset(self,
+                      tf,
+                      max_seq_length,
+                      is_training,
+                      num_cpu_threads,
+                      ) -> "tf.Dataset":
 
-    import tensorflow as tf
-    
+    def _decode_record(self, record, name_to_features):
+      """Decodes a record to a TensorFlow example."""
+      example = tf.parse_single_example(record, name_to_features)
 
-    return
+      # tf.Example only supports tf.int64, but the TPU only supports tf.int32.
+      # So cast all int64 to int32.
+      for name in list(example.keys()):
+        t = example[name]
+        if t.dtype == tf.int64:
+          t = tf.to_int32(t)
+        example[name] = t
+
+      return example
+      
+    def input_fn(params):
+
+      batch_size = params["batch_size"]
+      name_to_features = {
+          "input_ids":
+              tf.FixedLenFeature([max_seq_length], tf.int64),
+          "masked_lm_positions":
+              tf.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.int64),
+          "masked_lm_ids":
+              tf.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.int64),
+      }
+
+      # For training, we want a lot of parallel reading and shuffling.
+      # For eval, we want no shuffling and parallel reading doesn't matter.
+      if is_training:
+        d = tf.data.Dataset.from_tensor_slices(tf.constant(self.masked_corpus))
+        d = d.repeat()
+        d = d.shuffle(buffer_size=len(self.masked_corpus))
+
+        # `cycle_length` is the number of parallel files that get read.
+        cycle_length = min(num_cpu_threads, len(self.masked_corpus))
+
+        # `sloppy` mode means that the interleaving is not exact. This adds
+        # even more randomness to the training pipeline.
+        d = d.apply(
+            tf.contrib.data.parallel_interleave(
+                tf.data.TFRecordDataset,
+                sloppy=is_training,
+                cycle_length=cycle_length))
+        d = d.shuffle(buffer_size=100)
+      else:
+        d = tf.data.TFRecordDataset(self.masked_corpus)
+        # Since we evaluate for a fixed number of steps we don't want to encounter
+        # out-of-range exceptions.
+        d = d.repeat()
+
+      # We must `drop_remainder` on training because the TPU requires fixed
+      # size dimensions. For eval, we assume we are evaluating on the CPU or GPU
+      # and we *don't* want to drop the remainder, otherwise we wont cover
+      # every sample.
+      d = d.apply(
+          tf.contrib.data.map_and_batch(
+              lambda record: self._decode_record(record, name_to_features),
+              batch_size=batch_size,
+              num_parallel_batches=num_cpu_threads,
+              drop_remainder=True))
+      return d
+
+    return input_fn
+
 
   def CreateCorpus(self) -> None:
     l.getLogger().debug("deeplearning.clgen.models.data_generators.MaskLMBatchGenerator.CreateBatches()")
