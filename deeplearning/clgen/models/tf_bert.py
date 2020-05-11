ec4eff03f0b379f6de22d5f6bc0d481375ef1e51
fivosts <fivos_ts@hotmail.com>
2020-05-11 16:36:28 +0100

    #7: Refactor train function

--- deeplearning/clgen/models/tf_bert.py
+++ deeplearning/clgen/models/tf_bert.py
@@ -151,7 +151,7 @@ class tfBert(backends.BackendBase):
     )
     self.summary_writer = tf.compat.v1.summary.FileWriter(tensorboard_dir)
 
-  ## TODO change this function as in modelling:BertModel.__init__
+  ## This is BertConfig.__init__ + BertModel.__init__
   def InitTfGraph(
     self, sampler: typing.Optional[samplers.Sampler] = None
   ) -> "tf":
@@ -264,144 +264,6 @@ class tfBert(backends.BackendBase):
             activation=tf.tanh,
             kernel_initializer=create_initializer(config.initializer_range))
 
-
-    # """Instantiate a TensorFlow graph for training or inference.
-
-    # The tensorflow graph is different for training and inference, so must be
-    # reset when switching between modes.
-
-    # Args:
-    #   sampler: If set, initialize the model for inference using the given
-    #     sampler. If not set, initialize model for training.
-
-    # Returns:
-    #   The imported TensorFlow module.
-    # """
-    # l.getLogger().debug("deeplearning.clgen.models.tf_sequential.tfBert.InitTfGraph()")
-    # # Lock exclusive access to a GPU, if present.
-    # gpu_scheduler.LockExclusiveProcessGpuAccess()
-
-    # start_time = time.time()
-
-    # # Quiet tensorflow.
-    # # See: https://github.com/tensorflow/tensorflow/issues/1258
-    # os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-
-    # # Deferred importing of TensorFlow.
-    # import tensorflow as tf
-    # tf.compat.v1.disable_eager_execution()
-    # from deeplearning.clgen.models import helper
-
-    # cell_type = {
-    #   model_pb2.NetworkArchitecture.LSTM: tf.compat.v1.nn.rnn_cell.LSTMCell,
-    #   model_pb2.NetworkArchitecture.GRU: tf.compat.v1.nn.rnn_cell.GRUCell,
-    #   model_pb2.NetworkArchitecture.RNN: tf.compat.v1.nn.rnn_cell.BasicRNNCell,
-    # }.get(self.config.architecture.neuron_type, None)
-    # if cell_type is None:
-    #   raise NotImplementedError
-
-    # # Reset the graph when switching between training and inference.
-    # tf.compat.v1.reset_default_graph()
-
-    # if sampler:
-    #   sequence_length = sampler.sequence_length
-    #   batch_size = sampler.batch_size
-    # else:
-    #   sequence_length = self.config.training.sequence_length
-    #   batch_size = self.config.training.batch_size
-    # vocab_size = self.atomizer.vocab_size
-
-    # cells_lst = []
-    # for _ in range(self.config.architecture.num_layers):
-    #   cells_lst.append(cell_type(self.config.architecture.neurons_per_layer))
-    # self.cell = cell = tf.keras.layers.StackedRNNCells(cells_lst)
-
-    # self.input_data = tf.compat.v1.placeholder(
-    #   tf.int32, [batch_size, sequence_length]
-    # )
-    # self.targets = tf.compat.v1.placeholder(
-    #   tf.int32, [batch_size, sequence_length]
-    # )
-    # self.initial_state = self.cell.get_initial_state(batch_size = batch_size, dtype = tf.float32)
-    # self.temperature = tf.Variable(1.0, trainable=False)
-    # self.seed_length = tf.compat.v1.placeholder(name = "seed_length", dtype = tf.int32, shape = ())
-
-    # if sampler:
-    #   self.tf_sequence_length = tf.compat.v1.placeholder(tf.int32, [batch_size])
-    # else:
-    #   self.tf_sequence_length = tf.fill([batch_size], sequence_length)
-
-    # scope_name = "rnnlm"
-    # with tf.compat.v1.variable_scope(scope_name):
-    #   with tf.device("/cpu:0"):
-    #     embedding = tf.compat.v1.get_variable(
-    #       "embedding", [vocab_size, self.config.architecture.neurons_per_layer]
-    #     )
-    #     inputs = tf.nn.embedding_lookup(embedding, self.input_data)
-
-    # if sampler:
-    #   decode_helper = helper.CustomInferenceHelper(
-    #     self.seed_length, embedding, self.temperature
-    #   )
-    # else:
-    #   decode_helper = tfa.seq2seq.sampler.TrainingSampler(time_major=False)
-
-    # decoder = tfa.seq2seq.BasicDecoder(
-    #   cell,
-    #   decode_helper,
-    #   tf.compat.v1.layers.Dense(vocab_size),
-    #   dtype = tf.float32,
-    # )
-    # outputs, self.final_state, _ = tfa.seq2seq.dynamic_decode(
-    #   decoder,
-    #   decoder_init_input = inputs,
-    #   decoder_init_kwargs = {
-    #                           'initial_state': self.initial_state,
-    #                           'sequence_length': self.tf_sequence_length,
-    #                         },
-    #   output_time_major=False,
-    #   impute_finished=True,
-    #   swap_memory=True,
-    #   scope=scope_name,
-    # )
-
-    # self.generated = outputs.sample_id
-    # self.logits = outputs.rnn_output
-
-    # sequence_weigths = tf.ones([batch_size, sequence_length])
-    # self.loss = tfa.seq2seq.sequence_loss(
-    #   self.logits, self.targets, sequence_weigths
-    # )
-
-    # self.learning_rate = tf.Variable(0.0, trainable=False)
-    # self.epoch = tf.Variable(0, trainable=False)
-    # trainable_variables = tf.compat.v1.trainable_variables()
-
-    # # TODO(cec): Support non-adam optimizers.
-    # grads, _ = tf.clip_by_global_norm(
-    #   tf.gradients(self.loss, trainable_variables, aggregation_method=2),
-    #   self.config.training.adam_optimizer.normalized_gradient_clip_micros / 1e6,
-    # )
-    # optimizer = tf.compat.v1.train.AdamOptimizer(self.learning_rate)
-    # self.train_op = optimizer.apply_gradients(zip(grads, trainable_variables))
-
-    # if not sampler:
-    #   # Create tensorboard summary writers for training progress.
-    #   tf.compat.v1.summary.scalar("loss", self.loss)
-    #   tf.compat.v1.summary.scalar("learning_rate", self.learning_rate)
-    #   tf.compat.v1.summary.scalar("epoch_num", self.epoch)
-
-    # num_trainable_params = int(
-    #   np.sum([np.prod(v.shape) for v in tf.compat.v1.trainable_variables()])
-    # )
-    # l.getLogger().info(
-    #   "Instantiated TensorFlow graph with {} trainable parameters " "in {} ms."
-    #     .format(
-    #       humanize.Commas(num_trainable_params),
-    #       humanize.Commas(int((time.time() - start_time) * 1000)),
-    #       )
-    # )
-
     return tf
 
   def GetShortSummary(self) -> str:
@@ -494,21 +356,18 @@ class tfBert(backends.BackendBase):
     **unused_kwargs,
   ) -> None:
 
-
   if not FLAGS.do_train and not FLAGS.do_eval:
     raise ValueError("At least one of `do_train` or `do_eval` must be True.")
 
-  bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
+  # tf.gfile.MakeDirs(FLAGS.output_dir)
 
-  tf.gfile.MakeDirs(FLAGS.output_dir)
+  # input_files = []
+  # for input_pattern in FLAGS.input_file.split(","):
+  #   input_files.extend(tf.gfile.Glob(input_pattern))
 
-  input_files = []
-  for input_pattern in FLAGS.input_file.split(","):
-    input_files.extend(tf.gfile.Glob(input_pattern))
-
-  tf.logging.info("*** Input Files ***")
-  for input_file in input_files:
-    tf.logging.info("  %s" % input_file)
+  # tf.logging.info("*** Input Files ***")
+  # for input_file in input_files:
+  #   tf.logging.info("  %s" % input_file)
 
   tpu_cluster_resolver = None
   if FLAGS.use_tpu and FLAGS.tpu_name:
@@ -519,13 +378,13 @@ class tfBert(backends.BackendBase):
   run_config = tf.contrib.tpu.RunConfig(
       cluster=tpu_cluster_resolver,
       master=FLAGS.master,
-      model_dir=FLAGS.output_dir,
+      model_dir=FLAGS.output_dir, ## TODO
       save_checkpoints_steps=FLAGS.save_checkpoints_steps,
       tpu_config=tf.contrib.tpu.TPUConfig(
           iterations_per_loop=FLAGS.iterations_per_loop,
           num_shards=FLAGS.num_tpu_cores,
           per_host_input_for_training=is_per_host))
-
+  ## All good until here
   model_fn = model_fn_builder(
       bert_config=bert_config,
       init_checkpoint=FLAGS.init_checkpoint,
@@ -574,167 +433,7 @@ class tfBert(backends.BackendBase):
         tf.logging.info("  %s = %s", key, str(result[key]))
         writer.write("%s = %s\n" % (key, str(result[key])))
 
-        
-
-    """Locked training.
-
-    If there are cached epoch checkpoints, the one closest to the target number
-    of epochs will be loaded, and the model will be trained for only the
-    remaining number of epochs, if any. This means that calling this function
-    twice will only actually train the model the first time, and all subsequent
-    calls will be no-ops.
-
-    This method must only be called when the model is locked.
-    """
-    l.getLogger().debug("deeplearning.clgen.models.tf_sequential.tfBert.Train()")
-    del unused_kwargs
-
-    if self.is_trained:
-      return
-
-    if self.data_generator is None:
-      self.data_generator = data_generators.TensorflowBatchGenerator(
-        corpus, self.config.training
-      )
-    tf = self.InitTfGraph()
-
-    logger = telemetry.TrainingLogger(self.cache.path / "logs")
-
-    # Create and merge the tensorboard summary ops.
-    merged = tf.compat.v1.summary.merge_all()
-
-    # training options
-    # TODO(cec): Enable support for multiple optimizers:
-    initial_learning_rate = (
-      self.config.training.adam_optimizer.initial_learning_rate_micros / 1e6
-    )
-    decay_rate = (
-      self.config.training.adam_optimizer.learning_rate_decay_per_epoch_micros
-      / 1e6
-    )
-
-    # # resume from prior checkpoint
-    ckpt_path, ckpt_paths = None, None
-    if (self.cache.path / "checkpoints" / "checkpoint").exists():
-      checkpoint_state = tf.train.get_checkpoint_state(
-        self.cache.path / "checkpoints"
-      )
-      assert checkpoint_state
-      assert checkpoint_state.model_checkpoint_path
-      ckpt_path, ckpt_paths = self.GetParamsPath(checkpoint_state)
-
-    with tf.compat.v1.Session() as sess, self.dashboard_db.Session() as dbs:
-      dbs.query(dashboard_db.TrainingTelemetry).filter(
-        dashboard_db.TrainingTelemetry.model_id == self.dashboard_model_id
-      ).filter(dashboard_db.TrainingTelemetry.pending == True).delete()
-
-      tf.compat.v1.global_variables_initializer().run()
-
-      # Keep all checkpoints.
-      saver = tf.compat.v1.train.Saver(
-        tf.compat.v1.global_variables(), max_to_keep=100, save_relative_paths=True
-      )
-
-      # restore model from closest checkpoint.
-      if ckpt_path:
-        l.getLogger().info("Restoring checkpoint {}".format(ckpt_path))
-        saver.restore(sess, ckpt_path)
-
-      # make sure we don't lose track of other checkpoints
-      if ckpt_paths:
-        saver.recover_last_checkpoints(ckpt_paths)
-
-      # Offset epoch counts by 1 so that they are in the range [1..n]
-      current_epoch = sess.run(self.epoch) + 1
-      max_epoch = self.config.training.num_epochs + 1
-
-      # Per-epoch training loop.
-      for epoch_num in range(current_epoch, max_epoch):
-        logger.EpochBeginCallback()
-
-        # decay and set learning rate
-        new_learning_rate = initial_learning_rate * (
-          (float(100 - decay_rate) / 100.0) ** (epoch_num - 1)
-        )
-        sess.run(tf.compat.v1.assign(self.learning_rate, new_learning_rate))
-        sess.run(tf.compat.v1.assign(self.epoch, epoch_num))
-
-        # TODO(cec): refactor data generator to a Python generator.
-        self.data_generator.CreateBatches()
-        l.getLogger().info("Epoch {}/{}:".format(epoch_num, self.config.training.num_epochs))
-        state = sess.run(self.initial_state)
-        # Per-batch inner loop.
-        bar = progressbar.ProgressBar(max_value=self.data_generator.num_batches)
-        last_log_time = time.time()
-        for i in bar(range(self.data_generator.num_batches)):
-          x, y = self.data_generator.NextBatch()
-          feed = {self.input_data: x, self.targets: y}
-          for j, (c, h) in enumerate(self.initial_state):
-            feed[c], feed[h] = state[j].c, state[j].h
-          summary, loss, state, _ = sess.run(
-            [merged, self.loss, self.final_state, self.train_op], feed
-          )
-
-          # Periodically write progress to tensorboard.
-          if i % FLAGS.clgen_tf_backend_tensorboard_summary_step_count == 0:
-            step = (epoch_num - 1) * self.data_generator.num_batches + i
-            self.summary_writer.add_summary(summary, step)
-            # Add telemetry database entry. This isn't committed until the end
-            # of the epoch, when the checkpoint is created.
-            now = time.time()
-            duration_ns = int((now - last_log_time) * 1e6)
-            dbs.add(
-              dashboard_db.TrainingTelemetry(
-                model_id=self.dashboard_model_id,
-                epoch=epoch_num,
-                step=step,
-                training_loss=loss,
-                learning_rate=new_learning_rate,
-                ns_per_batch=int(duration_ns)
-                / FLAGS.clgen_tf_backend_tensorboard_summary_step_count,
-              )
-            )
-            last_log_time = now
-            dbs.commit()
-
-        # Log the loss and delta.
-        l.getLogger().info("Loss: {:.6f}.".format(loss))
-
-        # Save after every epoch.
-        start_time = time.time()
-        global_step = epoch_num
-        checkpoint_prefix = self.cache.path / "checkpoints" / "checkpoint"
-        checkpoint_path = saver.save(
-          sess, checkpoint_prefix, global_step=global_step
-        )
-        l.getLogger().info(
-          "Saved checkpoint {} in {} ms."
-            .format(
-              checkpoint_path,
-              humanize.Commas(int((time.time() - start_time) * 1000)),
-              )
-        )
-        dbs.query(dashboard_db.TrainingTelemetry).filter(
-          dashboard_db.TrainingTelemetry.pending == True
-        ).update({"pending": False})
-        dbs.commit()
-        assert pathlib.Path(
-          f"{checkpoint_prefix}-{global_step}.index"
-        ).is_file()
-        assert pathlib.Path(f"{checkpoint_prefix}-{global_step}.meta").is_file()
-
-        logger.EpochEndCallback(epoch_num, loss)
-        # If we have a sampler that we can use at the end of epochs, then
-        # break now to run the test sampler.
-        # This is confusing logic! Consider a refactor to simplify things.
-        if test_sampler:
-          break
-      else:
-        return
-
-    if test_sampler and FLAGS.clgen_per_epoch_test_samples > 0:
-      self._EndOfEpochTestSample(corpus, test_sampler, step, epoch_num)
-      self.Train(corpus, test_sampler=test_sampler)
+  return
 
   def _EndOfEpochTestSample(
     self, corpus, sampler: samplers.Sampler, step: int, epoch_num: int
