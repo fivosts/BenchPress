60527403f878a4fafccee252351e770d6b908e4c
fivosts <fivos_ts@hotmail.com>
2020-05-14 22:36:03 +0100

    Add custom tf module

--- deeplearning/clgen/tf.py
+++ deeplearning/clgen/tf.py
@@ -1,10 +1,10 @@
-
+"""A wrapper module to include tensorflow"""
 tf = None
 if tf is None:
+  import os
   import tensorflow
-  print("AHYAHA\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
   tf = tensorflow
-  tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+  tf.python.util.deprecation._PRINT_DEPRECATION_WARNINGS = False
+  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+  # tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
   tf.compat.v1.disable_eager_execution()
-else:
-  print("AOUA\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
