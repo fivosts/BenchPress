16570fe941e3646e1fb23f0622396fe387020f53
fivosts <fivos_ts@hotmail.com>
2020-05-15 14:36:06 +0100

    Update tf py

--- deeplearning/clgen/tf.py
+++ deeplearning/clgen/tf.py
@@ -5,6 +5,5 @@ if tf is None:
   import tensorflow
   tf = tensorflow
   tf.python.util.deprecation._PRINT_DEPRECATION_WARNINGS = False
-  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-  # tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.INFO)
+  os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' z
   tf.compat.v1.disable_eager_execution()
