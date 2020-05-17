1432d1834d031d6a64034a6c60b61e4d73153e60
fivosts <fivos_ts@hotmail.com>
2020-05-17 01:32:37 +0100

    Add custom progressbar hook

---
+++ deeplearning/clgen/models/tf_bert/hooks.py
@@ -0,0 +1,48 @@
+import progressbar
+
+from deeplearning.clgen.tf import tf
+from eupy.native import logger as l
+
+
+class tfProgressBar(tf.train.SessionRunHook):
+  """Real time progressbar to capture tf Estimator training or validation"""
+
+  def __init__(self, length: int):
+    """
+    Set class variables
+    :param length:
+        Upper threshold of progressbar
+    """
+    self.max_length = length
+
+  def begin(self):
+    """
+        Called once at initialization stage
+    :param session:
+        Tensorflow session
+    :param coord:
+        unused
+    """
+    self.current_step = 0
+    self.bar = progressbar.ProgressBar(max_value = self.max_length)
+
+  def before_run(self):
+    """
+      Called before session.run()
+      Any tensor/op should be declared here in order to be evaluated
+      returns None or SessionRunArgs()
+    """
+    self.current_step += 1
+    return None
+
+  def after_run(self):
+    """
+      Requested tensors are evaluated and their values are available
+    """
+    self.bar.update(self.current_step)
+
+  def end(self):
+    """
+      Called at the end of session
+    """
+    self.current_step = 0
