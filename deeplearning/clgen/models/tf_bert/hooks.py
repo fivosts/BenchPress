93faa032af5ecafd98404c6e36a5879ad72af6af
fivosts <fivos_ts@hotmail.com>
2020-05-17 01:38:21 +0100

    Debug hooks

--- deeplearning/clgen/models/tf_bert/hooks.py
+++ deeplearning/clgen/models/tf_bert/hooks.py
@@ -4,7 +4,7 @@ from deeplearning.clgen.tf import tf
 from eupy.native import logger as l
 
 
-class tfProgressBar(tf.train.SessionRunHook):
+class tfProgressBar(tf.compat.v1.train.SessionRunHook):
   """Real time progressbar to capture tf Estimator training or validation"""
 
   def __init__(self, length: int):
@@ -26,7 +26,7 @@ class tfProgressBar(tf.train.SessionRunHook):
     self.current_step = 0
     self.bar = progressbar.ProgressBar(max_value = self.max_length)
 
-  def before_run(self):
+  def before_run(self, run_context):
     """
       Called before session.run()
       Any tensor/op should be declared here in order to be evaluated
@@ -35,13 +35,13 @@ class tfProgressBar(tf.train.SessionRunHook):
     self.current_step += 1
     return None
 
-  def after_run(self):
+  def after_run(self, run_context, run_values):
     """
       Requested tensors are evaluated and their values are available
     """
     self.bar.update(self.current_step)
 
-  def end(self):
+  def end(self, session):
     """
       Called at the end of session
     """
