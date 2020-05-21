c47ad271a7b2e58768a7badfd7880ca1d7e5d2ed
fivosts <fivos_ts@hotmail.com>
2020-05-21 15:38:20 +0100

    Split generator with initialization

--- deeplearning/clgen/models/data_generators.py
+++ deeplearning/clgen/models/data_generators.py
@@ -478,14 +478,14 @@ class MaskLMBatchGenerator(object):
                                       )
             ])
 
-  def generateTfSamples(self,
-                       input_sample,
-                       max_seq_length,
-                       batch_size,
-                       num_cpu_threads,
-                       use_tpu,
-                       drop_remainder
-                       ):
+  def initSampleBatch(self,
+                      input_sample,
+                      max_seq_length,
+                      batch_size,
+                      num_cpu_threads,
+                      use_tpu,
+                      drop_remainder
+                      ) -> None:
 
     if np.ndim(input_sample) != 1:
       raise ValueError("generateTfSamples works only on single dimensional samples!\
@@ -494,88 +494,88 @@ class MaskLMBatchGenerator(object):
     expanded_sample = self.expandHoleToMasks(
           input_sample, max_seq_length - len(input_sample) + 1
           )
-    assert len(expanded_sample) == max_seq_length
+
     padded_sample = self.padToMaxPosition(expanded_sample)
+
+    assert len(expanded_sample) == max_seq_length
     assert len(padded_sample) == self.max_position_embeddings
 
-    l.getLogger().critical("generateTfSamples Not implemented")
-    exit()
+    return
+    # def _decode_record(record, name_to_features):
+    #   """Decodes a record to a TensorFlow example."""
+    #   ## This function assumes record is still a file (expressed as TF dataset)
+    #   ## It decodes this record to tf scalars.
+    #   ## You already have them so this will be skipped
+    #   example = tf.io.parse_single_example(record, name_to_features)
 
-    def _decode_record(record, name_to_features):
-      """Decodes a record to a TensorFlow example."""
-      ## This function assumes record is still a file (expressed as TF dataset)
-      ## It decodes this record to tf scalars.
-      ## You already have them so this will be skipped
-      example = tf.io.parse_single_example(record, name_to_features)
+    #   # tf.Example only supports tf.int64, but the TPU only supports tf.int32.
+    #   # So cast all int64 to int32.
+    #   for name in list(example.keys()):
+    #     t = example[name]
+    #     if t.dtype == tf.int64:
+    #       # t = tf.compat.v1.to_int32(t)
+    #       t = tf.cast(t, dtype = tf.int32)
 
-      # tf.Example only supports tf.int64, but the TPU only supports tf.int32.
-      # So cast all int64 to int32.
-      for name in list(example.keys()):
-        t = example[name]
-        if t.dtype == tf.int64:
-          # t = tf.compat.v1.to_int32(t)
-          t = tf.cast(t, dtype = tf.int32)
+    #     example[name] = t
 
-        example[name] = t
+    #   return example
 
-      return example
+    # def input_fn(params):
 
-    def input_fn(params):
+    #   batch_size = params["batch_size"]
+    #   name_to_features = {
+    #       "input_ids":
+    #           tf.io.FixedLenFeature([max_seq_length], tf.int64),
+    #       "masked_lm_positions":
+    #           tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.int64),
+    #       "masked_lm_ids":
+    #           tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.int64),
+    #       "masked_lm_weights":
+    #           tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.float32),
+    #       "next_sentence_labels":
+    #           tf.io.FixedLenFeature([1], tf.int64),
+    #   }
 
-      batch_size = params["batch_size"]
-      name_to_features = {
-          "input_ids":
-              tf.io.FixedLenFeature([max_seq_length], tf.int64),
-          "masked_lm_positions":
-              tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.int64),
-          "masked_lm_ids":
-              tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.int64),
-          "masked_lm_weights":
-              tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.float32),
-          "next_sentence_labels":
-              tf.io.FixedLenFeature([1], tf.int64),
-      }
+    #   dataset = tf.io.gfile.glob(str(self.tfRecord))
+    #   # For training, we want a lot of parallel reading and shuffling.
+    #   # For eval, we want no shuffling and parallel reading doesn't matter.
+    #   if is_training:
+    #     d = tf.data.Dataset.from_tensor_slices(tf.constant(dataset))
+    #     d = d.repeat()
+    #     if self.training_opts.shuffle_corpus_contentfiles_between_epochs:
+    #       d = d.shuffle(buffer_size=len(dataset))
 
-      dataset = tf.io.gfile.glob(str(self.tfRecord))
-      # For training, we want a lot of parallel reading and shuffling.
-      # For eval, we want no shuffling and parallel reading doesn't matter.
-      if is_training:
-        d = tf.data.Dataset.from_tensor_slices(tf.constant(dataset))
-        d = d.repeat()
-        if self.training_opts.shuffle_corpus_contentfiles_between_epochs:
-          d = d.shuffle(buffer_size=len(dataset))
+    #     # `cycle_length` is the number of parallel files that get read.
+    #     cycle_length = min(num_cpu_threads, len(dataset))
 
-        # `cycle_length` is the number of parallel files that get read.
-        cycle_length = min(num_cpu_threads, len(dataset))
+    #     # `sloppy` mode means that the interleaving is not exact. This adds
+    #     # even more randomness to the training pipeline.
+    #     d = d.apply(
+    #         tf.data.experimental.parallel_interleave(
+    #             tf.data.TFRecordDataset,
+    #             sloppy=is_training,
+    #             cycle_length=cycle_length))
+    #     if self.training_opts.shuffle_corpus_contentfiles_between_epochs:
+    #       d = d.shuffle(buffer_size=100)
+    #   else:
+    #     d = tf.data.TFRecordDataset(dataset)
+    #     # Since we evaluate for a fixed number of steps we don't want to encounter
+    #     # out-of-range exceptions.
+    #     d = d.repeat()
 
-        # `sloppy` mode means that the interleaving is not exact. This adds
-        # even more randomness to the training pipeline.
-        d = d.apply(
-            tf.data.experimental.parallel_interleave(
-                tf.data.TFRecordDataset,
-                sloppy=is_training,
-                cycle_length=cycle_length))
-        if self.training_opts.shuffle_corpus_contentfiles_between_epochs:
-          d = d.shuffle(buffer_size=100)
-      else:
-        d = tf.data.TFRecordDataset(dataset)
-        # Since we evaluate for a fixed number of steps we don't want to encounter
-        # out-of-range exceptions.
-        d = d.repeat()
+    #   # We must `drop_remainder` on training because the TPU requires fixed
+    #   # size dimensions. For eval, we assume we are evaluating on the CPU or GPU
+    #   # and we *don't* want to drop the remainder, otherwise we wont cover
+    #   # every sample.
+    #   d = d.apply(
+    #       tf.data.experimental.map_and_batch(
+    #           lambda record: _decode_record(record, name_to_features),
+    #           batch_size=batch_size,
+    #           num_parallel_batches=num_cpu_threads,
+    #           drop_remainder=use_tpu))
+    #   return d
 
-      # We must `drop_remainder` on training because the TPU requires fixed
-      # size dimensions. For eval, we assume we are evaluating on the CPU or GPU
-      # and we *don't* want to drop the remainder, otherwise we wont cover
-      # every sample.
-      d = d.apply(
-          tf.data.experimental.map_and_batch(
-              lambda record: _decode_record(record, name_to_features),
-              batch_size=batch_size,
-              num_parallel_batches=num_cpu_threads,
-              drop_remainder=use_tpu))
-      return d
-
-    return input_fn
+    return
 
   def CreateCorpus(self) -> None:
     l.getLogger().debug("deeplearning.clgen.models.data_generators.MaskLMBatchGenerator.CreateBatches()")
