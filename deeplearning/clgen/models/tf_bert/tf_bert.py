4a0abffa31862f7ae3bbfcf77129ebc4a80a7712
fivosts <fivos_ts@hotmail.com>
2020-05-23 22:09:05 +0100

    Print short summary of training model

--- deeplearning/clgen/models/tf_bert/tf_bert.py
+++ deeplearning/clgen/models/tf_bert/tf_bert.py
@@ -196,7 +196,7 @@ class tfBert(backends.BackendBase):
           eval_batch_size = self.eval_batch_size)
 
       l.getLogger().info("BERT Training initialization")
-      ## TODO print short summary of model and/or dataset features
+      l.getLogger().info(self.GetShortSummary())
 
       train_input_fn = self.data_generator.generateTfDataset(
           max_seq_length = self.max_seq_length,
@@ -213,7 +213,6 @@ class tfBert(backends.BackendBase):
       estimator.train(input_fn=train_input_fn, max_steps = self.num_train_steps)
 
       l.getLogger().info("BERT Validation")
-      ## TODO print short summary of model and/or dataset features
 
       eval_input_fn = self.data_generator.generateTfDataset(
           max_seq_length=self.max_seq_length,
