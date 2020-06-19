3d25d950f857f035e0008d5244e0b691731b4e50
fivosts <fivos_ts@hotmail.com>
2020-06-19 15:50:14 +0300

    #64: Add libclc and data includes to path

--- deeplearning/clgen/environment.py
+++ deeplearning/clgen/environment.py
@@ -1,8 +1,14 @@
 import os
-
 try:
-  CLANG          = os.environ['CLANG']
-  CLANG_REWRITER = os.environ['CLANG_REWRITER']
-  LLVM           = os.environ['LLVM']
+  LLVM                = os.environ['LLVM']
+  LIBCXX_HEADERS      = os.environ['LIBCXX_HEADERS']
+  CLANG               = os.environ['CLANG']
+  CLANG_HEADERS       = os.environ['CLANG_HEADERS']
+  CLANG_REWRITER      = os.environ['CLANG_REWRITER']
+  LIBCLC              = os.environ['LIBCLC']
+  DASHBOARD_TEMPLATES = os.environ['DASHBOARD_TEMPLATES']
+  DASHBOARD_STATIC    = os.environ['DASHBOARD_STATIC']
+  DATA_CL_INCLUDE     = os.environ['DATA_CL_INCLUDE']
 except Exception as e:
   raise e
+  
