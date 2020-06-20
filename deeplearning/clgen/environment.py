8c2a9deafe7052dda0dcc7848df2d8fecbd9075a
fivosts <fivos_ts@hotmail.com>
2020-06-20 22:04:55 +0300

    Add some path checks

--- deeplearning/clgen/environment.py
+++ deeplearning/clgen/environment.py
@@ -1,15 +1,22 @@
 import os
+
+def check_exists(path):
+  if not os.path.exists(path):
+    raise ValueError("{} does not exist.".format(path))
+  return path
+
 try:
-  LLVM                = os.environ['LLVM']
-  LIBCXX_HEADERS      = os.environ['LIBCXX_HEADERS']
-  CLANG               = os.environ['CLANG']
-  CLANG_FORMAT        = os.environ['CLANG_FORMAT']
-  CLANG_HEADERS       = os.environ['CLANG_HEADERS']
-  CLANG_REWRITER      = os.environ['CLANG_REWRITER']
-  LIBCLC              = os.environ['LIBCLC']
-  DASHBOARD_TEMPLATES = os.environ['DASHBOARD_TEMPLATES']
-  DASHBOARD_STATIC    = os.environ['DASHBOARD_STATIC']
-  DATA_CL_INCLUDE     = os.environ['DATA_CL_INCLUDE']
+  LLVM                = check_exists(os.environ['LLVM'])
+  LIBCXX_HEADERS      = check_exists(os.environ['LIBCXX_HEADERS'])
+  CLANG               = check_exists(os.environ['CLANG'])
+  CLANG_FORMAT        = check_exists(os.environ['CLANG_FORMAT'])
+  CLANG_HEADERS       = check_exists(os.environ['CLANG_HEADERS'])
+  CLANG_REWRITER      = check_exists(os.environ['CLANG_REWRITER'])
+  LIBCLC              = check_exists(os.environ['LIBCLC'])
+  DASHBOARD_TEMPLATES = check_exists(os.environ['DASHBOARD_TEMPLATES'])
+  DASHBOARD_STATIC    = check_exists(os.environ['DASHBOARD_STATIC'])
+  DATA_CL_INCLUDE     = check_exists(os.environ['DATA_CL_INCLUDE'])
+
 except Exception as e:
   raise e
   
