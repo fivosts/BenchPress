c2fe1dc0a1bf2689112e9287aa097ea4d3cb3f53
fivosts <fivos_ts@hotmail.com>
2020-06-23 20:08:06 +0300

    Rename model zoo

--- bert_tiny.sh
+++ bert_tiny.sh
@@ -1,1 +1,1 @@
-./clgen --config model_zoo/BERT/bert_tiny.pbtxt --level 20 --min_samples 10 --workspace_dir ./workspace
+./clgen --config model_zoo/BERT/tiny.pbtxt --level 20 --min_samples 10 --workspace_dir ./workspace
