fdc5b6c0ee9d9782da1ae588af3b793cf3472d75
fivosts <fivos_ts@hotmail.com>
2020-06-24 20:16:57 +0300

    Add varargs in helper script

--- bert_tiny.sh
+++ bert_tiny.sh
@@ -1,1 +1,1 @@
-./clgen --config model_zoo/BERT/tiny.pbtxt --level 20 --min_samples 10 --workspace_dir ./workspace
+./clgen --config model_zoo/BERT/tiny.pbtxt --min_samples 10 --workspace_dir ./workspace "$@"
