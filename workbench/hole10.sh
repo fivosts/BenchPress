2fe5063de6d905e5d8263a7e5eab688a49bce673
fivosts <fivos_ts@hotmail.com>
2020-07-14 21:42:50 +0300

    Edit scripts

--- workbench/hole10.sh
+++ workbench/hole10.sh
@@ -2,10 +2,10 @@ echo "GPU 0 on Swift"
 for i in {1..10};
 do
 	steps=$((20000*$i))
-	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/workspace --config ./workbench/hole10/hole.pbtxt --notify_me="fivos_ts@hotmail.com"
-	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/workspace --config ./workbench/hole10/hole.pbtxt --notify_me="fivos_ts@hotmail.com"
-	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/workspace --config ./workbench/hole10/hole.pbtxt --notify_me="fivos_ts@hotmail.com"
-	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/workspace --config ./workbench/hole10/hole.pbtxt --notify_me="fivos_ts@hotmail.com"
-	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/workspace --config ./workbench/hole10/hole.pbtxt --notify_me="fivos_ts@hotmail.com"
-	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/workspace --config ./workbench/hole10/hole.pbtxt --notify_me="fivos_ts@hotmail.com"
+	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/mount/ft/workspace --config ./workbench/hole10/hole.pbtxt --notify_me="fivos_ts@hotmail.com"
+	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/mount/ft/workspace --config ./workbench/hole10/hole_dupe.pbtxt --notify_me="fivos_ts@hotmail.com"
+	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/mount/ft/workspace --config ./workbench/hole10/hole_few_preds.pbtxt --notify_me="fivos_ts@hotmail.com"
+	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/mount/ft/workspace --config ./workbench/hole10/hole_many_preds.pbtxt --notify_me="fivos_ts@hotmail.com"
+	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/mount/ft/workspace --config ./workbench/hole10/hole_normal.pbtxt --notify_me="fivos_ts@hotmail.com"
+	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/mount/ft/workspace --config ./workbench/hole10/hole_seq.pbtxt --notify_me="fivos_ts@hotmail.com"
 done
