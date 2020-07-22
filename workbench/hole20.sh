bf28bcaf3e03cacc1fe704f4ec54b1d55693ae64
fivosts <fivos_ts@hotmail.com>
2020-07-22 22:15:27 +0300

    Fix new experiment workbench

--- workbench/hole20.sh
+++
@@ -1,6 +0,0 @@
-echo "GPU 1 on CC3"
-for i in {1..10};
-do
-	steps=$((100000*$i))
-	./clgen  --min_samples 2 --sample_per_epoch=0 --num_train_steps=$steps --workspace_dir $HOME/mount/ft/workspace --config ./workbench/hole20/hole.pbtxt --notify_me="fivos_ts@hotmail.com"
-done
