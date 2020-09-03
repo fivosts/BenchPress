65d0ffea87254c4fd03c1995a124c9e8a1a87fc5
fivosts <fivos_ts@hotmail.com>
2020-09-03 20:33:07 +0300

    #81: Fix pytorch dataloader for training

--- deeplearning/clgen/models/torch_bert/data_generator.py
+++ deeplearning/clgen/models/torch_bert/data_generator.py
@@ -6,6 +6,7 @@ provides Python Generator classes for use by a sequential Keras model's
 fit_generator() method to stream batches of training data.
 """
 import sys
+import os
 import time
 import typing
 import random
@@ -63,27 +64,6 @@ class MaskSequence(typing.NamedTuple):
   masked_lm_weights    : np.array
   masked_lm_lengths    : np.array
   next_sentence_label  : np.int32
-
-  @staticmethod
-  def tfTypes():
-    return (tf.int32, tf.int32, tf.int32, tf.int32, tf.int32, tf.int32, tf.float32, tf.int32, tf.int32)
-
-  @staticmethod
-  def npTypes():
-    return (np.int32, np.int32, np.int32, np.int32, np.int32, np.int32, np.float32, np.int32, np.int32)
-
-  @staticmethod
-  def tfShapes(batch_size, sequence_length, max_position_embeddings = None):
-    return (tf.TensorShape([batch_size, 1]),
-            tf.TensorShape([batch_size, sequence_length]),
-            tf.TensorShape([batch_size, sequence_length]),
-            tf.TensorShape([batch_size, sequence_length]),
-            tf.TensorShape([batch_size, max_position_embeddings]),
-            tf.TensorShape([batch_size, max_position_embeddings]),
-            tf.TensorShape([batch_size, max_position_embeddings]),
-            tf.TensorShape([batch_size, max_position_embeddings]),
-            tf.TensorShape([batch_size, 1]),
-           )
 
   @staticmethod
   def estimatedSize(batch_size, sequence_length, max_position_embeddings = None):
@@ -560,204 +540,19 @@ class MaskLMBatchGenerator(object):
       )
     return
 
-  def generateTfDataset(self,
-                      sequence_length: int,
-                      is_training    : bool,
-                      num_cpu_threads: int,
-                      eval_set       : typing.List = None,
-                      use_tpu        : bool = False,
-                      ) -> "tf.Dataset":
-    """Wrapper function that constructs a tf.Dataset used for training BERT."""
-
-    def input_fn(params):
-      """
-      function used by tf.estimator to generate inputs for training.
-      """
-      def _decode_record(record, name_to_features):
-        """Decodes a record to a TensorFlow example."""
-        ## This function assumes record is still a file (expressed as TF dataset)
-        ## It decodes this record to tf scalars.
-        example = tf.io.parse_single_example(record, name_to_features)
-        # tf.Example only supports tf.int64, but the TPU only supports tf.int32.
-        # So cast all int64 to int32.
-        for name in list(example.keys()):
-          t = example[name]
-          if t.dtype == tf.int64:
-            t = tf.cast(t, dtype = tf.int32)
-          example[name] = t
-        return example
-
-      batch_size = params["batch_size"]
-      name_to_features = {
-          "seen_in_training"        : tf.io.FixedLenFeature([1], tf.int64),
-          "original_input"          : tf.io.FixedLenFeature([sequence_length], tf.int64),
-          "input_ids"               : tf.io.FixedLenFeature([sequence_length], tf.int64),
-          "input_mask"              : tf.io.FixedLenFeature([sequence_length], tf.int64),
-          "masked_lm_positions"     : tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.int64),
-          "masked_lm_ids"           : tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.int64),
-          "masked_lm_weights"       : tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.float32),
-          "masked_lm_lengths"       : tf.io.FixedLenFeature([self.training_opts.max_predictions_per_seq], tf.int64),
-          "next_sentence_labels"    : tf.io.FixedLenFeature([1], tf.int64),
-      }
-
-      # For training, we want a lot of parallel reading and shuffling.
-      # For eval, we want no shuffling and parallel reading doesn't matter.
-      if is_training:
-        dataset = tf.io.gfile.glob([str(p) for p in self.dataset['train_dataset']['pt_record']])
-        d = tf.data.Dataset.from_tensor_slices(tf.constant(dataset))
-        d = d.repeat()
-        if self.training_opts.shuffle_corpus_contentfiles_between_epochs:
-          d = d.shuffle(buffer_size=len(dataset))
-
-        # `cycle_length` is the number of parallel files that get read.
-        cycle_length = min(num_cpu_threads, len(dataset))
-
-        # `sloppy` mode means that the interleaving is not exact. This adds
-        # even more randomness to the training pipeline.
-        d = d.apply(
-            tf.data.experimental.parallel_interleave(
-                tf.data.TFRecordDataset,
-                sloppy=is_training,
-                cycle_length=cycle_length))
-        if self.training_opts.shuffle_corpus_contentfiles_between_epochs:
-          d = d.shuffle(buffer_size=100)
-      else:
-        if eval_set is None:
-          dataset = tf.io.gfile.glob(
-            [str(path) for tf_set in self.dataset for path in self.dataset[tf_set]['pt_record']]
-          )
-        else:
-          dataset = tf.io.gfile.glob([str(tf_set) for tf_set in eval_set])
-        d = tf.data.TFRecordDataset(dataset)
-        # Since we evaluate for a fixed number of steps we don't want to encounter
-        # out-of-range exceptions.
-        d = d.repeat()
-
-      # We must `drop_remainder` on training because the TPU requires fixed
-      # size dimensions. For eval, we assume we are evaluating on the CPU or GPU
-      # and we *don't* want to drop the remainder, otherwise we wont cover
-      # every sample.
-      d = d.apply(
-          tf.data.experimental.map_and_batch(
-              lambda record: _decode_record(record, name_to_features),
-              batch_size=batch_size,
-              num_parallel_batches=num_cpu_threads,
-              drop_remainder=use_tpu))
-      return d
-    return input_fn
-
-  def generateTfSamples(self):
-    """
-    Contains input_fn closure function for estimator
-    
-    Returns:
-      input_fn callable.
-    """
-
-    def input_fn(params):
-      """
-      function used by tf.estimator to generate inputs for inference.
-      """
-
-      def sample_gen(batch_size: int):
-        """
-        This generator yields iteratively the inference input blob for each step.
-        In the first iteration, it yields sampler.encoded_start_text and then for each step,
-        self.sampleBatch is updated with the model's current output through self.updateVocabulary.
-        The generator stops when the model has filled all mask or hole tokens with predictions.
-
-        Arguments:
-          batch_size: The batch size used during inference.
-
-        Yields:
-          Current step's inference input for model.
-
-        Returns:
-          None
-        """
-        assert batch_size == len(self.sampleBatch), "{}, {}".format(batch_size, len(self.sampleBatch))
-        original_input = [sample for sample in self.sampleBatch]
-        while True:
-
-          (input_ids, input_mask, masked_lm_positions,
-          masked_lm_ids, masked_lm_weights, masked_lm_lengths) = [], [], [], [], [], []
-
-          max_mask_len = max(
-          [len(np.where(np.in1d(np.asarray(x), [self.atomizer.maskToken, self.atomizer.holeToken]))[0]) for x in self.sampleBatch]
-          )
-          if max_mask_len == 0:
-            return
-          for sample in self.sampleBatch:
-            sample_masks = np.where(np.in1d(sample, [self.atomizer.maskToken, self.atomizer.holeToken]))[0]
-            actual_mask_len = len(sample_masks)
-            len_offset     = max_mask_len - actual_mask_len
-            pad_idx      = np.where(sample == self.atomizer.padToken)[0]
-            inp_mask     = np.ones(len(sample), dtype = np.int32)
-            if len(pad_idx) > 0:
-              inp_mask[pad_idx[0]:] = 0
-
-            input_ids.append(list(sample))
-            input_mask.append(list(inp_mask))
-            masked_lm_positions.append(list(sample_masks) + [0] * len_offset)
-            masked_lm_ids.append([self.atomizer.maskToken] * actual_mask_len + [self.atomizer.padToken] * len_offset)
-            masked_lm_weights.append([0.0] * (actual_mask_len + len_offset))
-            masked_lm_lengths.append([-1] * (actual_mask_len + len_offset))
-          yield (np.full([batch_size, 1], -1), original_input, 
-            input_ids, input_mask,
-            masked_lm_positions, masked_lm_ids,
-            masked_lm_weights, masked_lm_lengths,
-            np.zeros([batch_size, 1]))
-
-      batch_size = params['batch_size']
-      sample = tf.data.Dataset.from_generator(
-                lambda: sample_gen(batch_size), 
-                output_types = MaskSequence.tfTypes(),
-                output_shapes = MaskSequence.tfShapes(batch_size, self.sampler.sequence_length)
-                )
-
-      it = tf.compat.v1.data.make_one_shot_iterator(sample)
-      (seen_in_training, original_input, 
-        input_ids, input_mask,
-        masked_lm_positions, masked_lm_ids,
-        masked_lm_weights, masked_lm_lengths, next_sentence_labels) = it.get_next()
-
-      return {
-          'seen_in_training'      : seen_in_training,
-          'original_input'        : original_input,
-          'input_ids'             : input_ids,
-          'input_mask'            : input_mask,
-          'masked_lm_positions'   : masked_lm_positions,
-          'masked_lm_ids'         : masked_lm_ids,
-          'masked_lm_weights'     : masked_lm_weights,
-          'masked_lm_lengths'     : masked_lm_lengths,
-          'next_sentence_labels'  : next_sentence_labels,
-      }
-    return input_fn
-
-  def tfRecordSampleGenerator(self):
-    assert not self.sampler.config.HasField("start_text")
-
-    if self.sampler.config.HasField("train_set"):
-      sampledDataset = "train_dataset"
-    elif self.sampler.config.HasField("validation_set"):
-      sampledDataset = "validation_dataset"
-    elif self.sampler.config.HasField("sample_set"):
-      sampledDataset = "pred_{}_{}".format(
-        self.sampler.config.sample_set.max_predictions_per_seq,
-        "mask" if self.sampler.config.sample_set.HasField("mask") 
-               else "hole_{}".format(self.sampler.config.sample_set.hole.hole_length)
-      )
-    path_list = glob.glob(str(self.cache.path / "train_dataset_*.pt_record"))
-    if len(path_list) == 0:
-      raise FileNotFoundError(path_list)
-
-    for path in path_list:
-      for example in tf.compat.v1.io.pt_record_iterator(path):
-        input_ids = np.asarray(tf.train.Example.FromString(example).features.feature['input_ids'].int64_list.value)
-        if self.atomizer.padToken in input_ids:
-          yield input_ids[:np.where(input_ids == self.atomizer.padToken)[0][0]]
-        else:
-          yield input_ids
+  def trainDataLoader(self):
+    """Pytorch dataloader that assembles all dataset files into a single-mapped dataset."""
+    dataset = torch.utils.data.ConcatDataset(
+                [torch.load(x) for x in self.dataset['train_dataset']['pt_record']]
+              )
+    loader = torch.utils.data.dataloader.DataLoader(
+      dataset     = dataset,
+      batch_size  = self.training_opts.batch_size,
+      sampler     = torch.utils.data.RandomSampler(dataset, replacement = False),
+      num_workers = os.cpu_count(),
+      drop_last   = True,
+    )
+    return loader
 
   def createCorpus(self) -> None:
     """
