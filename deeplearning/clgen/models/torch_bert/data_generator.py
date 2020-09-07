bf2419f072a1b58809f0247163a03c69bf7276b5
fivosts <fivos_ts@hotmail.com>
2020-09-08 02:45:37 +0300

    Refactor torch data generator

--- deeplearning/clgen/models/torch_bert/data_generator.py
+++ deeplearning/clgen/models/torch_bert/data_generator.py
@@ -23,7 +23,8 @@ import numpy as np
 from deeplearning.clgen.util import cache
 from deeplearning.clgen.util import pbutil
 from deeplearning.clgen.util import distributions
-from deeplearning.clgen.util.tf import tf
+from deeplearning.clgen.util import pytorch
+from deeplearning.clgen.util.pytorch import torch
 from deeplearning.clgen.proto import model_pb2
 from absl import flags
 from eupy.native import logger as l
@@ -291,7 +292,7 @@ def _holeSequence(seq: np.array,
                       np.asarray(input_ids[:len(seq)], dtype = np.int64),
                       input_mask,
                       np.arange(len(seq),              dtype = np.int64),
-                      np.asarray(mask_labels,          dtype = np.int64), 
+                      np.asarray(mask_labels,          dtype = np.int64),
                       np.asarray(masked_lm_lengths,    dtype = np.int64),
                       next_sentence_label
                      )
@@ -381,7 +382,7 @@ def _maskSequence(seq: np.array,
                       np.asarray(input_ids[:len(seq)], dtype = np.int64),
                       input_mask,
                       np.arange(len(seq),              dtype = np.int64),
-                      np.asarray(mask_labels,          dtype = np.int64), 
+                      np.asarray(mask_labels,          dtype = np.int64),
                       np.asarray(masked_lm_lengths,    dtype = np.int64),
                       next_sentence_label
                      )
@@ -389,8 +390,6 @@ def _maskSequence(seq: np.array,
 class MaskLMBatchGenerator(object):
   def __init__(self):
 
-    self.pytorch                 = None
-    self.torch                   = None
     self.dataset                 = None
     self.corpus                  = None
     self.atomizer                = None
@@ -410,15 +409,12 @@ class MaskLMBatchGenerator(object):
 
   @classmethod
   def TrainMaskLMBatchGenerator(cls,
-                               pytorch,
                                corpus: "corpuses.Corpus",
                                training_opts: model_pb2.TrainingOptions,
                                cache_path,
                                ) -> "data_generator.MaskLMBatchGenerator":
     """Initializes data generator for training."""
     d               = MaskLMBatchGenerator()
-    d.pytorch       = pytorch
-    d.torch         = pytorch.torch
     d.cache         = cache.mkcache(cache_path, "dataset")
     d.cache.path.mkdir(exist_ok = True, parents = True)
 
@@ -435,7 +431,6 @@ class MaskLMBatchGenerator(object):
 
   @classmethod
   def SampleMaskLMBatchGenerator(cls,
-                                pytorch,
                                 sampler,
                                 atomizer,
                                 seed: int,
@@ -444,8 +439,6 @@ class MaskLMBatchGenerator(object):
                                 ) -> "data_generator.MaskLMBatchGenerator":
     """Initializes data generator for inference."""
     d                         = MaskLMBatchGenerator()
-    d.pytorch       = pytorch
-    d.torch                   = pytorch.torch
     d.cache                   = cache.mkcache(cache_path, "dataset")
     d.sampler                 = sampler
     d.atomizer                = atomizer
@@ -532,19 +525,19 @@ class MaskLMBatchGenerator(object):
 
   def trainDataLoader(self):
     """Pytorch dataloader that assembles all dataset files into a single-mapped dataset."""
-    dataset = self.torch.utils.data.ConcatDataset(
-                [self.torch.load(x) for x in self.dataset['train_dataset']['pt_record']]
+    dataset = torch.utils.data.ConcatDataset(
+                [torch.load(x) for x in self.dataset['train_dataset']['pt_record']]
               )
-    loader = self.torch.utils.data.dataloader.DataLoader(
-      dataset    = dataset,
-      batch_size = self.training_opts.batch_size,
-      sampler    = (
-            self.torch.utils.data.RandomSampler(dataset, replacement = False)
-            if not self.pytorch.torch_tpu_available or self.pytorch.torch_xla.xrt_world_size() <= 1
-            else self.torch.utils.data.distributed.DistributedSampler(
+    loader = torch.utils.data.dataloader.DataLoader(
+      dataset     = dataset,
+      batch_size  = self.training_opts.batch_size,
+      sampler     = (
+            torch.utils.data.RandomSampler(dataset, replacement = False)
+            if not pytorch.torch_tpu_available or pytorch.torch_xla.xrt_world_size() <= 1
+            else torch.utils.data.distributed.DistributedSampler(
                   dataset, 
-                  num_replicas = self.pytorch.torch_xla.xrt_world_size(), 
-                  rank = self.pytorch.torch_xla.get_ordinal()
+                  num_replicas = pytorch.torch_xla.xrt_world_size(), 
+                  rank = pytorch.torch_xla.get_ordinal()
                  )
             ),
       num_workers = os.cpu_count(),
@@ -690,7 +683,7 @@ class MaskLMBatchGenerator(object):
     extended_corpus   = np.repeat(corpus, max_dupe, axis = 0)
     remaining_corpus   = np.repeat(corpus, remaining, axis = 0)
 
-    l.getLogger().info("Estimated element size: {}. Dupe factor {} split into {} dupes of {} (plus {} remaining)".format(
+    l.getLogger().info("Estimated element size: {}. Dupe factor {} split into {} iterations of {} (plus {} remaining)".format(
         humanize.naturalsize(single_item_bytes), self.training_opts.dupe_factor, iterations, max_dupe, remaining
       )
     )
@@ -892,14 +885,14 @@ class MaskLMBatchGenerator(object):
       assert len(input_ids) == self.training_opts.sequence_length, "len(input_ids):  {}, sequence_length: {}".format(len(input_ids), self.training_opts.sequence_length)
 
       features = collections.OrderedDict()
-      features["seen_in_training"]     = self.torch.LongTensor([seen_in_training])
-      features["original_input"]       = self.torch.from_numpy(original_input)
-      features["input_ids"]            = self.torch.from_numpy(input_ids)
-      features["input_mask"]           = self.torch.from_numpy(input_mask)
-      features["position_ids"]         = self.torch.from_numpy(position_ids)
-      features["mask_labels"]          = self.torch.from_numpy(mask_labels)
-      features["masked_lm_lengths"]    = self.torch.from_numpy(masked_lm_lengths)
-      features["next_sentence_label"]  = self.torch.LongTensor([next_sentence_label])
+      features["seen_in_training"]     = torch.LongTensor([seen_in_training])
+      features["original_input"]       = torch.from_numpy(original_input)
+      features["input_ids"]            = torch.from_numpy(input_ids)
+      features["input_mask"]           = torch.from_numpy(input_mask)
+      features["position_ids"]         = torch.from_numpy(position_ids)
+      features["mask_labels"]          = torch.from_numpy(mask_labels)
+      features["masked_lm_lengths"]    = torch.from_numpy(masked_lm_lengths)
+      features["next_sentence_label"]  = torch.LongTensor([next_sentence_label])
       feature_set.append(features)
 
       if FLAGS.write_text_dataset:
@@ -914,7 +907,7 @@ class MaskLMBatchGenerator(object):
                                     next_sentence_label)
                             )
 
-    self.torch.save(feature_set, masked_corpus['pt_record'])
+    torch.save(feature_set, masked_corpus['pt_record'])
     if FLAGS.write_text_dataset:
       file_writer.close()
     l.getLogger().info("Wrote {} instances ({} batches of {} datapoints) to {}"
