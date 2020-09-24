526e14b25b7ceb724ca35a43193ab37533278b78
fivosts <fivos_ts@hotmail.com>
2020-09-24 21:12:48 +0300

    #98: Add json credentials to repo

---
+++ deeplearning/clgen/corpuses/bigQuery.py
@@ -0,0 +1,24 @@
+"""BigQuery API to fetch github data."""
+import os
+import pathlib
+
+from google.cloud import bigquery
+from eupy.native import logger as l
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+query = """
+    SELECT name, SUM(number) as total_people
+    FROM `bigquery-public-data.usa_names.usa_1910_2013`
+    WHERE state = 'TX'
+    GROUP BY name, state
+    ORDER BY total_people DESC
+    LIMIT 20
+"""
+query_job = client.query(query)  # Make an API request.
+
+print("The query data:")
+for row in query_job:
+    # Row values can be accessed by field name or index.
+    print("name={}, count={}".format(row[0], row["total_people"]))
