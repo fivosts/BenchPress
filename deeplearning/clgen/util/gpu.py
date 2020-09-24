f5d6c15d20b262307085fd9c4e39088c7eae872d
fivosts <fivos_ts@hotmail.com>
2020-09-24 15:36:55 +0300

    Add gpu selectors

--- deeplearning/clgen/util/gpu.py
+++ deeplearning/clgen/util/gpu.py
@@ -1,5 +1,49 @@
-from deeplearning.clgen.util.tf import tf
+"""Helper module for GPU system handling"""
+import os
+import subprocess
 
-def numGPUs():
-  devices = tf.python.client.device_lib.list_local_devices()
-  return len([x for x in devices if x.device_type == 'GPU'])
+from eupy.native import logger as l
+
+NVIDIA_SMI_GET_GPUS = "nvidia-smi --query-gpu=index,uuid,utilization.gpu,memory.total,memory.used,memory.free,driver_version,name,gpu_serial,display_active,display_mode,temperature.gpu --format=csv,noheader,nounits"
+
+def _to_float_or_inf(value: str):
+  """Util conversion string to float"""
+  try:
+    number = float(value)
+  except ValueError:
+    number = float("nan")
+  return number
+
+def getGPUs(smi_output):
+  """
+  Get all available GPU entries with information.
+  """
+  gpus = []
+  for line in smi_output:
+    if line.strip():
+      values = line.split(", ")
+      gpus.append({
+        'id' : values[0],
+        'uuid' : values[1],
+        'gpu_util'      : _to_float_or_inf(values[2]),
+        'mem_total'     : _to_float_or_inf(values[3]),
+        'mem_used'      : _to_float_or_inf(values[4]),
+        'mem_free'      : _to_float_or_inf(values[5]),
+        'driver'        : values[6],
+        'gpu_name'      : values[7],
+        'serial'        : values[8],
+        'display_active': values[9],
+        'display_mode'  : values[10],
+        'temp_gpu'      : _to_float_or_inf(values[11]),
+      })
+  return gpus
+
+def getGPUID():
+  """
+  Get GPU entries and select the one with the most memory available.
+  """
+  output = subprocess.check_output(NVIDIA_SMI_GET_GPUS.split())
+  gpus = getGPUs(output.decode("utf-8").split(os.linesep))
+  selected_gpu = sorted(gpus, key=lambda x: x['mem_used'])[0]
+  l.getLogger().info("Selected GPU:{} {}".format(selected_gpu['id'], selected_gpu['gpu_name']))
+  return int(selected_gpu['id'])
--- deeplearning/clgen/util/tf.py
+++ deeplearning/clgen/util/tf.py
@@ -2,6 +2,8 @@
 from absl import flags
 import os
 
+from deeplearning.clgen.util import gpu
+
 FLAGS = flags.FLAGS
 
 flags.DEFINE_boolean(
@@ -29,13 +31,39 @@ flags.DEFINE_boolean(
   therefore eager execution must be disabled."
 )
 
+flags.DEFINE_string(
+  "tf_device",
+  "gpu",
+  "Select device to deploy application. Valid options are 'cpu', 'gpu' and 'tpu'. [Default]: 'gpu'"
+  "If GPU unavailable, it rolls back to CPU."
+)
+
 import tensorflow
 tf = tensorflow
 
 def initTensorflow():
+  from eupy.native import logger as l
+
   tensorflow.python.util.deprecation._PRINT_DEPRECATION_WARNINGS = FLAGS.tf_print_deprecation
   os.environ['TF_CPP_MIN_LOG_LEVEL'] = str(FLAGS.tf_logging_level).lower()
   os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = str(FLAGS.tf_gpu_allow_growth).lower()
+
+  try:
+    if FLAGS.tf_device == "tpu":
+      raise NotImplementedError
+    elif FLAGS.tf_device == "gpu" and len(tensorflow.config.list_physical_devices('GPU')) > 0:
+      tensorflow.config.set_visible_devices(
+        tensorflow.config.list_physical_devices('GPU')[gpu.getGPUID()], 'GPU'
+      )
+    else:
+      l.getLogger().info("Selected CPU device.")
+      os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
+      tensorflow.config.set_visible_devices(
+        tensorflow.config.list_physical_devices('CPU'), 'CPU'
+      )
+  except RuntimeError as e:
+    raise e
+
   if FLAGS.tf_disable_eager:
     tensorflow.compat.v1.disable_eager_execution()
   tf = tensorflow
