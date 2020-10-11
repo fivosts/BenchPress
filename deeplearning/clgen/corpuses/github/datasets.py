696f760f8c1843fbc15502e0348db91be17a5a31
fivosts <fivos_ts@hotmail.com>
2020-10-12 01:30:11 +0300

    #98: Implement specific query signatures for CL

--- deeplearning/clgen/corpuses/github/datasets.py
+++ deeplearning/clgen/corpuses/github/datasets.py
@@ -37,18 +37,18 @@ class Dataset(object):
       self.query_file_id = " OR ".join(["substr(file.path, {}, {}) = '{}'".format(-len(ext), 1+len(ext), ext)
                               for ext in self.extensions
                         ])
-    self.file_count = 0
+    self.file_count = None
     return
 
   @property
   def filecount(self):
     """Return file count of represented query."""
-    if self.file_count == 0:
+    if self.file_count is None:
       return self.filecount_query()
     else:
       return self.file_count
 
-  def filecount_query(self) -> int:
+  def filecount_query(self) -> typing.Tuple[int, int]:
     """
     Queries the file count of files intended to query.
     Returns file count in int.
@@ -61,21 +61,20 @@ class Dataset(object):
     with bigquery.Client() as client:
       job = client.query(count_query)
       for f in job:
-        self.file_count = f[0]
+        self.file_count = (f[0], 0)
         return self.file_count
 
-  def repository_query(self, client) -> typing.Generator[int, bigquery.Query]:
+  def repository_query(self, client) -> typing.Tuple[typing.Callable]:
+    """Returns iterable of query files"""
     repo_query = """
     SELECT DISTINCT file.repo_name, file.ref
     FROM `bigquery-public-data.github_repos.files` as file
     {}
     """.format(not self.query_file_id or "WHERE " + self.query_file_id)
+    return client.query(repo_query,)
 
-    job = client.query(repo_query)
-    for en, row in enumerate(job):
-      yield en, row
-
-  def contentfile_query(self, client) -> typing.Generator[int, bigquery.Query]:
+  def contentfile_query(self, client) -> typing.Tuple[typing.Callable]:
+    """Returns iterable of query files"""
     contentfile_query = """
     SELECT file.repo_name, file.path, file.ref, file.mode, 
            file.id, file.symlink_target, contentfile.size, 
@@ -83,17 +82,53 @@ class Dataset(object):
     FROM `bigquery-public-data.github_repos.contents` as contentfile
     INNER JOIN `bigquery-public-data.github_repos.files` as file ON file.id = contentfile.id {}
     """.format(not self.query_file_id or "AND " + self.query_file_id)
-
-    job = client.query(contentfile_query)
-    for en, row in enumerate(job):
-      yield en, row
+    return client.query(contentfile_query,)
 
 class openclDataset(Dataset):
   """Opencl Dataset"""
   def __init__(self, data_format: int):
     self.extensions = ['.cl']
+    self.query_exception = [
+        "(substr(file.path, {}, {}) = '{}' AND contentfile.content LIKE '%kernel void%')"
+          .format(-len(ext), 1+len(ext), ext)
+      for ext in ['.c', '.cc', '.cpp', '.cxx', '.c++']
+    ]
     super(openclDataset, self).__init__(data_format, "opencl")
     return
+
+  def filecount_query(self) -> typing.Tuple[int, int]:
+    """
+    Queries the file count of files intended to query.
+    Returns file count in int.
+    """
+    super(openclDataset, self).filecount_query()
+    other_count_query = """
+    SELECT COUNT(*)
+    FROM `bigquery-public-data.github_repos.files` as file
+    {} {}
+    """.format(not self.query_file_id or "WHERE " + self.query_file_id, self.query_exception)
+
+    with bigquery.Client() as client:
+      job = client.query(other_count_query)
+      for f in job:
+        self.file_count[1] = f[0]
+        return self.file_count
+
+  def repository_query(self,
+                       client
+                       ) -> typing.Tuple[typing.Callable, typing.Callable]:
+    """
+    Query repositories that tested positive for having CL.
+    CL has its own function, because two types of files are checked:
+    '.cl' files and any C/C++ file that contains the keyword 'kernel void'
+    """
+    cl_file_it = super(openclDataset, self).repository_query(client)
+    other_repo_query = """
+    SELECT DISTINCT file.repo_name, file.ref
+    FROM `bigquery-public-data.github_repos.files` as file
+    {} {}
+    """.format(not self.query_file_id or "WHERE " + self.query_file_id, self.query_exception)
+    return (cl_file_it, client.query(repo_query))
 
 class cDataset(Dataset):
   """C Dataset"""
