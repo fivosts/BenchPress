3da8c4ab32edc8db11814115250c3b00cfb3a822
fivosts <fivos_ts@hotmail.com>
2020-10-11 19:13:45 +0300

    #98: Extend datasets for data format

--- deeplearning/clgen/corpuses/github/datasets.py
+++ deeplearning/clgen/corpuses/github/datasets.py
@@ -3,45 +3,22 @@ import os
 import pathlib
 import progressbar
 import humanize
-
 from google.cloud import bigquery
-from absl import flags
 
 from deeplearning.clgen.corpuses.github import bigQuery_database
 from eupy.native import logger as l
 
-FLAGS = flags.FLAGS
-
-# flags.DEFINE_boolean(
-#   "bq_only_repos",
-#   False,
-#   "Avoid huge queries, mine only repo entries for specified language."
-# )
-
-# flags.DEFINE_boolean(
-#   "bq_only_files",
-#   False,
-#   "Do not explicitly mine repository entries for contentfiles."
-# )
-
-# languages = {
-#   'opencl': ['.cl'],
-#   'c'     : ['.c'],
-#   'cpp'   : ['.cc', '.cpp', '.cxx', '.c++'],
-#   'java'  : ['.java'],
-#   'python': ['.py'],
-# }
-
 languages = {
-  'opencl': openclDataset,
-  'c'     : cDataset,
-  'cpp'   : cppDataset,
-  'java'  : javaDataset,
-  'python': pythonDataset,
+  'generic': Dataset,
+  'opencl' : openclDataset,
+  'c'      : cDataset,
+  'cpp'    : cppDataset,
+  'java'   : javaDataset,
+  'python' : pythonDataset,
 }
 
 class Dataset(object):
-  def __init__(self, dataset_id: str = None):
+  def __init__(self, data_format: int, dataset_id: str = None):
     self.config = bigquery.QueryJobConfig()
     self.config.allow_large_results = True
     self.client = bigquery.Client(default_query_job_config = config)
@@ -49,42 +26,42 @@ class Dataset(object):
     self.project_id = self.client.project_id
     self.dataset_id = "generic_github" if dataset_id is None else dataset_id
     self.dataset_id = "{}_github".format(dataset_id or "generic")
+
+    self.data_format = data_format
     return
 
   @classmethod
-  def FromArgs(cls, lang = None):
-    if lang is None:
-      return Dataset()
-    elif lang not in languages:
+  def FromArgs(cls, lang, data_format):
+    if lang not in languages:
       raise NotImplementedError(lang)
-    return languages[lang]()
+    return languages[lang](data_format)
 
 class openclDataset(Dataset):
-  def __init__(self):
-    super(openclDataset, self).__init__("opencl")
+  def __init__(self, data_format: int):
+    super(openclDataset, self).__init__(data_format, "opencl")
     self.extentions = ['.cl']
     return
 
 class cDataset(Dataset):
-  def __init__(self):
-    super(cDataset, self).__init__("c")
+  def __init__(self, data_format: int):
+    super(cDataset, self).__init__(data_format, "c")
     self.extentions = ['.c']
     return
 
 class cppDataset(Dataset):
-  def __init__(self):
-    super(cppDataset, self).__init__("cpp")
+  def __init__(self, data_format: int):
+    super(cppDataset, self).__init__(data_format, "cpp")
     self.extentions = ['.cc'. 'cpp', '.cxx', '.c++']
     return
 
 class javaDataset(Dataset):
-  def __init__(self):
-    super(javaDataset, self).__init__("java")
+  def __init__(self, data_format: int):
+    super(javaDataset, self).__init__(data_format, "java")
     self.extentions = ['.java']
     return
 
 class pythonDataset(Dataset):
-  def __init__(self):
-    super(pythonDataset, self).__init__("python")
+  def __init__(self, data_format: int):
+    super(pythonDataset, self).__init__(data_format, "python")
     self.extentions = ['.py']
     return
