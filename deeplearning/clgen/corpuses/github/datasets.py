24f789e8ce7371f7224f69fa84dbe58bb8d54a65
fivosts <fivos_ts@hotmail.com>
2020-10-12 14:54:31 +0300

    #98: Add client as constructor argument

--- deeplearning/clgen/corpuses/github/datasets.py
+++ deeplearning/clgen/corpuses/github/datasets.py
@@ -21,13 +21,22 @@ languages = {
 class Dataset(object):
   """Representation of dataset instance in Big Query"""
   @classmethod
-  def FromArgs(cls, lang, data_format):
+  def FromArgs(cls,
+               client: bigquery.Client,
+               lang: int,
+               data_format: str
+               ) -> Dataset:
     if lang not in languages:
       raise NotImplementedError(lang)
-    return languages[lang](data_format)
+    return languages[lang](client, data_format)
   
-  def __init__(self, data_format: int, dataset_id: str = None):
+  def __init__(self,
+               client: bigquery.Client,
+               data_format: int,
+               dataset_id: str = None
+               ):
 
+    self.client     = client
     self.dataset_id = "generic_github" if dataset_id is None else dataset_id
     self.dataset_id = "{}_github".format(dataset_id or "generic")
     self.data_format = data_format
@@ -58,22 +67,22 @@ class Dataset(object):
     FROM `bigquery-public-data.github_repos.files` as file
     {}
     """.format(not self.query_file_id or "WHERE " + self.query_file_id)
-    with bigquery.Client() as client:
-      job = client.query(count_query)
-      for f in job:
-        self.file_count = (f[0], 0)
-        return self.file_count
 
-  def repository_query(self, client) -> typing.Tuple[typing.Callable]:
+    job = self.client.query(count_query)
+    for f in job:
+      self.file_count = (f[0], 0)
+      return self.file_count
+
+  def repository_query(self) -> typing.Tuple[typing.Callable]:
     """Returns iterable of query files"""
     repo_query = """
     SELECT DISTINCT file.repo_name, file.ref
     FROM `bigquery-public-data.github_repos.files` as file
     {}
     """.format(not self.query_file_id or "WHERE " + self.query_file_id)
-    return client.query(repo_query,)
+    return self.client.query(repo_query,)
 
-  def contentfile_query(self, client) -> typing.Tuple[typing.Callable]:
+  def contentfile_query(self) -> typing.Tuple[typing.Callable]:
     """Returns iterable of query files"""
     contentfile_query = """
     SELECT file.repo_name, file.path, file.ref, file.mode, 
@@ -82,18 +91,22 @@ class Dataset(object):
     FROM `bigquery-public-data.github_repos.contents` as contentfile
     INNER JOIN `bigquery-public-data.github_repos.files` as file ON file.id = contentfile.id {}
     """.format(not self.query_file_id or "AND " + self.query_file_id)
-    return client.query(contentfile_query,)
+    return self.client.query(contentfile_query,)
 
 class openclDataset(Dataset):
   """Opencl Dataset"""
-  def __init__(self, data_format: int):
+  def __init__(self,
+               client: bigquery.Client,
+               data_format: int
+               ):
+
     self.extensions = ['.cl']
     self.query_exception = [
         "(substr(file.path, {}, {}) = '{}' AND contentfile.content LIKE '%kernel void%')"
           .format(-len(ext), 1+len(ext), ext)
       for ext in ['.c', '.cc', '.cpp', '.cxx', '.c++']
     ]
-    super(openclDataset, self).__init__(data_format, "opencl")
+    super(openclDataset, self).__init__(client, data_format, "opencl")
     return
 
   def filecount_query(self) -> typing.Tuple[int, int]:
@@ -108,37 +121,32 @@ class openclDataset(Dataset):
     {} {}
     """.format(not self.query_file_id or "WHERE " + self.query_file_id, self.query_exception)
 
-    with bigquery.Client() as client:
-      job = client.query(other_count_query)
-      for f in job:
-        self.file_count[1] = f[0]
-        return self.file_count
+    job = self.client.query(other_count_query)
+    for f in job:
+      self.file_count[1] = f[0]
+      return self.file_count
 
-  def repository_query(self,
-                       client
-                       ) -> typing.Tuple[typing.Callable, typing.Callable]:
+  def repository_query(self) -> typing.Tuple[typing.Callable, typing.Callable]:
     """
     Query repositories that tested positive for having CL.
     CL has its own function, because two types of files are checked:
     '.cl' files and any C/C++ file that contains the keyword 'kernel void'
     """
-    cl_repo_it = super(openclDataset, self).repository_query(client)
+    cl_repo_it = super(openclDataset, self).repository_query()
     other_repo_query = """
     SELECT DISTINCT file.repo_name, file.ref
     FROM `bigquery-public-data.github_repos.files` as file
     {} {}
     """.format(not self.query_file_id or "WHERE " + self.query_file_id, self.query_exception)
-    return (cl_repo_it, client.query(other_repo_query))
+    return (cl_repo_it, self.client.query(other_repo_query))
 
-  def contentfile_query(self,
-                        client,
-                        ) -> typing.Tuple[typing.Callable, typing.Callable]:
+  def contentfile_query(self) -> typing.Tuple[typing.Callable, typing.Callable]:
     """
     Query contentfiles that tested positive for being CL.
     CL has its own function, because two types of files are checked:
     '.cl' files and any C/C++ file that contains the keyword 'kernel void'
     """
-    cl_file_it = super(openclDataset, self).contentfile_query(client)
+    cl_file_it = super(openclDataset, self).contentfile_query()
     other_file_query = """
     SELECT file.repo_name, file.path, file.ref, file.mode, 
            file.id, file.symlink_target, contentfile.size, 
@@ -146,32 +154,44 @@ class openclDataset(Dataset):
     FROM `bigquery-public-data.github_repos.files` as file
     {} {}
     """.format(not self.query_file_id or "WHERE " + self.query_file_id, self.query_exception)
-    return (cl_file_it, client.query(other_file_query))
+    return (cl_file_it, self.client.query(other_file_query))
 
 class cDataset(Dataset):
   """C Dataset"""
-  def __init__(self, data_format: int):
+  def __init__(self,
+               client: bigquery.Client,
+               data_format: int
+               ):
     self.extensions = ['.c']
-    super(cDataset, self).__init__(data_format, "c")
+    super(cDataset, self).__init__(client, data_format, "c")
     return
 
 class cppDataset(Dataset):
   """C++ Dataset"""
-  def __init__(self, data_format: int):
+  def __init__(self,
+               client: bigquery.Client,
+               data_format: int
+               ):
     self.extensions = ['.cc'. 'cpp', '.cxx', '.c++']
-    super(cppDataset, self).__init__(data_format, "cpp")
+    super(cppDataset, self).__init__(client, data_format, "cpp")
     return
 
 class javaDataset(Dataset):
   """java Dataset"""
-  def __init__(self, data_format: int):
+  def __init__(self,
+               client: bigquery.Client,
+               data_format: int
+               ):
     self.extensions = ['.java']
-    super(javaDataset, self).__init__(data_format, "java")
+    super(javaDataset, self).__init__(client, data_format, "java")
     return
 
 class pythonDataset(Dataset):
   """python Dataset"""
-  def __init__(self, data_format: int):
+  def __init__(self,
+               client: bigquery.Client,
+               data_format: int
+               ):
     self.extensions = ['.py']
-    super(pythonDataset, self).__init__(data_format, "python")
+    super(pythonDataset, self).__init__(client, data_format, "python")
     return
