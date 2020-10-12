1bf21c3c38d30a7a3bfb1e8447a918ca576c6ef0
fivosts <fivos_ts@hotmail.com>
2020-10-12 22:08:13 +0300

    Finish miner fetch

--- deeplearning/clgen/corpuses/github/big_query.py
+++ deeplearning/clgen/corpuses/github/big_query.py
@@ -33,17 +33,22 @@ class BigQuery(miner.GithubMiner):
     # Get repository list of requested file specifications.
     mainrep_it, otherrep_it = self.dataset.repository_query()
     if mainrep_it is not None:
+      main_repo_count = 0
       for en, mr in enumerate(mainrep_it):
         self.storage.save(bigQuery_database.bqRepo(
             **bigQuery_database.bqRepo.FromArgs(en, mr)
           )
         )
+        main_repo_count = en
+
     if otherrep_it is not None:
+      other_repo_count = 0
       for en, orep in enumerate(otherrep_it):
         self.storage.save(bigQuery_database.bqRepo(
             **bigQuery_database.bqRepo.FromArgs(en, orep)
           )
         )
+        other_repo_count = en
 
     # Get contentfiles.
     mainf_it, otherf_it = self.dataset.contentfile_query()
@@ -60,36 +65,35 @@ class BigQuery(miner.GithubMiner):
           )
         )
 
-    url = "sqlite:///{}{}".format(self.cache_path, "bqcorpus_{}.db".format(lang))
-    db = bigQuery_database.bqDatabase(url)
-    with db.Session(commit = True) as session, progressbar.ProgressBar(max_value = file_count) as bar:
-      with progressbar.ProgressBar(max_value = file_count) as bar:
-        for en,row in enumerate(file_job):
-          contentfile = bigQuery_database.bqFile(
-            **bigQuery_database.bqFile.FromArgs(en, row)
-          )
-          session.add(contentfile)
-          bar.update(en)
-      with progressbar.ProgressBar(max_value = repo_count) as bar:
-        for en, row in enumerate(repo_job):
-          repofile = bigQuery_database.bqRepo(
-            **bigQuery_database.bqRepo.FromArgs(en, row)
-          )
-          session.add(repofile)
-          bar.update(en)
-      r = [
-        "total_contentfiles: {}".format(db.file_count),
-        "total_repositories: {}".format(db.repo_count),
-      ]
-      exists = session.query(
-          bigQuery_database.bqData.key
-        ).filter_by(key = lang).scalar() is not None
-      if exists:
-        entry = session.query(
-            bigQuery_database.bqData
-          ).filter_by(key = lang).first()
-        entry.value = "\n".join(r)
-      else:
-        session.add(
-          bigQuery_database.bqData(key = lang, value = "\n".join(r))
-        )
+    query_data = [
+      "main_contentfiles : {}".format(mainfile_count),
+      "other_contentfiles: {}".format(otherfile_count),
+      "total_contentfiles: {}".format(mainfile_count + otherfile_count),
+      "main_repositories : {}".format(0 if not mainrep_it else main_repo_count),
+      "other_repositories: {}".format(0 if not otherrep_it else other_repo_count),
+      "total_repositories: {}".format(0 if not (mainrep_it or otherrep_it) else main_repo_count + other_repo_count),
+    ]
+    self.storage.save(bigQuery_database.bqData(key = self.dataset.name, value = '\n'.join(r)))
+    return
+
+    # url = "sqlite:///{}{}".format(self.cache_path, "bqcorpus_{}.db".format(lang))
+    # db = bigQuery_database.bqDatabase(url)
+    # with db.Session(commit = True) as session, progressbar.ProgressBar(max_value = file_count) as bar:
+    #   with progressbar.ProgressBar(max_value = file_count) as bar:
+
+    #   r = [
+    #     "total_contentfiles: {}".format(db.file_count),
+    #     "total_repositories: {}".format(db.repo_count),
+    #   ]
+    #   exists = session.query(
+    #       bigQuery_database.bqData.key
+    #     ).filter_by(key = lang).scalar() is not None
+    #   if exists:
+    #     entry = session.query(
+    #         bigQuery_database.bqData
+    #       ).filter_by(key = lang).first()
+    #     entry.value = "\n".join(r)
+    #   else:
+    #     session.add(
+    #       bigQuery_database.bqData(key = lang, value = "\n".join(r))
+    #     )
