0fdb615d071046be4ff621d24deae33220aa6d9f
fivosts <fivos_ts@hotmail.com>
2020-10-17 01:11:58 +0300

    Distinct duplicates by sha

--- deeplearning/clgen/corpuses/github/storage.py
+++ deeplearning/clgen/corpuses/github/storage.py
@@ -340,7 +340,11 @@ class dbStorage(Storage):
         if not exists:
           session.add(contentfile)
         if isinstance(contentfile, bigQuery_database.bqFile):
-          session.add(contentfile)
+          exists = session.query(
+            bigQuery_database.bqFile.sha256
+          ).filter_by(sha256 = contentfile.sha256).scalar() is not None
+          if not exists:
+            session.add(contentfile)
     return
 
 class bqStorage(Storage):
