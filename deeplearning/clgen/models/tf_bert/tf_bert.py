b01244cac778dfc3d0c9201bdfca02daccd7e0ac
fivosts <fivos_ts@hotmail.com>
2020-11-02 21:57:37 +0200

    #50: Upgrade sample message in tf_bert

--- deeplearning/clgen/models/tf_bert/tf_bert.py
+++ deeplearning/clgen/models/tf_bert/tf_bert.py
@@ -360,6 +360,7 @@ class tfBert(backends.BackendBase):
                   sample_indices         = '\n'.join([self.atomizer.DeatomizeIndices(mind).replace('\n', '\\n') for mind in sind]),
                   encoded_sample_indices = '\n'.join([','.join([str(x) for x in mind]) for mind in sind ]),
                   sample_time_ms         = int(round(1000 * ((end_time - start_time) / sampler.batch_size).total_seconds())),
+                  feature_vector         = self.atomizer.DeatomizeIndices(sample, ignore_token = self.atomizer.padToken).replace("\\n", "\n"),
                   num_tokens             = len(sample),
                   compile_status         = compile_flag,
                   categorical_sampling   = self.samplesWithCategorical(),
