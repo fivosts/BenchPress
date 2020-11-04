fc4299e8fd9b0e2267ebda3488a2dafa34b4fd13
fivosts <fivos_ts@hotmail.com>
2020-11-04 18:35:57 +0200

    Add pv to requirements for extracting progressbar

--- deeplearning/clgen/corpuses/corpuses.py
+++ deeplearning/clgen/corpuses/corpuses.py
@@ -598,7 +598,7 @@ def GetHashOfArchiveContents(archive: pathlib.Path) -> str:
     raise ValueError(f"Archive not found: '{archive}'")
 
   with tempfile.TemporaryDirectory(prefix="clgen_corpus_") as d:
-    cmd = ["tar", "-xf", str(archive), "-C", d]
+    cmd = ["pv", str(archive), "|", "tar", "xfj", "-" "-C", d]
     try:
       subprocess.check_call(cmd)
     except subprocess.CalledProcessError:
