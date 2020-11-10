b1e7d1fc5de25e5ca1ca0b6775aa6cda119c6af3
fivosts <fivos_ts@hotmail.com>
2020-11-10 20:19:35 +0200

    Remove duplicate code in active sampler

--- deeplearning/clgen/features/active_generator.py
+++ deeplearning/clgen/features/active_generator.py
@@ -10,7 +10,7 @@ from deeplearning.clgen.util import distributions
 
 from eupy.native import logger as l
 
-class ActiveSamplingGenerator(lm_data_generator.MaskLMDataGenerator):
+class ActiveSamplingGenerator(online_generator.OnlineSamplingGenerator):
   """
   Data generation object that performs active learning
   on sampling process.
@@ -23,36 +23,21 @@ class ActiveSamplingGenerator(lm_data_generator.MaskLMDataGenerator):
   @classmethod
   def FromDataGenerator(cls,
                         generator: lm_data_generator.MaskLMDataGenerator,
-                        ) -> "active_generator.ActiveSamplingGenerator":
+                        ) -> "active_generator.OnlineSamplingGenerator":
     """Initializes data generator for active sampling."""
-    d = ActiveSamplingGenerator()
-
-    d.data_generator = generator
-    d.sampler        = d.data_generator.sampler
-    d.atomizer       = d.data_generator.atomizer
-
-    d.configSamplingParams()
-    d.configSampleCorpus()
-
-    d.dataloader = d.sample_dataloader()
+    d = ActiveSamplingGenerator(generator)
+    d.dataloader = d.active_dataloader()
     return d
 
-  def __init__(self):
-    self.data_generator = None
-
-    # Wrapped data generator attributes
-    self.sampler       = None
-    self.atomizer      = None
-    self.sample_corpus = None
-
-    # Inherent attributes
-    self.distribution  = None
-    self.masking_func  = None
-    self.dataloader    = None
-    self.feed_stack    = None
+  def __init__(self,
+               generator: lm_data_generator.MaskLMDataGenerator
+               ):
+    super(ActiveSamplingGenerator, self).__init__(generator)
+    # Active sampling attributes.
+    self.feed_stack = None
     return
 
-  def sample_dataloader(self) -> typing.Union[
+  def active_dataloader(self) -> typing.Union[
                                    typing.Dict[str, typing.TypeVar("Tensor")],
                                    typing.NamedTuple
                                  ]:
@@ -63,11 +48,11 @@ class ActiveSamplingGenerator(lm_data_generator.MaskLMDataGenerator):
     In torch, Dict[str, np.array] instances are generated.
     masking_func output goes through TensorFormat to convert np arrays to relevant tensors.
     """
-    for seed in self.sample_corpus:
-      sample_feed, hole_lengths, masked_idxs = self.masking_func(seed)
+    for seed in self.online_corpus:
+      input_feed, hole_lengths, masked_idxs = self.masking_func(seed)
       # TODO do sth with hole_lengths and masked_idxs
       self.feed_stack.append(seed)
-      yield self.data_generator.toTensorFormat(sample_feed)
+      yield self.data_generator.toTensorFormat(input_feed)
 
   def EvaluateFromFeatures(self,
                            samples: np.array,
@@ -99,53 +84,3 @@ class ActiveSamplingGenerator(lm_data_generator.MaskLMDataGenerator):
       sample_feed, hole_lengths, masked_idxs = self.masking_func(latest_seed)
       # TODO do sth with the hole_lengths and masked_idxs
       return sample_feed, False
-
-  def configSampleCorpus(self) -> None:
-    """
-    Configure sampling corpus container to iterate upon.
-    """
-    if self.sampler.isFixedStr:
-      if (self.atomizer.maskToken in self.sampler.encoded_start_text or
-          self.atomizer.holeToken in self.sampler.encoded_start_text):
-        raise ValueError("{} targets found in active sampler start text. This is wrong. Active sampler masks a sequence on the fly...")
-      self.sample_corpus = [self.sampler.encoded_start_text]
-    else:
-      self.sample_corpus = self.data_generator.createCorpus(self.sampler.corpus_directory)
-    return
-
-  def configSamplingParams(self) -> None:
-    """
-    Configure masking function used by active sampler.
-    """
-    class SampleTrainingOpts(typing.NamedTuple):
-      max_predictions_per_seq: int
-      masked_lm_prob: float
-
-    corpus_config = self.sampler.config.sample_corpus.corpus_config
-    sampling_opts = SampleTrainingOpts(
-      self.data_generator.training_opts.max_predictions_per_seq, corpus_config.masked_lm_prob
-    )
-
-    if corpus_config.HasField("hole"):
-      self.distribution = distributions.Distribution.FromHoleConfig(
-        corpus_config.hole, self.sampler.corpus_directory, "sample_corpus"
-      )
-      self.masking_func = functools.partial(sequence_masking.HoleSequence,
-                            train_set            = False,
-                            max_predictions      = corpus_config.max_predictions_per_seq,
-                            pickled_distribution = pickle.dumps(self.distribution),
-                            pickled_atomizer     = pickle.dumps(self.atomizer),
-                            training_opts        = sampling_opts,
-                            is_torch             = self.data_generator.is_torch,
-                          )
-    elif corpus_config.HasField("mask"):
-      self.masking_func = functools.partial(sequence_masking.MaskSequence,
-                            train_set          = False,
-                            max_predictions    = corpus_config.max_predictions_per_seq,
-                            config             = corpus_config,
-                            pickled_atomizer   = pickle.dumps(self.atomizer),
-                            training_opts      = sampling_opts,
-                            rngen              = self.data_generator.rngen,
-                            is_torch           = self.data_generator.is_torch,
-                          )
-    return
