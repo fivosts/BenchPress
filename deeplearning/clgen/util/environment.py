8b88171aede6b88faeba3f87051c6e74960721f8
fivosts <fivos_ts@hotmail.com>
2020-11-14 18:05:35 +0200

    Bug fix

--- deeplearning/clgen/util/environment.py
+++ deeplearning/clgen/util/environment.py
@@ -7,8 +7,8 @@ def check_exists(path):
   return path
 
 try:
+  LLVM_VERSION        = os.environ['LLVM_VERSION']
   LLVM                = check_exists(os.environ['LLVM'])
-  LLVM_VERSION        = check_exists(os.environ['LLVM_VERSION'])
   LLVM_LIB            = check_exists(os.environ['LLVM_LIB'])
   LIBCXX_HEADERS      = check_exists(os.environ['LIBCXX_HEADERS'])
   CLANG               = check_exists(os.environ['CLANG'])
