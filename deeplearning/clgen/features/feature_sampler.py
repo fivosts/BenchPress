cb78ca6e4f82a1c40936607089b5bc67d969a98a
fivosts <fivos_ts@hotmail.com>
2020-11-20 00:44:22 +0200

    Start benchmark feature sampler

--- deeplearning/clgen/features/feature_sampler.py
+++ deeplearning/clgen/features/feature_sampler.py
@@ -34,3 +34,33 @@ def is_kernel_smaller(input_feature: typing.Dict[str, float],
           input_feature['comp']      + input_feature['rational'] +
           input_feature['mem']       + input_feature['localmem'] +
           input_feature['coalesced'] + input_feature['atomic'])
+
+class FeatureSampler(object):
+  """
+  This is a shitty experimental class to work with benchmark comparison.
+  Will be refactored obviously.
+  """
+  class Benchmark(typing.NamedTuple):
+    path: pathlib.Path
+    name: str
+    contents: str
+    feature_vector: typing.Dict[str, float]
+    times_achieved: int
+
+  def __init__(self):
+    self.path = pathlib.Path("/home/fivosts/rodinia_benchmarks")
+    self.benchmarks = []
+    for f in self.path.iterdir():
+      with open(f, 'r') as file:
+        contents = file.readlines()
+        self.benchmarks.append(
+          Benchmark(
+            f,
+            f.name,
+            contents,
+            extractor.StrToDictFeatures(extractor.kernel_features(contents)),
+            0
+          )
+        )
+
+  def 
