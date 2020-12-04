773d989b37f008b1a2c43a74519762b04e5a6bcc
fivosts <fivos_ts@hotmail.com>
2020-12-04 21:00:52 +0200

    Remove dummy functionns

--- deeplearning/clgen/features/feature_sampler.py
+++ deeplearning/clgen/features/feature_sampler.py
@@ -8,36 +8,6 @@ from deeplearning.clgen.features import extractor
 
 from eupy.native import logger as l
 
-def is_kernel_bigger(input_feature: typing.Dict[str, float],
-                     sample_feature: typing.Dict[str, float],
-                     ) -> bool:
-  """
-  Checks if sample kernel is larger than original feed
-  by comparing all numerical features.
-  """
-  return (sample_feature['comp']      + sample_feature['rational'] +
-          sample_feature['mem']       + sample_feature['localmem'] +
-          sample_feature['coalesced'] + sample_feature['atomic']
-          > 
-          input_feature['comp']      + input_feature['rational'] +
-          input_feature['mem']       + input_feature['localmem'] +
-          input_feature['coalesced'] + input_feature['atomic'])
-
-def is_kernel_smaller(input_feature: typing.Dict[str, float],
-                      sample_feature: typing.Dict[str, float],
-                      ) -> bool:
-  """
-  Checks if sample kernel is smaller than original feed
-  by comparing all numerical features.
-  """
-  return (sample_feature['comp']      + sample_feature['rational'] +
-          sample_feature['mem']       + sample_feature['localmem'] +
-          sample_feature['coalesced'] + sample_feature['atomic']
-          < 
-          input_feature['comp']      + input_feature['rational'] +
-          input_feature['mem']       + input_feature['localmem'] +
-          input_feature['coalesced'] + input_feature['atomic'])
-
 class FeatureSampler(object):
   """
   This is a shitty experimental class to work with benchmark comparison.
