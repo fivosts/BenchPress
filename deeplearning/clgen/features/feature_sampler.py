87e3fcfb437a1517a1bb263d7d7d97183c4fc764
fivosts <fivos_ts@hotmail.com>
2020-12-10 23:21:26 +0200

    Fix euclidean distance sampler

--- deeplearning/clgen/features/feature_sampler.py
+++ deeplearning/clgen/features/feature_sampler.py
@@ -3,12 +3,12 @@ Feature space sampling of source code.
 """
 import typing
 import pathlib
+import math
 
 from deeplearning.clgen.features import extractor
-
 from eupy.native import logger as l
 
-class FeatureSampler(object):
+class EuclideanSampler(object):
   """
   This is a shitty experimental class to work with benchmark comparison.
   Will be refactored obviously.
@@ -38,28 +38,34 @@ class FeatureSampler(object):
             )
           )
 
-  def calculate_proximity(self, infeat, tarfeat):
-    try:
-      return abs((tarfeat - infeat) / tarfeat)
-    except ZeroDivisionError:
-      return abs( ((tarfeat + 0.01) - infeat) / (tarfeat + 0.01))
+  def calculate_distance(self, infeat) -> float:
+    """
+    Comparing all benchmarks, return the minimum euclidan distance
+    of the input feature vector, among any of the target feature vector.
+    """
+    d = 0
+    for b in self.benchmarks:
+      cd = 0
+      for i, t in zip(infeat.values(), b.feature_vector.values()):
+        cd += (t**2) - (i**2)
+      d = min(d, cd)
+    return math.sqrt(d)
 
-  def sample_from_set(self, input_feature: typing.Dict[str, float]) -> bool:
-    min_proximity = None
-    min_idx = None
-    for idx, benchmark in enumerate(self.benchmarks):
-      proximity_vector = [self.calculate_proximity(infeat, tarfeat) for (_, infeat), (_, tarfeat) in zip(input_feature.items(), benchmark.feature_vector.items())]
-      avg_proximity = sum(proximity_vector) / len(proximity_vector)
-      if min_proximity is None:
-        min_proximity = avg_proximity
-        min_idx = idx
-      else:
-        if avg_proximity < min_proximity:
-          min_proximity = avg_proximity
-          min_idx = idx
+  def topK_candidates(self,
+                      candidates: typing.List[typing.TypeVar("ActiveSample")],
+                      K : int,
+                      ) -> typing.List[typing.TypeVar("ActiveSample")]:
+    return sorted(candidates, key = lambda x: x.score)[:K+1]
 
-    threshold = 0.4
-    if min_proximity <= threshold:
-      self.benchmarks[min_idx] = self.benchmarks[min_idx]._replace(times_achieved = 1 + self.benchmarks[min_idx].times_achieved)
-      return True
-    return False
+  def sample_from_set(self, 
+                      candidates: typing.List[typing.TypeVar("ActiveSample")],
+                      ) -> bool:
+    """
+    Find top K candidates by getting minimum
+    euclidean distance from set of rodinia benchmarks.
+    """
+    for idx in len(candidates):
+      candidates[idx] = candidates[idx]._replace(
+        score = self.calculate_distance(candidates[idx].features)
+      )
+    return self.topK_candidates(candidates, 5)
