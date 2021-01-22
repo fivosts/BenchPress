b52e0b7d018726743b9dee4112155d41285cc7a8
fivosts <fivos_ts@hotmail.com>
2021-01-22 19:36:26 +0200

    Add draft function of auxiliary header file gathering

--- deeplearning/clgen/preprocessors/common.py
+++ deeplearning/clgen/preprocessors/common.py
@@ -14,12 +14,15 @@
 # along with clgen.  If not, see <https://www.gnu.org/licenses/>.
 """Common preprocessor passes."""
 import typing
+# import pathlib
+# from absl import flags
 
 from deeplearning.clgen.preprocessors import public
-from absl import flags
-from eupy.native import logger as l
-FLAGS = flags.FLAGS
+# from deeplearning.clgen.util import crypto
 
+from eupy.native import logger as l
+
+# FLAGS = flags.FLAGS
 
 def _MinimumLineCount(text: str, min_line_count: int) -> str:
   """Private implementation of minimum number of lines.
@@ -205,3 +208,72 @@ def ExtractOnlySingleKernels(text: str) -> typing.List[str]:
       # Add to kernels all global space met so far + 'kernel void' + the kernel's body
       actual_kernels.append(kernel_specifier + chunk[:chunk_idx])
   return actual_kernels
+
+# @public.clgen_preprocessor
+# def ExtractOnlySingleKernelsWithHeaders(text: str) -> typing.List[str]:
+#   """
+#   A preprocessor that splits a single source file to discrete kernels
+#   along with their potential global declarations.
+
+#   Args:
+#     text: The text to preprocess.
+
+#   Returns:
+#     List of kernels (strings).
+#   """
+#   # OpenCL kernels can only be void
+#   kernel_specifier = 'kernel void'
+#   kernel_chunks = text.split(kernel_specifier)
+#   actual_kernels, global_space = [], []
+
+#   for idx, chunk in enumerate(kernel_chunks):
+#     if idx == 0:
+#       # There is no way the left-most part is not empty or global
+#       if chunk != '':
+#         global_space.append(chunk)
+#     else:
+#       # Given this preprocessor is called after compile,
+#       # we are certain that brackets will be paired
+#       num_lbrack, num_rbrack, chunk_idx = 0, 0, 0
+#       while ((num_lbrack  == 0 
+#       or      num_lbrack  != num_rbrack)
+#       and     chunk_idx   <  len(chunk)):
+
+#         try:
+#           cur_tok = chunk[chunk_idx]
+#         except IndexError:
+#           l.getLogger().warn(chunk)
+#         if   cur_tok == "{":
+#           num_lbrack += 1
+#         elif cur_tok == "}":
+#           num_rbrack += 1
+#         chunk_idx += 1
+
+#       while chunk_idx < len(chunk):
+#         # Without this line, global_space tends to gather lots of newlines and wspaces
+#         # Then they are replicated and become massive. Better isolate only actual text there.
+#         if chunk[chunk_idx] == ' ' or chunk[chunk_idx] == '\n':
+#           chunk_idx += 1
+#         else:
+#           break
+
+#       # Add to kernels all global space met so far + 'kernel void' + the kernel's body
+#       actual_kernels.append(kernel_specifier + chunk[:chunk_idx])
+#       if ''.join(chunk[chunk_idx:]) != '':
+#         # All the rest below are appended to global_space
+#         global_space.append(chunk[chunk_idx:])
+
+#   print(global_space)
+#   if global_space:
+#     sha = crypto.sha256_str(''.join(global_space).replace('\n', '').replace(' ', ''))
+#     header_path = pathlib.Path(FLAGS.workspace_dir).resolve() / "header_files"
+#     header_path.mkdir(parents = True, exist_ok = True)
+#     print("HA")
+#     if not (header_path / "{}.h".format(sha)).exists():
+#       with open(header_path / "{}.h".format(sha), 'w') as f:
+#         f.write(''.join(global_space))
+#     else:
+#       print("TSAPOU")
+#   else:
+#     print("EMPTY")
+#   return actual_kernels
