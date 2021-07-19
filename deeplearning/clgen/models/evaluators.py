58f4f895c3dbf191adbcae7973f9d174825afe97
fivosts <fivos_ts@hotmail.com>
2021-07-20 01:40:30 +0300

    Implement eval function for evaluators

--- deeplearning/clgen/models/evaluators.py
+++ deeplearning/clgen/models/evaluators.py
@@ -2,6 +2,7 @@ import typing
 import pathlib
 
 from deeplearning.clgen.samplers import samplers
+from deeplearning.clgen.samplers import samples_database
 from deeplearning.clgen.features import feature_sampler
 from deeplearning.clgen.features import extractor
 from deeplearning.clgen.preprocessors import opencl
@@ -20,9 +21,11 @@ flags.DEFINE_string(
 )
 
 class BaseEvaluator(object):
+  """
+  Base class for evaluators.
+  """
   def __init__(self, sampler: samplers.Sampler):
     self.sampler = sampler
-    self.path = 
     return
 
   def eval(self) -> None:
@@ -65,24 +68,38 @@ class BenchmarkDistance(object):
       self.clgen_cands    = clgen_cands
       self.github_cands   = github_cands
 
-  def __init__(self, github_corpus: corpuses.Corpus, sampler: samplers.Sampler):
+  def __init__(self,
+               github_corpus : corpuses.Corpus,
+               samples_db    : samples_database.SamplesDatabase,
+               sampler       : samplers.Sampler
+               ) -> None:
     super(BenchmarkDistance, self).__init__(sampler)
-    self.target        = self.sampler.sample_corpus.corpus_config.active.target
 
-    self.target_path   = pathlib.Path(feature_sampler.targets[self.target]).resolve()
+    # Target and path to target benchmarks
+    self.target      = self.sampler.sample_corpus.corpus_config.active.target
+    self.target_path = pathlib.Path(feature_sampler.targets[self.target]).resolve()
+
+    # BERT DB setup
+    self.bert_db = samples_db
+
+    # clgen DB setup
     self.clgen_samples_path = pathlib.Path(FLAGS.clgen_samples_path)
     if not self.clgen_samples_path.exists():
       raise FileNotFoundError
+    self.clgen_db = samples_database.SamplesDatabase("sqlite:///{}".format(str(self.clgen_samples_path)))
 
-    self.feature_space = self.sampler.sample_corpus.corpus_config.active.feature_space
     self.github_corpus = github_corpus
-
+    # Feature Space setup
+    self.feature_space = self.sampler.sample_corpus.corpus_config.active.feature_space
     loadBenchmarks()
     return
 
   def loadBenchmarks(self) -> None:
+    """
+    Unzip benchmarks zip, iterate, split and collect features for a feature space.
+    """
     self.evaluated_benchmarks = []
-    with feature_sampler.GetContentFileRoot(self.path) as root:
+    with feature_sampler.GetContentFileRoot(self.target_path) as root:
       contentfiles = []
       for file in root.iterdir():
         with open(file, 'r') as inf:
@@ -101,13 +118,24 @@ class BenchmarkDistance(object):
         )
     return
 
-  def eval(self) -> None:
+  def eval(self, topK: int) -> None:
+    """
+    Iterate benchmarks and evaluate datasets efficacy.
+    """
+    bert_corpus  = [
+      (cf, feats[self.feature_space])
+      for cf, feats in self.sampler.
+    ]
+    clgen_corpus = [
+      (cf, feats[self.feature_space])
+      for cf, feats in self.clgen_db.get_samples_features
+    ]
+    git_corpus   = [
+      (cf, feats[self.feature_space])
+      for cf, feats in self.github_corpus.getFeaturesContents(self.feature_space)
+    ]
     for benchmark in self.evaluated_benchmarks:
-      # gather top-K berts, run cldrive for each
-      # gather top-K clgen's, run cldrive for each
-      
-      # gather top-K github's, run cldrive for each
-      git = self.github_corpus.getFeaturesContents(self.feature_space)
-      # plot stuff + write in json
-      pass
+      bert_corpus  = sorted([(cf, feature_sampler.calculate_distance(fts)) for cf, fts in bert_corpus ], key = lambda x: x[1])[:topK]
+      clgen_corpus = sorted([(cf, feature_sampler.calculate_distance(fts)) for cf, fts in clgen_corpus], key = lambda x: x[1])[:topK]
+      git_corpus   = sorted([(cf, feature_sampler.calculate_distance(fts)) for cf, fts in git_corpus  ], key = lambda x: x[1])[:topK]
     return
