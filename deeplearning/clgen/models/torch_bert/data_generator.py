638d5f113a618950d3448ccddad65f9f5f070749
fivosts <fivos_ts@hotmail.com>
2021-09-03 15:10:43 +0300

    Bug fix

--- deeplearning/clgen/models/torch_bert/data_generator.py
+++ deeplearning/clgen/models/torch_bert/data_generator.py
@@ -398,8 +398,8 @@ class torchLMDataGenerator(lm_data_generator.MaskLMDataGenerator):
           # Post-process outputs.
           step_candidates = [] # TEMP add
           # tcs, ts = self.registerOutputData(outputs, feed, step_candidates, bar)
-          # bar = progressbar.ProgressBar(max_value = rem * self.sample_batch_size)
-          bar = tqdm.auto.trange(rem * self.sample_batch_size, desc="Register Outputs", leave = False, position = 0)
+          bar = progressbar.ProgressBar(max_value = rem * self.sample_batch_size)
+          # bar = tqdm.auto.trange(rem * self.sample_batch_size, desc="Register Outputs", leave = False, position = 0)
           bar.update(0)
           (tcs, ts), better_found = self.registerOutputData(outputs, feed, step_candidates, bar)
           cmp_rate[0] += tcs
