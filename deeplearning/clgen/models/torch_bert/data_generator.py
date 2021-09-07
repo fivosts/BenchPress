e8b798c4905e1b8f5fd310410e1b17e4f51d7856
fivosts <fivos_ts@hotmail.com>
2021-09-07 19:55:28 +0300

    Fixed bugs on batch_per_feed

--- deeplearning/clgen/models/torch_bert/data_generator.py
+++ deeplearning/clgen/models/torch_bert/data_generator.py
@@ -112,7 +112,7 @@ def dataload_worker(x              : int,
                     batch_per_feed : int,
                     ) -> typing.Dict[str, torch.Tensor]:
   try:
-    return [func(feed) for _ in range(batch * batch_per_feed)]
+    return [f for _ in range(batch // batch_per_feed) for f in [func(feed)] * batch_per_feed]
   except Exception:
     return None
 
@@ -400,7 +400,7 @@ class torchLMDataGenerator(lm_data_generator.MaskLMDataGenerator):
         better_found = None
         while not better_found and cmp_rate[1] < 160000: # TEMP add
           # Pre-process inputs
-          rem = 4096 // self.sample_batch_size # TEMP add
+          rem = 8192 // self.sample_batch_size # TEMP add
           inputs = self.collateInputData(feed.input_feed, rem, sample_batch_per_feed)
           # Infer
           outputs, time = mwrapper.sample_model_step(
@@ -583,12 +583,12 @@ class torchLMDataGenerator(lm_data_generator.MaskLMDataGenerator):
                          ):
           if batch:
             out = {
-              k: torch.from_numpy(v).unsqueeze(0).repeat_interleave(sample_batch_per_feed, dim = 0)
+              k: torch.from_numpy(v).unsqueeze(0)
               for (k, v) in batch[0].items()
             }
             for f in batch[1:]:
               for k, v in f.items():
-                nt = torch.from_numpy(v).unsqueeze(0).repeat_interleave(sample_batch_per_feed, dim = 0)
+                nt = torch.from_numpy(v).unsqueeze(0)
                 out[k] = torch.cat((out[k], nt), 0)
             for k in inputs.keys():
               inputs[k].append(out[k])
