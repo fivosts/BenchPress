09ed451a14a99f4a2ed94d6d4705757018159926
fivosts <fivos_ts@hotmail.com>
2021-11-10 20:20:22 +0000

    Add new plot for motivational example. Fix bar plot for results

--- deeplearning/clgen/models/evaluators.py
+++ deeplearning/clgen/models/evaluators.py
@@ -9,6 +9,7 @@ from deeplearning.clgen.features import extractor
 from deeplearning.clgen.preprocessors import opencl
 from deeplearning.clgen.corpuses import corpuses
 from deeplearning.clgen.util import monitors
+from deeplearning.clgen.util import plotter
 
 from eupy.native import logger as l
 
@@ -18,7 +19,7 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string(
   "clgen_samples_path",
-  "",
+  None,
   "Set path to clgen samples database for evaluation",
 )
 
@@ -90,9 +91,11 @@ class BenchmarkDistance(BaseEvaluator):
     self.bert_db = samples_db
 
     # clgen DB setup
-    self.clgen_samples_path = pathlib.Path(FLAGS.clgen_samples_path)
+    if not FLAGS.clgen_samples_path:
+      raise ValueError("clgen_samples_path has not been set for evaluation")
+    self.clgen_samples_path = pathlib.Path(FLAGS.clgen_samples_path).resolve()
     if not self.clgen_samples_path.exists():
-      raise FileNotFoundError
+      raise FileNotFoundError(str(self.clgen_samples_path))
     self.clgen_db = samples_database.SamplesDatabase("sqlite:///{}".format(str(self.clgen_samples_path)))
 
     self.github_corpus = github_corpus
@@ -120,6 +123,7 @@ class BenchmarkDistance(BaseEvaluator):
               features[self.feature_space],
             )
         )
+    l.getLogger().info("Loaded {} benchmarks".format(len(self.evaluated_benchmarks)))
     return
 
   def eval(self, topK: int) -> None:
@@ -146,27 +150,74 @@ class BenchmarkDistance(BaseEvaluator):
       for cf, feats in self.github_corpus.getFeaturesContents(sequence_length = self.sampler.sequence_length)
       if self.feature_space in feats and feats[self.feature_space]
     ]
-    print(len(clgen_corpus))
-    print(len(clgen_corpus))
-    print(len(git_corpus))
-    print(len(reduced_git_corpus))
+    self.avg_score_per_target(bert_corpus, clgen_corpus, git_corpus, reduced_git_corpus, topK)
+
+    outfile = pathlib.Path("./results.out").resolve()
+    with open(outfile, 'w') as outf:
+      for benchmark in self.evaluated_benchmarks:
+        try:
+          bc  = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in bert_corpus ], key = lambda x: x[1])[:topK]
+          cc  = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in clgen_corpus], key = lambda x: x[1])[:topK]
+          gc  = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in git_corpus  ], key = lambda x: x[1])[:topK]
+          rgc = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in reduced_git_corpus  ], key = lambda x: x[1])[:topK]
+        except KeyError:
+          print(git_corpus)
+
+        if rgc[0][1] > 0:
+          outf.write("###############################\n")
+          outf.write("{}\n".format(benchmark.name))
+          outf.write("Target features: {}\n".format(benchmark.features))
+
+          outf.write("BenchPress: {}\n".format([x[1] for x in bc]))
+
+          outf.write("CLgen: {}\n".format([x[1] for x in cc]))
+          outf.write("Github: {}\n".format([x[1] for x in gc]))
+          outf.write("Github <= 768 tokens: {}\n".format([x[1] for x in rgc]))
+
+          outf.write("\n\nTop-5 functions BenchPress's functions: \n")
+          for x in bc:
+            outf.write("{}\n\n".format(x[0]))
+          outf.write("###############################\n\n")
+
+    return
+
+  def avg_score_per_target(self, bert, clgen, git, reduced_git, topK):
+    groups = {}
+    names = {}
+    final_benchmarks = []
+
     for benchmark in self.evaluated_benchmarks:
-      try:
-        bc  = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in bert_corpus ], key = lambda x: x[1])[:topK]
-        cc  = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in clgen_corpus], key = lambda x: x[1])[:topK]
-        gc  = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in git_corpus  ], key = lambda x: x[1])[:topK]
-        rgc = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in reduced_git_corpus  ], key = lambda x: x[1])[:topK]
-      except KeyError:
-        print(git_corpus)
+      rgc = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in reduced_git], key = lambda x: x[1])[:topK]
+      if rgc[0][1] > 0:
+        final_benchmarks.append(benchmark)
 
-      print(benchmark.name)
-      print(benchmark.features)
+    for benchmark in final_benchmarks:
+      if benchmark.name not in names:
+        names[benchmark.name] = [0, 0]
+      else:
+        names[benchmark.name][1] += 1
 
-      print([x[1] for x in bc])
-      print([x[1] for x in cc])
-      print([x[1] for x in gc])
-      print([x[1] for x in rgc])
-      input()
+    l.getLogger().info("Plotting {} benchmarks".format(len(final_benchmarks)))
+
+    for dsetname, dset in [("BERT", bert), ("CLgen", clgen), ("GitHub", git), ("Reduced GitHub", reduced_git)]:
+      groups[dsetname] = ([], [])
+      for benchmark in final_benchmarks:
+        if names[benchmark.name][1] == 0:
+          bn = benchmark.name
+        else:
+          names[benchmark.name][0] += 1
+          bn = "{}-{}".format(benchmark.name, names[benchmark.name][0])
+        dst_list = sorted([(cf, feature_sampler.calculate_distance(fts, benchmark.features, self.feature_space)) for cf, fts in dset], key = lambda x: x[1])[:topK]
+        groups[dsetname][0].append(bn)
+        groups[dsetname][1].append(sum([x[1] for x in dst_list]) / len(dst_list))
+      for benchmark in final_benchmarks:
+        names[benchmark.name][0] = 0
+    plotter.GrouppedBars(
+      groups = groups,
+      title  = "Average distance of top-5 samples",
+      x_name = "Datasets",
+      plot_name = "avg_dist",
+    )
     return
 
 def motivational_example_fig():
@@ -198,8 +249,7 @@ def motivational_example_fig():
   if not bert_samples_path.exists():
     raise FileNotFoundError
   bert_db = samples_database.SamplesDatabase("sqlite:///{}".format(str(bert_samples_path)))
-  bert_datapoints = bert_db.get_by_ids([143826])
-
+  bert_datapoints = bert_db.get_by_ids([143826, 146576, 144315])
   data = [s for s in bert_db.correct_samples]
   # for x in range(20):
   #   s = data[np.random.randint(0, len(data))]
@@ -209,28 +259,50 @@ def motivational_example_fig():
   #   else:
   #     s.feature_vector = extractor.ExtractRawFeatures(s.text)
 
+  groups = {
+    "Rodinia Benchmarks": {'data': [], 'names': []},
+    "CLgen samples": {'data': [], 'names': []},
+    "BenchPress Examples": {'data': [], 'names': []},
+  }
+
   mon = monitors.TSNEMonitor(".", "motivational_example")
   for b in benchmarks:
     mon.register((b.features, "Rodinia Benchmarks", b.name))
+    groups["Rodinia Benchmarks"]['data'].append([b.features['comp'], b.features['mem']])
+    groups["Rodinia Benchmarks"]['names'].append(b.name)
   for i in range(30):
     sample = clgen_corpus[np.random.randint(0, len(clgen_corpus))]
     feats = extractor.RawToDictFeats(sample.feature_vector)
     if feature_space in feats and feats[feature_space]:
       mon.register((feats[feature_space], "clgen samples"))
+      groups["CLgen samples"]['data'].append([feats[feature_space]['comp'], feats[feature_space]['mem']])
+      groups["CLgen samples"]['names'].append("")
 
   for s in bert_datapoints:
     feats = extractor.RawToDictFeats(s.feature_vector)[feature_space]
-    for k in feats.keys():
-      feats[k] += 100
+    # for k in feats.keys():
+    #   feats[k] += 100
     mon.register((feats, "bert sample"))
+    groups["BenchPress Examples"]["data"].append([feats['comp'], feats['mem']])
   mon.plot()
+  plotter.GroupScatterPlot(
+    groups = groups,
+    title = "",
+    x_name = "# Computational Instructions",
+    y_name = "# Memory Instructions",
+    plot_name = "motivational_example_compvsmem",
+    marker_style = [
+      dict(color = 'darkslateblue', size = 7, symbol = "diamond-open", line = dict(width = 2)),
+      dict(color = 'goldenrod', size = 7, symbol = "circle"),
+      dict(color = 'firebrick', size = 11, symbol = "cross"),
+    ],
+    path = pathlib.Path(".").resolve(),
+  )
   return
 
 def initMain(*args, **kwargs):
   l.initLogger(name = "evaluators", lvl = 20, mail = (None, 5), colorize = True, step = False)
   motivational_example_fig()
-  raise NotImplementedError
-  evaluator = BenchmarkDistance()
   return
 
 if __name__ == "__main__":
