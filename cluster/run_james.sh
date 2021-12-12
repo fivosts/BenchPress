98fd6bd34e145b76551c9572cbd4b5f4d21d6c43
fivosts <fivos_ts@hotmail.com>
2021-12-12 20:13:52 +0000

    Add cluster dispatching tools

---
+++ cluster/run_james.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Let's stick to the following allocation:
+# james06 -> db0
+# james09 -> db1
+# james10 -> db2
+# james11 -> db3
+# james13 -> db4
+# james15 -> db5
+# james16 -> db6
+
+declare -A nodes
+nodes=( ["james06.inf.ed.ac.uk"]="0" ["james09.inf.ed.ac.uk"]="1" ["james10.inf.ed.ac.uk"]="2" ["james11.inf.ed.ac.uk"]="3" ["james13.inf.ed.ac.uk"]="4" ["james15.inf.ed.ac.uk"]="5" ["james16.inf.ed.ac.uk"]="6")
+
+if [ ! -f /disk/scratch/s1879742/bigQuery/clgen_c_github_"${nodes[$(hostname)]}".db ]; then
+	echo "Database "${nodes[$(hostname)]}" for $(hostname) not found!"
+	exit
+fi
+
+cp /disk/scratch/s1879742/bigQuery/clgen_c_github_"${nodes[$(hostname)]}".db /disk/scratch/s1879742/bigQuery/clgen_c_github.db
+
+cd
+cd clgen
+
+./clgen --stop_after corpus --workspace_dir /disk/scratch/s1879742/c_corpus_pretrain --config model_zoo/BERT/bert_base_pretrain.pbtxt > log-$(hostname).txt
