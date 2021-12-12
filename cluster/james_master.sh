807de03119f5ebe1c69a46f30a55f4c975a57580
fivosts <fivos_ts@hotmail.com>
2021-12-12 21:13:33 +0000

    Add clean and remove workspace option

--- cluster/james_master.sh
+++ cluster/james_master.sh
@@ -12,6 +12,18 @@
 declare -A nodes
 nodes=( ["james06.inf.ed.ac.uk"]="0" ["james09.inf.ed.ac.uk"]="1" ["james10.inf.ed.ac.uk"]="2" ["james11.inf.ed.ac.uk"]="3" ["james13.inf.ed.ac.uk"]="4" ["james15.inf.ed.ac.uk"]="5" ["james16.inf.ed.ac.uk"]="6")
 
-for key in ${!nodes[@]}; do
-  ssh s1879742@${key} "bash -s" < ./run_james.sh&
-done
+if [ $# -ge 1 ] && [ "$1" == "--kill" ]; then
+  for key in ${!nodes[@]}; do
+    ssh s1879742@${key} "kill $(pgrep clgen)"
+  done
+elif [ $# -ge 1 ] && [ "$1" == "--clean_workspace" ]; then
+  for key in ${!nodes[@]}; do
+    ssh s1879742@${key} "rm -rf /disk/scratch/s1879742/c_corpus_pretrain"
+  done
+elif [ $# -ge 1 ] && [ "$1" == "--dispatch" ]; then
+  for key in ${!nodes[@]}; do
+    ssh s1879742@${key} "bash -s" < ./run_james.sh&
+  done
+else
+  echo "No option selected."
+fi
