212331bd7c025a385b29c69e8f92a84e2fac25fd
fivosts <fivos_ts@hotmail.com>
2022-01-14 17:20:00 +0000

    Implement standard way of DDP initialization

---
+++ cluster/ddp_launcher.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+########################
+# To start a Distributed slurm node session type
+# - salloc --time=HH:MM:SS --gres=gpu:<X> -c <Y> --ntasks-per-node=<X> -N <Z> --nodelist=N1,N2,... bash -l
+# Where:
+# X: number of GPUs per node
+# Y: Number of CPUs per node
+# Z: Number of Nodes
+# --nodelist is optional to target specific nodes.
+#
+#
+# Then, within the allocated salloc session execute this script.
+
+# Collect the address of the master node.
+export MASTER_ADDR=$(srun --ntasks=1 hostname 2>&1 | tail -n1)
+
+# Execute clgen command. Modify the command script accordingly.
+srun ./ddp_run.sh
