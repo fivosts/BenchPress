be848848c78e7c4273c09e83f9c2810eaed53969
fivosts <fivos_ts@hotmail.com>
2022-01-18 17:27:52 +0000

    Implement more analytical experiments on cldrive

--- deeplearning/clgen/experiments/cldrive.py
+++ deeplearning/clgen/experiments/cldrive.py
@@ -10,7 +10,36 @@ import scipy.stats as st
 from deeplearning.clgen.preprocessors import opencl
 from deeplearning.clgen.util import plotter as plt
 
-def confidenceInterval():
+from eupy.native import logger as l
+
+src1 ="""
+kernel void A(global float *a, global float* n, global float* j, global float* h, const unsigned int r) {
+  int l = get_global_id(0);
+  if (l >= r)
+    return;
+  h[l] = n[l] - j[l]  * j[l-1];
+}
+
+"""
+src2 ="""
+kernel void A(global float *a, global float* n, global float* j, global float* h, const unsigned int r) {
+  int l = get_global_id(0);
+  for (unsigned int i = 0; i < 10; i++){
+    a[i] = n[i]*j[i]*h[i];
+    a[i+1] = n[i+1]*j[i+1]*h[i+1];
+  }
+}
+"""
+
+l.initLogger(
+  name = "experiments",
+  lvl = 10,
+  mail = (None, 10),
+  colorize = True,
+  step = False
+)
+
+def confidenceInterval() -> None:
   """
   For a simple and a more complicated OpenCL kernel,
   this experiment uses a fixed local size, iterates over a wide range of global sizes
@@ -19,24 +48,9 @@ def confidenceInterval():
   The 95% confidence interval is calculated for each run and the average distance of the mean
   from the confidence interval boundaries is expressed as a percentage distance.
   """
-  src1 ="""
-  kernel void A(global float *a, global float* n, global float* j, global float* h, const unsigned int r) {
-    int l = get_global_id(0);
-    if (l >= r)
-      return;
-    h[l] = n[l] - j[l]  * j[l-1];
-  }
 
-  """
-  src2 ="""
-  kernel void A(global float *a, global float* n, global float* j, global float* h, const unsigned int r) {
-    int l = get_global_id(0);
-    for (unsigned int i = 0; i < 10; i++){
-      a[i] = n[i]*j[i]*h[i];
-      a[i+1] = n[i+1]*j[i+1]*h[i+1];
-    }
-  }
-  """
+  global src1
+  global src2
 
   MIN_GISZE_POW = 8
   MAX_GSIZE_POW = 28
@@ -86,12 +100,193 @@ def confidenceInterval():
         print("95 interval: {}".format(interval))
         print("Ratio: {}%".format(100*(0.5 * (interval[1] - interval[0])) / gpk_mean))
         print()
-      x_axis = [10**x for x in range(1, MAX_RUNS_POW+1)]
+      x_axis = [x for x in range(1, MAX_RUNS_POW+1)]
       plt.MultiScatterLine(
-        x = [x_axis]*MAX_RUNS_POW,
+        x = [x_axis] * MAX_RUNS_POW,
         y = [data_cpt, data_cpk, data_gpt, data_gpk],
         names = ['cpu_transfer', 'cpu_kernel', 'gpu_transfer', 'gpu_kernel'],
+        x_name = "power of 10",
         plot_name = "{}_perc_diff_mean_int".format(2**gsize_pow),
-        path = pathlib.Path("./plots/{}".format(n)).resolve()
+        path = pathlib.Path("./plots/conf_interval/{}".format(n)).resolve()
       )
+  return
+
+def LabelGlobalSize() -> None:
+  """
+  Iterate over multiple global sizes and collect the optimal device
+  to execute an OpenCL kernel. GPU or CPU.
+  """
+  global src1
+  global src2
+
+  MIN_GISZE_POW = 8
+  MAX_GSIZE_POW = 28
+  N_RUNS = {
+    'src1': {
+      2**8 : 10**5,
+      2**9 : 10**5,
+      2**10: 10**5,
+      2**11: 10**5,
+      2**12: 10**5,
+      2**13: 10**5,
+      2**14: 10**4,
+      2**15: 10**4,
+      2**16: 10**4,
+      2**17: 10**4,
+      2**18: 10**4,
+      2**19: 10**3,
+      2**20: 10**3,
+      2**21: 10**3,
+      2**22: 10**3,
+      2**23: 10**2,
+      2**24: 10**2,
+      2**25: 10**1,
+      2**26: 10**1,
+      2**27: 10**1,
+      2**28: 10**1,
+    },
+    'src2': {
+      2**8 : 10**5,
+      2**9 : 10**5,
+      2**10: 10**5,
+      2**11: 10**5,
+      2**12: 10**5,
+      2**13: 10**4,
+      2**14: 10**4,
+      2**15: 10**4,
+      2**16: 10**4,
+      2**17: 10**3,
+      2**18: 10**3,
+      2**19: 10**3,
+      2**20: 10**3,
+      2**21: 10**2,
+      2**22: 10**2,
+      2**23: 10**2,
+      2**24: 10**1,
+      2**25: 10**1,
+      2**26: 10**1,
+      2**27: 10**1,
+      2**28: 10**1,
+    }
+  }
+  for n, src in [("src1", src1), ("src2", src2)]:
+    labels = {
+      'CPU': {'data': [], 'names': None},
+      'GPU': {'data': [], 'names': None},
+    }
+    for gsize_pow in range(MIN_GISZE_POW, MAX_GSIZE_POW+1):
+      print("##########", gsize_pow, 2**gsize_pow)
+      label = opencl.CLDriveLabel(src, num_runs = N_RUNS[n][2**gsize_pow], gsize = 2**gsize_pow, lsize = 256)
+      try:
+        if label == 'ERR':
+          while label == 'ERR':
+            label = opencl.CLDriveLabel(src, num_runs = N_RUNS[n][2**gsize_pow], gsize = 2**gsize_pow, lsize = 256)
+      except KeyboardInterrupt:
+        continue
+      labels[label]['data'].append([gsize_pow, 1])
+    plt.GroupScatterPlot(
+      groups = labels,
+      plot_name = "label_per_gsize",
+      path = pathlib.Path("./plots/label_gsize/{}".format(n)),
+      x_name = "power of 2",
+    )
+  return
+
+def ExecutionTimesGlobalSize() -> None:
+  """
+  Iterate over multiple global sizes and collect the optimal device
+  to execute an OpenCL kernel. GPU or CPU.
+  """
+  global src1
+  global src2
+
+  MIN_GISZE_POW = 8
+  MAX_GSIZE_POW = 28
+  N_RUNS = {
+    'src1': {
+      2**8 : 10**5,
+      2**9 : 10**5,
+      2**10: 10**5,
+      2**11: 10**5,
+      2**12: 10**5,
+      2**13: 10**5,
+      2**14: 10**4,
+      2**15: 10**4,
+      2**16: 10**4,
+      2**17: 10**4,
+      2**18: 10**4,
+      2**19: 10**3,
+      2**20: 10**3,
+      2**21: 10**3,
+      2**22: 10**3,
+      2**23: 10**2,
+      2**24: 10**2,
+      2**25: 10**1,
+      2**26: 10**1,
+      2**27: 10**1,
+      2**28: 10**1,
+    },
+    'src2': {
+      2**8 : 10**5,
+      2**9 : 10**5,
+      2**10: 10**5,
+      2**11: 10**5,
+      2**12: 10**5,
+      2**13: 10**4,
+      2**14: 10**4,
+      2**15: 10**4,
+      2**16: 10**4,
+      2**17: 10**3,
+      2**18: 10**3,
+      2**19: 10**3,
+      2**20: 10**3,
+      2**21: 10**2,
+      2**22: 10**2,
+      2**23: 10**2,
+      2**24: 10**1,
+      2**25: 10**1,
+      2**26: 10**1,
+      2**27: 10**1,
+      2**28: 10**1,
+    }
+  }
+  for n, src in [("src1", src1), ("src2", src2)]:
+    labels = {
+      'CPU': {'data': [], 'names': None},
+      'GPU': {'data': [], 'names': None},
+    }
+    groups = {
+      'cpu_transfer' : [[], []],
+      'cpu_kernel'   : [[], []],
+      'gpu_transfer' : [[], []],
+      'gpu_kernel'   : [[], []],
+    }
+    for gsize_pow in range(MIN_GISZE_POW, MAX_GSIZE_POW+1):
+      print("##########", gsize_pow, 2**gsize_pow)
+
+      cpt, cpk, gpt, gpk = opencl.CLDriveExecutionTimes(src, num_runs = N_RUNS[n][2**gsize_pow], gsize = 2**gsize_pow, lsize = 256)
+
+      if cpt is None:
+        while cpt is None:
+          cpt, cpk, gpt, gpk = opencl.CLDriveExecutionTimes(src, num_runs = N_RUNS[n][2**gsize_pow], gsize = 2**gsize_pow, lsize = 256)
+
+
+      groups['cpu_transfer'][0].append(gsize_pow)
+      groups['cpu_transfer'][1].append(cpt.mean())
+
+      groups['cpu_kernel'][0].append(gsize_pow)
+      groups['cpu_kernel'][1].append(cpk.mean())
+
+      groups['gpu_transfer'][0].append(gsize_pow)
+      groups['gpu_transfer'][1].append(gpt.mean())
+
+      groups['gpu_kernel'][0].append(gsize_pow)
+      groups['gpu_kernel'][1].append(cpk.mean())
+
+    plt.GrouppedBars(
+      groups = groups,
+      plot_name = "exec_times_per_gsize",
+      path = pathlib.Path("./plots/exec_times_gsize/{}".format(n)),
+      x_name = "power of 2",
+    )
   return
