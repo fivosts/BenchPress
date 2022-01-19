7e27447a89ffb19a3452cbf3cde1282d57012a4f
fivosts <fivos_ts@hotmail.com>
2022-01-19 18:36:52 +0000

    #179: Implement distributed progressbar

--- deeplearning/clgen/util/distrib.py
+++ deeplearning/clgen/util/distrib.py
@@ -3,6 +3,8 @@ import glob
 import os
 import time
 import pathlib
+import typing
+import progressbar
 
 from deeplearning.clgen.util import environment
 from eupy.native import logger as l
@@ -15,7 +17,7 @@ WORLD_SIZE  = environment.WORLD_SIZE
 
 PATH = None
 
-def barrier() -> None:
+def barrier(fn: typing.Callable = None) -> None:
   """
   Node processes are blocked until all nodes have reached this checkpoint.
   !!Warning!!: This function must not be called under a child process or thread.
@@ -29,7 +31,7 @@ def barrier() -> None:
     barriers = glob.glob(str(PATH / "barrier-*"))
 
     while len(barriers) < environment.WORLD_SIZE:
-      time.sleep(2)
+      time.sleep(1)
       barriers = glob.glob(str(PATH / "barrier-*"))
 
     while len(barriers) > 0:
@@ -51,3 +53,57 @@ def init(path: pathlib.Path) -> None:
   else:
     PATH = path
   return
+
+class ProgressBar(object):
+  """
+  Creates a distributed progressbar.
+  All nodes write their current index to a distinct file.
+  Only master node reads the indices and updates the progressbar.
+  """
+  def __init__(self, max_value: int, offset: int):
+    self.max_value = max_value
+    self.offset    = offset
+    self.path      = PATH
+    if self.path is None:
+      raise FileNotFoundError("Distributed env path has not been set!")
+    if WORLD_RANK == 0:
+      self.bar = progressbar.ProgressBar(max_value = max_value)
+    return
+
+  def _fetch_indices(idx: int) -> int:
+    """
+    Master node reads current workload indices of all nodes.
+    """
+    total = idx - self.offset
+    for n in range(1, WORLD_SIZE):
+      if (self.path / "index-{}".format(n)).exist():
+        with open(self.path / "index-{}".format(n), 'r') as inf:
+          total += int(inf.read())
+    return total
+
+  def _write_index(idx: int) -> None:
+    """
+    Update personal node dictionary with current index.
+    """
+    with open(self.path / "index-{}".format(WORLD_RANK), 'w') as outf:
+      outf.write(idx - self.offset)
+    return
+
+  def update(idx: int) -> None:
+    if WORLD_RANK == 0:
+      total_idx = self._fetch_indices(idx)
+      self.bar.update(total_idx)
+    else:
+      self._write_index(idx)
+    return
+
+  def finalize(idx: int) -> None:
+    """
+    Do a final bar update and cleanup progressbar object.
+    """
+    self.update(idx)
+    if WORLD_RANK == 0:
+      indices = glob.glob(str(PATH / "index-*"))
+      for ip in indices:
+        os.remove(str(ip))
+    return
