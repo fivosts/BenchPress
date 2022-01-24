0f5fafc7d26735aef1206ceae1f6cc59e1b63a45
fivosts <fivos_ts@hotmail.com>
2022-01-24 23:04:57 +0000

    bug fix

--- deeplearning/clgen/models/evaluators.py
+++ deeplearning/clgen/models/evaluators.py
@@ -684,7 +684,7 @@ def main(config: evaluator_pb2.Evaluation):
       raise NotImplementedError(ev)
 
     # Gather database groups and cache them.
-    if sev.HasField("db_group"):
+    if not ev.HasField("mutec_vs_benchpress") and sev.HasField("db_group"):
       for dbs in sev.db_group:
         key = dbs.group_name + ''.join(dbs.database)
         if key not in db_cache:
