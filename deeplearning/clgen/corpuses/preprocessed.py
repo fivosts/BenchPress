642e27820d7b4e2dd81c966daec0847a4cbd3cfa
fivosts <fivos_ts@hotmail.com>
2022-01-25 16:32:11 +0000

    #179: Extend DDP for preprocessed constructor

--- deeplearning/clgen/corpuses/preprocessed.py
+++ deeplearning/clgen/corpuses/preprocessed.py
@@ -232,9 +232,14 @@ class PreprocessedContentFiles(sqlutil.Database):
   """A database of pre-processed contentfiles."""
 
   def __init__(self, url: str, must_exist: bool = False):
-    super(PreprocessedContentFiles, self).__init__(
-      url, Base, must_exist=must_exist
-    )
+    if environment.WORLD_RANK == 0:
+      super(PreprocessedContentFiles, self).__init__(
+        url, Base, must_exist=must_exist
+      )
+    else:
+      self.url = url
+    distrib.barrier()
+    return
 
   def Create(self, config: corpus_pb2.Corpus):
     with self.Session() as session:
