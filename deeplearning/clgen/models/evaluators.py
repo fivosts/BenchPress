b4caf0aad068e013077a37f8aff645fa21993113
fivosts <fivos_ts@hotmail.com>
2022-01-25 18:43:50 +0000

    Reduce threshold

--- deeplearning/clgen/models/evaluators.py
+++ deeplearning/clgen/models/evaluators.py
@@ -582,7 +582,7 @@ def MutecVsBenchPress(**kwargs) -> None:
           if x not in visited]
         )
         visited.update(mutecs)
-        if depth < 3 and len(visited) < 1000:
+        if depth < 3 and len(visited) < 200:
           ret = set()
           for mutated in mutecs:
             ret.update(run_single(mutated, depth = depth + 1, visited = visited))
