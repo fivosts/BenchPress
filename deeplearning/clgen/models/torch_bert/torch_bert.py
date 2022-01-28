4b15c6cdd44fe454ee04453eb9b773e020b8ed13
fivosts <fivos_ts@hotmail.com>
2022-01-28 18:49:49 +0000

    Fixes

--- deeplearning/clgen/models/torch_bert/torch_bert.py
+++ deeplearning/clgen/models/torch_bert/torch_bert.py
@@ -510,14 +510,15 @@ class torchBert(backends.BackendBase):
             if self.is_world_process_zero():
               ## Collect tensors for logging.
               if self.pytorch.num_nodes > 1:
-                masked_lm_loss     = [self.torch.zeros(tuple(step_out['masked_lm_loss'].shape    ), dtype = self.torch.int64).to(self.pytorch.device) for _ in range(self.torch.distributed.get_world_size())]
+                total_loss         = [self.torch.zeros(tuple(step_out['total_loss'        ].shape), dtype = self.torch.int64).to(self.pytorch.device) for _ in range(self.torch.distributed.get_world_size())]
+                masked_lm_loss     = [self.torch.zeros(tuple(step_out['masked_lm_loss'    ].shape), dtype = self.torch.int64).to(self.pytorch.device) for _ in range(self.torch.distributed.get_world_size())]
                 next_sentence_loss = [self.torch.zeros(tuple(step_out['next_sentence_loss'].shape), dtype = self.torch.int64).to(self.pytorch.device) for _ in range(self.torch.distributed.get_world_size())]
-                masked_lm_lengths  = [self.torch.zeros(tuple(inputs['masked_lm_lengths'].shape ), dtype = self.torch.int64).to(self.pytorch.device) for _ in range(self.torch.distributed.get_world_size())]
+                masked_lm_lengths  = [self.torch.zeros(tuple(inputs  ['masked_lm_lengths' ].shape), dtype = self.torch.int64).to(self.pytorch.device) for _ in range(self.torch.distributed.get_world_size())]
 
                 self.torch.distributed.all_gather(masked_lm_loss,     step_out["masked_lm_loss"])
                 self.torch.distributed.all_gather(next_sentence_loss, step_out["next_sentence_loss"])
                 self.torch.distributed.all_gather(masked_lm_lengths,  inputs['masked_lm_lengths'].to(self.pytorch.device))
-                self.torch.distributed.reduce(total_loss, dst = 0)
+                self.torch.distributed.all_gather(total_loss,         step_out['total_loss'])
               else:
                 masked_lm_loss     = step_out['masked_lm_loss'    ]
                 next_sentence_loss = step_out['next_sentence_loss']
