f34c6ba0352ac63960e9483d82a6f1b1484333a8
fivosts <fivos_ts@hotmail.com>
2022-02-02 23:06:14 +0000

    Add average and min distance score in evaluator

---
+++ deeplearning/clgen/experiments/distance_score.py
@@ -0,0 +1,62 @@
+"""
+Top-K or min distance of database groups against target benchmark suites.
+"""
+import tqdm
+import math
+
+from deeplearning.clgen.corpuses import encoded
+from deeplearning.clgen.samplers import samples_database
+from deeplearning.clgen.util import plotter
+
+def KAverageScore(**kwargs) -> None:
+  """
+  Compare the average of top-K closest per target benchmark
+  for all different database groups.
+  """
+  db_groups      = kwargs.get('db_groups')
+  target         = kwargs.get('targets')
+  feature_space  = kwargs.get('feature_space')
+  top_k          = kwargs.get('top_k')
+  unique_code    = kwargs.get('unique_code', True)
+  plot_config    = kwargs.get('plot_config')
+  workspace_path = kwargs.get('workspace_path')
+  groups = {}
+
+  for dbg in db_groups:
+    if not (dbg.db_type == samples_database.SamplesDatabase or dbg.db_type == encoded.EncodedContentFiles):
+      raise ValueError("Scores require SamplesDatabase or EncodedContentFiles but received", dbg.db_type)
+    groups[dbg.group_name] = ([], [])
+    benchmarks = target.get_benchmarks(feature_space)
+    for benchmark in tqdm.tqdm(benchmarks, total = len(benchmarks), desc = "Benchmarks"):
+      groups[dbg.group_name][0].append(benchmark.name)
+      # Find shortest distances.
+      if unique_code:
+        get_data = lambda x: dbg.get_unique_data_features(x)
+      else:
+        get_data = lambda x: dbg.get_data_features(x)
+
+      distances = SortedDistances(get_data(feature_space), benchmark.features, feature_space)
+      # Compute target's distance from O(0,0)
+      assert len(distances) != 0, "Sorted src list for {} is empty!".format(dbg.group_name)
+      target_origin_dist = math.sqrt(sum([x**2 for x in benchmark.features.values()]))
+      avg_dist = sum(distances[:top_k]) / top_k
+
+      groups[dbg.group_name][1].append(100 * ((target_origin_dist - avg_dist) / target_origin_dist))
+
+  plotter.GrouppedBars(
+    groups = groups,
+    plot_name = "avg_{}_dist_{}".format(top_k, feature_space.replace("Features", " Features")),
+    path = workspace_path,
+    **plot_config if plot_config else {},
+  )
+  return
+
+def MinScore(**kwargs) -> None:
+  """
+  Compare the closest sample per target benchmark
+  for all different database groups.
+  """
+  if 'top_k' in kwargs:
+    del kwargs['top_k']
+  KAverageScore(top_k = 1, unique_code = False, **kwargs)
+  return
