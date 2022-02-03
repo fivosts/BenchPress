90aab8f286f2d8154f9001693d673e035e308b64
fivosts <fivos_ts@hotmail.com>
2022-02-03 15:45:38 +0000

    Add csmith to environment

--- deeplearning/clgen/util/environment.py
+++ deeplearning/clgen/util/environment.py
@@ -31,6 +31,7 @@ try:
   GREWE               = check_path_exists(os.environ['GREWE'])
   CLDRIVE             = check_path_exists(os.environ['CLDRIVE'], must_exist = False)
   MUTEC               = check_path_exists(os.environ['MUTEC'], must_exist = False)
+  CSMITH              = check_path_exists(os.environ['CSMITH'], must_exist = False)
   INSTCOUNT           = check_path_exists(os.environ['INSTCOUNT'])
   AUTOPHASE           = check_path_exists(os.environ['AUTOPHASE'])
   MASTER_PORT         = int(os.environ.get("MASTER_PORT", 8738))
