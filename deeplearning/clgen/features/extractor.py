ed4224486b9bfad328cf2dd80e46c23b2190ee20
fivosts <fivos_ts@hotmail.com>
2022-02-06 17:09:07 +0000

    Implemented extractor IR API

--- deeplearning/clgen/features/extractor.py
+++ deeplearning/clgen/features/extractor.py
@@ -30,6 +30,19 @@ def ExtractFeatures(src: str,
     ext = list(extractors.keys())
   return {xt: extractors[xt].ExtractFeatures(src, header_file = header_file, use_aux_headers = use_aux_headers, extra_args = extra_args) for xt in ext}
 
+def ExtractIRFeatures(bytecode: str,
+                      ext: typing.List[str] = None,
+                      ) -> typing.Dict[str, typing.Dict[str, float]]:
+  """
+  Wrapper method for core feature functions.
+  Returns a mapping between extractor type(string format) and feature data collected.
+
+  Works for LLVM-IR as an input.
+  """
+  if not ext:
+    ext = list(extractors.keys())
+  return {xt: extractors[xt].ExtractIRFeatures(bytecode) for xt in ext}
+
 def ExtractRawFeatures(src: str,
                        ext: typing.List[str] = None,
                        header_file     : str = None,
@@ -45,6 +58,21 @@ def ExtractRawFeatures(src: str,
   if ext and not isinstance(ext, list):
     raise TypeError("Requested feature space extractors must be a list, {} received".format(type(ext)))
   return '\n'.join(["{}:\n{}".format(xt, extractors[xt].ExtractRawFeatures(src, header_file = header_file, use_aux_headers = use_aux_headers, extra_args = extra_args)) for xt in ext])
+
+def ExtractIRRawFeatures(bytecode: str,
+                         ext: typing.List[str] = None,
+                         ) -> str:
+  """
+  Wrapper method for core feature functions.
+  Returns a mapping between extractor type(string format) and feature data collected.
+
+  Works for LLVM-IR as an input.
+  """
+  if not ext:
+    ext = list(extractors.keys())
+  if ext and not isinstance(ext, list):
+    raise TypeError("Requested feature space extractors must be a list, {} received".format(type(ext)))
+  return '\n'.join(["{}:\n{}".format(xt, extractors[xt].ExtractIRRawFeatures(bytecode)) for xt in ext])
 
 def RawToDictFeats(str_feats: str) -> typing.Dict[str, typing.Dict[str, float]]:
   """
