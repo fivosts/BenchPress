83c496c018b9ae40ed61341d1dae87cb7723842a
fivosts <fivos_ts@hotmail.com>
2022-02-11 17:36:13 +0000

    Fix

--- deeplearning/clgen/models/torch_bert/torch_bert.py
+++ deeplearning/clgen/models/torch_bert/torch_bert.py
@@ -838,9 +838,10 @@ class torchBert(backends.BackendBase):
           self.torch.distributed.all_gather(generated_samples, step_out["generated_samples"])
           self.torch.distributed.all_gather(sample_indices,    step_out["sample_indices"])
           raise NotImplementedError("This will not work because generated_samples and sample indices are lists and not tensors")
+          raise NotImplementedError("Also maybe unsqueeze to match dimension")
         else:
-          generated_samples = step_out['generated_samples']
-          sample_indices    = step_out['sample_indices']
+          generated_samples = [step_out['generated_samples']]
+          sample_indices    = [step_out['sample_indices']]
 
         if self.sampler.is_live and input("Show logits figure ? [y/!y]") == "y":
           if self.pytorch.num_nodes > 1:
