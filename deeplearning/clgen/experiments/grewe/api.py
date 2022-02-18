aae7f31f514f4011fa8b6a2cd1e6e498e4262027
fivosts <fivos_ts@hotmail.com>
2022-02-18 16:35:14 +0000

    Edit paper artifacts

--- deeplearning/clgen/experiments/grewe/api.py
+++ deeplearning/clgen/experiments/grewe/api.py
@@ -9,3 +9,10 @@ missing cldrive data.
 import pathlib
 
 from deeplearning.clgen.experiments import public
+
+"""
+1. You may insert database groups as usual to convert to csv
+2. You need to introduce a systematic way to insert the amd/nvidia/clgen csv's from clgen's artifacts.
+  a) Could be protobuf path arguments pointing to results workspace
+"""
+
