386511092457331f8e0b50d4efe8de7a4aed62f5
fivosts <fivos_ts@hotmail.com>
2022-02-21 15:04:36 +0000

    Refactor cache storing to dataframe

--- deeplearning/clgen/experiments/grewe/api.py
+++ deeplearning/clgen/experiments/grewe/api.py
@@ -56,67 +56,100 @@ def DataFrameSchema() -> typing.List[str]:
     "kernel_size"
   ]
 
-def ToDataFrameRow(name: str,
-                   grewe_feats: typing.Dict[str, float],
-                   df: pd.DataFrame,
+def ToDataFrameRow(name              : str,
+                   grewe_feats       : typing.Dict[str, float],
+                   transferred_bytes : int,
+                   global_size       : int,
+                   local_size        : int,
+                   label             : str,
+                   cpu_transfer_time : int,
+                   cpu_kernel_time   : int,
+                   gpu_transfer_time : int,
+                   gpu_kernel_time   : int,
                    ) -> pd.DataFrame:
   """
   Convert a samples DB to a csv with the same columns found in paper's artifact.
   """
-  cts, cks, gts, gks = df[df['device'].str.contains("CPU")].transfer_time_ns.mean(), df[df['device'].str.contains("CPU")].kernel_time_ns.mean(), df[df['device'].str.contains("GPU")].transfer_time_ns.mean(), df[df['device'].str.contains("GPU")].kernel_time_ns.mean()
   return [
     name,
-    df.global_size[0],
+    global_size,
     grewe_feats['comp'],
     grewe_feats['rational'],
     grewe_feats['mem'],
     grewe_feats['localmem'],
     grewe_feats['coalesced'],
     grewe_feats['atomic'],
-    df[df['device'].str.contains("CPU")].transferred_bytes[0],
-    df.local_size[0],
-    df[df['device'].str.contains("CPU")].transferred_bytes[0] / (grewe_feats['comp'] + grewe_feats['mem']),
+    transferred_bytes,
+    local_size,
+    transferred_bytes / (grewe_feats['comp'] + grewe_feats['mem']),
     grewe_feats["F2:coalesced/mem"],
-    (grewe_feats['localmem'] / grewe_feats['mem']) * df.local_size[0],
+    (grewe_feats['localmem'] / grewe_feats['mem']) * local_size,
     grewe_feats["F4:comp/mem"],
-    "GPU" if cts + cks > gts + gks else "CPU",
-    min(cts + cks, gts + gks),
-    max(cts + cks / gts + gks, gts + gks / cts + cks),
-    min(cts + cks / gts + gks, gts + gks / cts + cks),
-    cts + cks,
-    cts,
-    cks,
-    gts + gks,
-    gts,
-    gks,
+    label,
+    min(cpu_transfer_time + cpu_kernel_time, gpu_transfer_time + gpu_kernel_time),
+    max(cpu_transfer_time + cpu_kernel_time / gpu_transfer_time + gpu_kernel_time, gpu_transfer_time + gpu_kernel_time / cpu_transfer_time + cpu_kernel_time),
+    min(cpu_transfer_time + cpu_kernel_time / gpu_transfer_time + gpu_kernel_time, gpu_transfer_time + gpu_kernel_time / cpu_transfer_time + cpu_kernel_time),
+    cpu_transfer_time + cpu_kernel_time,
+    cpu_transfer_time,
+    cpu_kernel_time,
+    gpu_transfer_time + gpu_kernel_time,
+    gpu_transfer_time,
+    gpu_kernel_time,
     0,
     0
   ]
 
 def DriveSource(src: str,
                 feats: typing.Dict[str, float],
-                cldrive_cache: typing.Set[str, str]
+                cldrive_db: cldrive.CLDriveExecutions,
                 ) -> typing.Generator:
   """
   For a given source code, drive to CLDrive and return a ready row.
   Args:
-    src: source code to process
-    feats: Grewe Feature vector of source code.
-    cldrive_cache: CLDriveExecutions DB with stored exections to load.
+    src        : source code to process
+    feats      : Grewe Feature vector of source code.
+    cldrive_db : Caches cldrive executions of source code.
   """
   for gsize in tqdm.tqdm([2**6, 2**7, 2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**20], desc = "gsize", leave = False):
     for lsize in tqdm.tqdm([2**2, 2**3, 2**4, 2**5, 2**6, 2**7, 2**8], desc = "lsize", leave = False):
       if lsize > gsize:
         continue
 
-      data, label = opencl.CLDriveDataFrame(src, num_runs = 400, gsize = gsize, lsize = lsize, timeout = 60)
-      if label not in {"CPU", "GPU"}:
-        yield None
+      sha = crypto.sha256_str(src + str(gsize) + str(local_size))
+      if sha in cldrive_db.status_cache:
+        cached = cldrive_db.get_entry(src, gsize, lsize)
+        if cached.status in {"CPU", "GPU"}:
+          yield ToDataFrameRow(
+            name              = "{}.cl".format(sha),
+            grewe_feats       = feats,
+            transferred_bytes = cached.transferred_bytes,
+            global_size       = gsize,
+            local_size        = lsize,
+            label             = cached.status,
+            cpu_transfer_time = cached.cpu_transfer_time_ns,
+            cpu_kernel_time   = cached.cpu_kernel_time_ns,
+            gpu_transfer_time = cached.gpu_transfer_time_ns,
+            gpu_kernel_time   = cached.gpu_kernel_time_ns,
+          )
+        else:
+          yield None
       else:
-        yield ToDataFrameRow(
-            name = "{}.cl".format(idx),
-            grewe_feats = feats,
-            df = data,
+        df, label = opencl.CLDriveDataFrame(src, num_runs = 400, gsize = gsize, lsize = lsize, timeout = 60)
+        cldrive_db.add_entry(src, label, gsize, lsize, df)
+        if label not in {"CPU", "GPU"}:
+          yield None
+        else:
+          yield ToDataFrameRow(
+            name              = "{}.cl".format(sha),
+            grewe_feats       = feats,
+            transferred_bytes = df[df['device'].str.contains("CPU")].transferred_bytes[0],
+            global_size       = gsize,
+            local_size        = lsize,
+            label             = label,
+            cpu_transfer_time = df[df['device'].str.contains("CPU")].transfer_time_ns.mean(),
+            cpu_kernel_time   = df[df['device'].str.contains("CPU")].kernel_time_ns.mean(),
+            gpu_transfer_time = df[df['device'].str.contains("GPU")].transfer_time_ns.mean(),
+            gpu_kernel_time   = df[df['device'].str.contains("GPU")].kernel_time_ns.mean(),
           )
 
 @public.evaluator
@@ -133,7 +166,6 @@ def GreweTopKCSV(**kwargs) -> None:
   tokenizer     = kwargs.get('tokenizer')
 
   cldrive_db = CLDriveExecutions(url = "sqlite:///{}".format(pathlib.Path(cldrive_cache).resolve()), must_exist = False)
-  cache      = cldrive_db.status_cache
 
   for dbg in tqdm.tqdm(db_groups, desc = "DB Groups", leave = True):
     l.logger().info("Running {} on cldrive".format(dbg.group_name))
@@ -155,7 +187,7 @@ def GreweTopKCSV(**kwargs) -> None:
       top_k_bar = tqdm.tqdm(total = top_k, desc = "Top K cands", leave = True)
       for (src, _, feats, dist) in tqdm.tqdm(workers.SortedSrcFeatsDistances(get_data(), benchmark.features, "GreweFeatures"), desc = "Sorted Data", leave = False):
         toggle = False
-        for row in DriveSource(src, feats, cache):
+        for row in DriveSource(src, feats, cldrive_db):
           if row:
             toggle = True
             datapoints.append(row)
@@ -175,9 +207,12 @@ def GreweCSV(**kwargs) -> None:
   Convert database groups to CSV files that are supported by Grewe's predictive model.
   """
   db_groups   = kwargs.get('db_groups')
+  cldrive_cache = kwargs.get('cldrive_cache', '')
   unique_code = kwargs.get('unique_code', False)
   workspace   = kwargs.get('workspace_path')
   tokenizer   = kwargs.get('tokenizer')
+
+  cldrive_db = CLDriveExecutions(url = "sqlite:///{}".format(pathlib.Path(cldrive_cache).resolve()), must_exist = False)
 
   for dbg in tqdm.tqdm(db_groups, desc = "DB Groups", leave = True):
     if not (dbg.db_type == samples_database.SamplesDatabase or dbg.db_type == encoded.EncodedContentFiles):
@@ -192,7 +227,7 @@ def GreweCSV(**kwargs) -> None:
       get_data = lambda: dbg.get_data_features("GreweFeatures")
 
     for (src, feats) in tqdm.tqdm(get_data(), desc = "Src", leave = True):
-      for row in DriveSource(src, feats, cache):
+      for row in DriveSource(src, feats, cldrive_db):
         if row:
           datapoints.append(row)
     frame = pd.DataFrame(datapoints, columns = DataFrameSchema())
