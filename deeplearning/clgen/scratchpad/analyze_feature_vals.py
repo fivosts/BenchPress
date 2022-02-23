5aeea39d5957caddc8eaeba41f41e4a962577f00
fivosts <fivos_ts@hotmail.com>
2022-02-23 19:38:40 +0000

    Add feature analysis scratchpad script

--- deeplearning/clgen/scratchpad/analyze_feature_vals.py
+++ deeplearning/clgen/scratchpad/analyze_feature_vals.py
@@ -21,12 +21,13 @@ from deeplearning.clgen.experiments import workers
 ENCODED_DB_PATH = "/home/foivos/unique_encoded.db"
 TOKENIZER_PATH = "/home/foivos/backup_tokenizer.pkl"
 
-def get_data_features(self, feature_space: str, db, tokenizer) -> typing.List[typing.Tuple[str, typing.Dict[str, float]]]:
+def get_data_features(feature_space: str, db, tokenizer, size_limit = None) -> typing.List[typing.Tuple[str, typing.Dict[str, float]]]:
   """
   Get or set feature with data list of tuples.
   """
+  data_features = {}
   data_features[feature_space] = []
-  db_feats = db.get_data_features(tokenizer)
+  db_feats = db.get_data_features(tokenizer, size_limit)
   for inp in tqdm.tqdm(db_feats, total = len(db_feats), desc = "Fetch data"):
     feats = workers.ContentFeat(inp)
     if len(inp) == 2:
@@ -41,31 +42,43 @@ def get_data_features(self, feature_space: str, db, tokenizer) -> typing.List[ty
 def main(*args):
 
   db = encoded.EncodedContentFiles(url = "sqlite:///{}".format(ENCODED_DB_PATH), must_exist = True)
-  tokenizer = tokenizers.FromFile(pathlib.Path(TOKENIZER_PATH).resolve())
-
-  flat_ints = {
-    "GreweFeatures" : [],
-    "AutophaseFeatures" : [],
-    "InstcountFeatures" : [],
-  }
+  tokenizer = tokenizers.TokenizerBase.FromFile(pathlib.Path(TOKENIZER_PATH).resolve())
 
   distr = {
     "GreweFeatures": None,
     "AutophaseFeatures": None,
-    "InstcountFeatures": None,
+    "InstCountFeatures": None,
   }
 
-  for fspace in {"GreweFeatures", "AutophaseFeatures", "InstcountFeatures"}:
-    feat_vecs = [v for s, v in get_data_features(fspace, db, tokenizer)]
+  distr_768 = {
+    "GreweFeatures": None,
+    "AutophaseFeatures": None,
+    "InstCountFeatures": None,
+  }
+
+  for fspace in {"GreweFeatures", "AutophaseFeatures", "InstCountFeatures"}:
+    feat_vecs = [v for s, i, v in get_data_features(fspace, db, tokenizer)]
     flat_vals = []
     for vec in feat_vecs:
       for v in vec.values():
         try:
-          flat_vals.append(int(v))
+          flat_vals.append(4 * int(v // 4))
         except Exception:
           pass
     distr[fspace] = distributions.GenericDistribution(flat_vals, "feature_vals", fspace)
     distr[fspace].plot()
+
+  for fspace in {"GreweFeatures", "AutophaseFeatures", "InstCountFeatures"}:
+    feat_vecs = [v for s, i, v in get_data_features(fspace, db, tokenizer, 768)]
+    flat_vals = []
+    for vec in feat_vecs:
+      for v in vec.values():
+        try:
+          flat_vals.append(4 * int(v // 4))
+        except Exception:
+          pass
+    distr_768[fspace] = distributions.GenericDistribution(flat_vals, "feature_vals", "{}_768".format(fspace))
+    distr_768[fspace].plot()
   return
 
 if __name__ == "__main__":
