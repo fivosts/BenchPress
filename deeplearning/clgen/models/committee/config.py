0562d913176ac5492277ff495069c3f685974e2e
fivosts <fivos_ts@hotmail.com>
2022-03-01 21:01:47 +0000

    Implement Config parsing for committee

--- deeplearning/clgen/models/committee/config.py
+++ deeplearning/clgen/models/committee/config.py
@@ -1,14 +1,41 @@
 """ Configuration base class for committee models."""
 import typing
 
+from deeplearning.clgen.util import pbutil
 from deeplearning.clgen.models.committee import downstream_tasks
-from deeplearning.clgen.models.proto import active_learning_pb2
+from deeplearning.clgen.proto import active_learning_pb2
 
-def AssertConfigIsValid(config: active_learning_pb2:CommitteeConfig) -> active_learning_pb2:CommitteeConfig:
+def AssertConfigIsValid(config: active_learning_pb2:ActiveLearner) -> active_learning_pb2:ActiveLearner:
   """
   Parse proto description and check for validity.
   """
-
+  pbutil.AssertFieldConstraint(
+    config,
+    "downstream_task",
+    lambda x: x in downstream_tasks.TASKS,
+    "Downstream task has to be one of {}".format(', '.join([str(x) for x in downstream_tasks.TASKS]))
+  )
+  pbutil.AssertFieldIsSet(config, "committee")
+  tm = 0
+  for nn in config.committee.mlp:
+    tl = 0
+    for ln in nn.linear:
+      pbutil.AssertFieldIsSet(ln, "in_features")
+      pbutil.AssertFieldIsSet(ln, "out_features")
+      tl += 1
+    for ln in nn.dropout:
+      pbutil.AssertFieldIsSet(ln, "dropout_prob")
+      tl += 1
+    for ln in nn.layer_norm:
+      pbutil.AssertFieldIsSet(ln, "layer_norm_eps")
+      tl += 1
+    for ln in nn.act_fn:
+      pbutil.AssertFieldIsSet(ln, "act_fn")
+      tl += 1
+    assert tl > 0, "Model is empty. No layers found."
+    tm += 1
+  ## Extend above for loop for other model architectures.
+  assert tm > 0, "Committee is empty. No models found."
   return
 
 class CommitteeConfig(object):
@@ -16,10 +43,7 @@ class CommitteeConfig(object):
   model_type = "committee"
 
   @classmethod
-  def FromConfig(cls,
-                 config: active_learning_pb2.Committee,
-                 downstream_task: downstream_tasks.DownstreamTask
-                 ) -> "CommitteeConfig":
+  def FromConfig(cls, config: active_learning_pb2.ActiveLearner) -> "CommitteeConfig":
     config = CommitteeConfig(config, downstream_task)
     return config
 
@@ -37,14 +61,7 @@ class CommitteeConfig(object):
     """
     return self.input_size
 
-  def __init__(self,
-               config: active_learning_pb2.Committee,
-               downstream_task: downstream_tasks.DownstreamTask
-               ) -> "CommitteeConfig":
+  def __init__(self, config: active_learning_pb2.ActiveLearner) -> "CommitteeConfig":
     self.config = config
-    self.task   = downstream_task
-    """
-    The config must look like this:
-    
-    """
+    self.task   = config.downstream_task
     return
