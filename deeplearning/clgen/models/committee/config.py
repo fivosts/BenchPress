bf0fca59e48ce34699747361e744901fa2bc083f
fivosts <fivos_ts@hotmail.com>
2022-03-01 17:24:36 +0000

    Remove setter for num labels

--- deeplearning/clgen/models/committee/config.py
+++ deeplearning/clgen/models/committee/config.py
@@ -23,11 +23,6 @@ class CommitteeConfig(object):
     """
     return len(self.id2label)
 
-  @num_labels.setter
-  def num_labels(self, num_labels: int):
-    self.id2label = {i: "LABEL_{}".format(i) for i in range(num_labels)}
-    self.label2id = dict(zip(self.id2label.values(), self.id2label.keys()))
-
   def __init__(
     self,
     vocab_size,
