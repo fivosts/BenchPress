ad6d9f7883456bb199a2c1828f905bf62b2cabb9
fivosts <fivos_ts@hotmail.com>
2022-03-04 15:20:37 +0000

    Move data generator to models scope

---
+++ deeplearning/clgen/models/active_data_generator.py
@@ -0,0 +1,25 @@
+"""
+Data generators for active learning committee.
+"""
+import typing
+import pathlib
+
+from deeplearning.clgen.utils import pytorch
+from deeplearning.clgen.utils.pytorch import torch
+
+class Dataloader(torch.utils.data.Dataset):
+  """
+  Abstract torch dataloading class.
+  Inherit for this for multiple downstream tasks.
+  """
+  def __init__(self):
+    super(Dataloader, self).__init__()
+    return
+
+class GrewePredictiveLoader(Dataloader):
+  """
+  Specified dataloader for Grewe predictive model.
+  """
+  def __init__(self):
+    super(GrewePredictiveLoader, self).__init__()
+    return
