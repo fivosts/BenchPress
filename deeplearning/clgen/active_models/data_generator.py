8c13f88a5e6c1fc7be3ea269ec77a224a69cd684
fivosts <fivos_ts@hotmail.com>
2022-03-04 20:59:20 +0000

    Implement dataloader constructor

--- deeplearning/clgen/active_models/data_generator.py
+++ deeplearning/clgen/active_models/data_generator.py
@@ -9,9 +9,19 @@ from deeplearning.clgen.util.pytorch import torch
 
 class Dataloader(torch.utils.data.Dataset):
   """
-  Abstract torch dataloading class.
-  Inherit for this for multiple downstream tasks.
+  Modular dataloading class for downstream tasks.
   """
-  def __init__(self, dataset: typing.List[typing.Tuple[typing.List[int], typing.List[int]]]):
+  def __init__(self, dataset):
     super(Dataloader, self).__init__()
+    ## The dataset here should be a list, and each entry
+    ## must be a tuple containing the input and the target vector.
+    self.dataset = dataset
+    if len(self.dataset) <= 0:
+      raise ValuError("Predictive model dataset seems empty.")
+    self.compute_dataset()
     return
+
+  def compute_dataset(self) -> None:
+    """
+    Convert list dataset to torch tensors.
+    """
