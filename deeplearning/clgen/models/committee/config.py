82e2c1554d616c0ec174bb8457413c9cbfb5e1c3
fivosts <fivos_ts@hotmail.com>
2022-03-04 14:59:13 +0000

    Refactor assert config is valid

--- deeplearning/clgen/models/committee/config.py
+++ deeplearning/clgen/models/committee/config.py
@@ -12,22 +12,21 @@ def AssertConfigIsValid(config: active_learning_pb2.ActiveLearner) -> active_lea
   tm = 0
   for nn in config.committee.mlp:
     tl = 0
-    for ln in nn.linear:
-      pbutil.AssertFieldIsSet(ln, "in_features")
-      pbutil.AssertFieldIsSet(ln, "out_features")
-      tl += 1
-    for ln in nn.dropout:
-      pbutil.AssertFieldIsSet(ln, "dropout_prob")
-      tl += 1
-    for ln in nn.layer_norm:
-      pbutil.AssertFieldIsSet(ln, "layer_norm_eps")
-      tl += 1
-    for ln in nn.act_fn:
-      pbutil.AssertFieldIsSet(ln, "act_fn")
+    for l in nn.layer:
+      if l.HasField("linear"):
+        pbutil.AssertFieldIsSet(l.linear, "in_features")
+        pbutil.AssertFieldIsSet(l.linear, "out_features")
+      elif l.HasField("dropout"):
+        pbutil.AssertFieldIsSet(l.dropout, "dropout_prob")
+      elif l.HasField("layer_norm"):
+        pbutil.AssertFieldIsSet(l.layer_norm, "layer_norm_eps")
+      elif l.HasField("act_fn"):
+        pbutil.AssertFieldIsSet(l.act_fn, "act_fn")
+      else:
+        raise AttributeError(l)
       tl += 1
     assert tl > 0, "Model is empty. No layers found."
     tm += 1
-  ## Extend above for loop for other model architectures.
   assert tm > 0, "Committee is empty. No models found."
   return
 
