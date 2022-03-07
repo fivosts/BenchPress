182f462f124cd3d3660a9847fa1abdb09c5cc1b3
fivosts <fivos_ts@hotmail.com>
2022-03-07 14:16:13 +0000

    Fix samplecommittee arguments

--- deeplearning/clgen/active_models/committee/active_committee.py
+++ deeplearning/clgen/active_models/committee/active_committee.py
@@ -303,11 +303,13 @@ class QueryByCommittee(backends.BackendBase):
       prediction.append(self.downstream_task.TargetIDtoLabels(out['target_id']))
     return predictions
 
-  def SampleCommittee(self) -> typing.Dict[str, typing.List[str]]:
+  def SampleCommittee(self,
+                      sample_vectors: typing.Dict[str, 'torch.Tensor']
+                      ) -> typing.Dict[str, typing.List[str]]:
     """
     Sample committee with a set of inputs.
     """
-    self._ConfigModelParams(self.downstream_task.data_generator)
+    self._ConfigModelParams()
     committee_predictions = {}
     for member in self.committee:
       committee_predictions[member] = self.TrainMember(member)
@@ -320,8 +322,9 @@ class QueryByCommittee(backends.BackendBase):
     This method queries all committee members and measures their cross-entropy to validate
     the usefulness of parts of the feature space.
     """
-    sampled_vectors = self.downstream_task.sample_space(num_samples = 512)
-    committee_predictions = self.SampleCommittee(sampled_vectors)
+    sample_vectors = self.downstream_task.sample_space(num_samples = 512)
+    raise NotImplementedError("Batch sample vectors, to WxBxS, where WxB = 512")
+    committee_predictions = self.SampleCommittee(inputs)
     raise NotImplementedError("For each of inputs provided to SampleCommittee, calculate cross entropy")
     raise NotImplementedError("Return those feature vectors that have the highest entropy.")
     return
