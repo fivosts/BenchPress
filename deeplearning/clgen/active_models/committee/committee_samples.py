6b18b1bce0eb850c1fa90c13b21d8d6060a6c13a
fivosts <fivos_ts@hotmail.com>
2022-03-14 19:35:01 +0000

    Implement database for committee prediction tracking

---
+++ deeplearning/clgen/active_models/committee/committee_samples.py
@@ -0,0 +1,126 @@
+"""A module for databases of active learning query by committee samples."""
+import datetime
+import typing
+import progressbar
+import sqlite3
+
+import sqlalchemy as sql
+from sqlalchemy.ext import declarative
+
+from deeplearning.clgen.util import crypto
+from deeplearning.clgen.util import sqlutil
+from deeplearning.clgen.util import logging as l
+
+Base = declarative.declarative_base()
+
+class QBCResults(Base):
+  __tablename__ = "qbc_results"
+  """
+    DB Table for concentrated validation results.
+  """
+  key     : str = sql.Column(sql.String(1024), primary_key=True)
+  results : str = sql.Column(sqlutil.ColumnTypes.UnboundedUnicodeText(), nullable = False)
+
+class CommitteeConfig(Base, sqlutil.ProtoBackedMixin):
+  """
+  A table where each row presents the configuration of a committee member.
+  """
+  __tablename__ = "committee_members"
+  # entry id
+  id            : int = sql.Column(sql.Integer, primary_key = True)
+  # Assigned member ID
+  member_id     : int = sql.Column(sql.Integer, nullable = False, index = True)
+  # Name of member
+  member_name   : str = sql.Column(sqlutil.ColumnTypes.UnboundedUnicodeText(), nullable = False)
+  # Type of AI architecture (supervised, unsupervised etc.)
+  type          : str = sql.Column(sqlutil.ColumnTypes.UnboundedUnicodeText(), nullable = False)
+  # configuration specs
+  configuration : str = sql.Column(sqlutil.ColumnTypes.UnboundedUnicodeText(), nullable = False)
+  # Date
+  date_added    : datetime.datetime = sql.Column(sql.DateTime, nullable=False)
+
+  @classmethod
+  def FromArgs(cls, id: int, member_id: int, type: str, configuration: str) -> 'CommitteeConfig':
+    return CommitteeConfig(
+      id            = id,
+      member_id     = member_id,
+      type          = type,
+      configuration = configuration,
+      date_added    = datetime.datetime.utcnow(),
+    )
+
+class CommitteeSample(Base, sqlutil.ProtoBackedMixin):
+  """A database row representing a CLgen sample.
+
+  This is the clgen.Sample protocol buffer in SQL format.
+  """
+  __tablename__    = "qbc_samples"
+  # entry id
+  id                     : int = sql.Column(sql.Integer,    primary_key = True)
+  # unique hash of sample text
+  sha256                 : str = sql.Column(sql.String(64), nullable = False, index = True)
+  # model's train step that generated the sample
+  train_step             : int = sql.Column(sql.Integer,    nullable = False)
+  # Original input where the feed came from
+  static_features        : str = sql.Column(sqlutil.ColumnTypes.UnboundedUnicodeText(), nullable = False)
+  # Starting feed of model
+  runtime_features       : str = sql.Column(sqlutil.ColumnTypes.UnboundedUnicodeText(), nullable = False)
+  # String-format generated text
+  input_features         : str = sql.Column(sqlutil.ColumnTypes.UnboundedUnicodeText(), nullable = False)
+  # Predictions of committee
+  member_predictions     : str = sql.Column(sqlutil.ColumnTypes.UnboundedUnicodeText(), nullable = False)
+  # Amount of entropy
+  entropy                : float = sql.Column(sql.Float, nullable=False)
+  # Date
+  date_added             : datetime.datetime = sql.Column(sql.DateTime, nullable=False)
+
+  @classmethod
+  def FromArgs(cls,
+               id                 : int,
+               static_features    : typing.Dict[str, float],
+               runtime_features   : typing.Dict[str, float],
+               input_features     : typing.Dict[str, float],
+               member_predictions : typing.Dict[str, str],
+               entropy            : float
+               ) -> 'CommitteeSample':
+    str_static_features    = '\n'.join(["{}:{}".format(k, v) for k, v in static_features.items()])
+    str_runtime_features   = '\n'.join(["{}:{}".format(k, v) for k, v in runtime_features.items()])
+    str_input_features     = '\n'.join(["{}:{}".format(k, v) for k, v in input_features.items()])
+    str_member_predictions = '\n'.join(["{}:{}".format(k, v) for k, v in member_predictions.items()])
+    sha256 = crypto.sha256_str(
+      str(train_step)
+      + str_static_features
+      + str_runtime_features
+      + str_input_features
+      + str_member_predictions
+    )
+    return CommitteeSample(
+      id                 = id,
+      sha256             = sha256,
+      train_step         = train_step,
+      static_features    = str_static_features,
+      runtime_features   = str_runtime_features,
+      input_features     = str_input_features,
+      member_predictions = str_member_predictions,
+      entropy            = entropy,
+      date_added         = datetime.datetime.utcnow(),
+    )
+
+class CommitteeSamples(sqlutil.Database):
+  """A database of Query-by-Committee samples."""
+
+  def __init__(self, url: str, must_exist: bool = False):
+    super(CommitteeSamples, self).__init__(url, Base, must_exist = must_exist)
+
+  @property
+  def count(self):
+    """Number of samples in DB."""
+    with self.Session() as s:
+      count = s.query(CommitteeSample).count()
+    return count
+
+  @property
+  def get_data(self):
+    """Return all database in list format"""
+    with self.Session() as s:
+      return s.query(CommitteeSample).all()
