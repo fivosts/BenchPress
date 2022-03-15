8a80e657d4d2b900c224154108edb12c75478c9f
fivosts <fivos_ts@hotmail.com>
2022-03-15 19:19:57 +0000

    Implement random subset getter

--- deeplearning/clgen/active_models/data_generator.py
+++ deeplearning/clgen/active_models/data_generator.py
@@ -48,6 +48,16 @@ class ListTrainDataloader(torch.utils.data.Dataset):
       'target_ids' : np.asarray([x['target_ids'].numpy() for x in self.dataset]),
     }
 
+  def get_random_subset(self, num: int) -> 'ListTrainDataloader':
+    """
+    Get a sample of num random samples from dataset.
+    """
+    ret  = ListTrainDataloader([], lazy = True)
+    num  = min(num, len(self.dataset))
+    rand = set(torch.randperm(num, generator = None).tolist())
+    ret.dataset = [x for idx, x in enumerate(self.dataset) if idx in rand]
+    return ret
+
   def __len__(self) -> int:
     return len(self.dataset)
 
@@ -107,6 +117,16 @@ class DictPredictionDataloader(torch.utils.data.Dataset):
       'runtime_features' : np.asarray([x['runtime_features'].numpy() for x in self.dataset]),
       'input_ids'        : np.asarray([x['input_ids'].numpy() for x in self.dataset]),
     }
+
+  def get_random_subset(self, num: int) -> 'DictPredictionDataloader':
+    """
+    Get a sample of num random samples from dataset.
+    """
+    ret  = DictPredictionDataloader([], lazy = True)
+    num  = min(num, len(self.dataset))
+    rand = set(torch.randperm(num, generator = None).tolist())
+    ret.dataset = [x for idx, x in enumerate(self.dataset) if idx in rand]
+    return ret
 
   def __len__(self) -> int:
     return len(self.dataset)
