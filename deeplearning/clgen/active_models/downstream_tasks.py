5cce0de8081d472faad313160bebfeb18278a348
fivosts <fivos_ts@hotmail.com>
2022-03-15 18:11:04 +0000

    Add warning message

--- deeplearning/clgen/active_models/downstream_tasks.py
+++ deeplearning/clgen/active_models/downstream_tasks.py
@@ -225,7 +225,8 @@ class GrewePredictive(DownstreamTask):
         [self.TargetLabeltoID(entry.runtime_features['label'])]
       ) for entry in new_samples
     ]
-    l.logger().warn("What happens if all runtime features crash and you don't have any new samples?")
+    if len(updated_dataset) == 0:
+      l.logger().warn("Update dataset is empty.")
     return new_samples, data_generator.ListTrainDataloader(updated_dataset, lazy = True)
 
   def sample_space(self, num_samples: int = 512) -> data_generator.DictPredictionDataloader:
