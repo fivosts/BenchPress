bfeb1dad06f0b5477f6535e93a25b7bc7d6a6c72
fivosts <fivos_ts@hotmail.com>
2022-03-15 15:17:42 +0000

    Bug fix sql query

--- deeplearning/clgen/experiments/cldrive.py
+++ deeplearning/clgen/experiments/cldrive.py
@@ -228,9 +228,20 @@ class CLDriveExecutions(sqlutil.Database):
     """
     with self.Session() as session:
       if dataset:
-        return session.query(CLDriveSample).filter(CLDriveSample.dataset == dataset and (CLDriveSample.status == "CPU" or CLDriveSample.status == "GPU")).yield_per(1000)
+        return session.query(
+          CLDriveSample
+        ).filter(
+          sqlalchemy.or_(
+            CLDriveSample.dataset == dataset,
+            CLDriveSample.status.in_({"CPU", "GPU"})
+          )
+        ).yield_per(1000)
       else:
-        return session.query(CLDriveSample).filter(CLDriveSample.status == "CPU" or CLDriveSample.status == "GPU").yield_per(1000)
+        return session.query(
+          CLDriveSample
+        ).filter(
+          CLDriveSample.status.in_({"CPU", "GPU"})
+        ).yield_per(1000)
 
   def get_execution_times_ms(self, src: str, dataset: str, global_size: int, local_size: int) -> typing.Tuple[typing.List[int], typing.List[int], typing.List[int], typing.List[int]]:
     """
