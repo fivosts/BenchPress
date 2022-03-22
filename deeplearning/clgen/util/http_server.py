c8458bf4a5d8a54d9994718453643d59fca8347b
fivosts <fivos_ts@hotmail.com>
2022-03-22 14:33:22 +0000

    Remove old stuff

--- deeplearning/clgen/util/http_server.py
+++ deeplearning/clgen/util/http_server.py
@@ -140,167 +140,6 @@ def index():
   }
   return '\n'.join(["{}: {}".format(k, v) for k, v in status.items()]), 200
 
-def listen_read_queue(read_queue    : multiprocessing.Queue,
-                      port          : int,
-                      status        : multiprocessing.Value,
-                      listen_status : multiprocessing.Value,
-                      ) -> None:
-  """
-  Keep a socket connection open, listen to incoming traffic
-  and populate read_queue queue.
-  """
-  try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Bind to socket.
-    s.bind(('0.0.0.0', port))
-    # Set listen settings
-    s.listen(2**16)
-    # Block until connection is established.
-    try:
-      conn, addr = s.accept()
-      while status.value:
-        data = conn.recv(MAX_PAYLOAD_SIZE)
-        if len(data) > 0:
-          read_queue.put(data)
-        else:
-          break
-      conn.close()
-    except KeyboardInterrupt:
-      try:
-        conn.close()
-      except Exception:
-        pass
-      raise KeyboardInterrupt
-    except Exception as e:
-      try:
-        conn.close()
-      except Exception:
-        pass
-      raise e
-    s.close()
-  except KeyboardInterrupt:
-    s.close()
-  except Exception as e:
-    s.close()
-    raise e
-  listen_status.value = False
-  return
-
-def send_write_queue(write_queue : multiprocessing.Queue,
-                     host        : str,
-                     port        : int,
-                     status      : multiprocessing.Value,
-                     send_status : multiprocessing.Value,
-                     ) -> None:
-  """
-  Keep scanning for new unpublished data in write_queue.
-  Fetch them and send them over to the out socket connection.
-  """
-  try:
-    # Create a socket connection.
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    while status.value:
-      try:
-        s.connect((host, port))
-        break
-      except Exception:
-        time.sleep(1)
-
-    while status.value:
-      cur = write_queue.get()
-      try:
-        s.send(cur)
-      except BrokenPipeError:
-        break
-    s.close()
-  except KeyboardInterrupt:
-    s.close()
-  except Exception as e:
-    s.close()
-    raise e
-  send_status.value = False
-  return
-
-def socket_serve(read_queue    : multiprocessing.Queue,
-                 write_queue   : multiprocessing.Queue,
-                 status_bit    : multiprocessing.Value,
-                 listen_status : multiprocessing.Value,
-                 send_status   : multiprocessing.Value,
-                 ) -> None:
-  """
-  A standalone daemon process executes this function and serves.
-  It's purpose is to populate input queue and publish out queue.
-  """
-  target_host = FLAGS.target_host
-  listen_port = FLAGS.listen_port
-  send_port   = FLAGS.send_port
-
-  if listen_port is None:
-    status_bit.value    = False
-    listen_status.value = False
-    send_status.value   = False
-    raise ValueError("You have to define listen_port to use the socket server.")
-  if send_port is None:
-    status_bit.value    = False
-    listen_status.value = False
-    send_status.value   = False
-    raise ValueError("You have to define send_port to use the socket server.")
-  if target_host is None:
-    status_bit.value    = False
-    listen_status.value = False
-    send_status.value   = False
-    raise ValueError("You have to define the IP of the target server to use the socket server.")
-
-  try:
-    lp = multiprocessing.Process(
-      target = listen_read_queue, 
-      kwargs = {
-        'read_queue'    : read_queue,
-        'port'          : listen_port,
-        'status'        : status_bit,
-        'listen_status' : listen_status,
-      }
-    )
-    sp = multiprocessing.Process(
-      target = send_write_queue,  
-      kwargs = {
-        'write_queue' : write_queue,
-        'host'        : target_host,
-        'port'        : send_port,
-        'status'      : status_bit,
-        'send_status' : send_status,
-      }
-    )
-    lp.start()
-    sp.start()
-
-    while status_bit.value:
-      time.sleep(1)
-
-    lp.join(timeout = 20)
-    sp.join(timeout = 20)
-
-    lp.terminate()
-    sp.terminate()
-  except KeyboardInterrupt:
-    status_bit.value = False
-
-    lp.join(timeout = 20)
-    sp.join(timeout = 20)
-
-    lp.terminate()
-    sp.terminate()
-  except Exception as e:
-    status_bit.value = False
-
-    lp.join(timeout = 20)
-    sp.join(timeout = 20)
-
-    lp.terminate()
-    sp.terminate()
-    raise e
-  return
-
 def http_serve(read_queue: multiprocessing.Queue, write_queue: multiprocessing.Queue):
   """
   Run http server for read and write workload queues.
@@ -333,35 +172,16 @@ def start_server_process():
   elements needed to control the server.
   """
   rq, wq = multiprocessing.Queue(), multiprocessing.Queue()
-
-  if FLAGS.use_server == 'socket':
-    sb, rb, wb = multiprocessing.Value('i', True), multiprocessing.Value('i', True), multiprocessing.Value('i', True)
-    p = multiprocessing.Process(
-      target = socket_serve,
-      kwargs = {
-        'read_queue'    : rq,
-        'write_queue'   : wq,
-        'status_bit'    : sb,
-        'listen_status' : rb,
-        'send_status'   : wb,
-      }
-    )
-    # p.daemon = True
-    p.start()
-    return p, sb, (rq, rb), (wq, wb)
-  elif FLAGS.use_server == 'http':
-    p = multiprocessing.Process(
-      target = http_serve,
-      kwargs = {
-        'read_queue'  : rq,
-        'write_queue' : wq,
-      }
-    )
-    p.daemon = True
-    p.start()
-    return p, None, (rq, None), (wq, None)
-  else:
-    raise ValueError(FLAGS.use_server)
+  p = multiprocessing.Process(
+    target = http_serve,
+    kwargs = {
+      'read_queue'  : rq,
+      'write_queue' : wq,
+    }
+  )
+  p.daemon = True
+  p.start()
+  return p, rq, wq
 
 def start_thread_process():
   """
@@ -370,31 +190,13 @@ def start_thread_process():
   elements needed to control the server.
   """
   rq, wq = multiprocessing.Queue(), multiprocessing.Queue()
-
-  if FLAGS.use_server == 'socket':
-    sb, rb, wb = multiprocessing.Value('i', True), multiprocessing.Value('i', True), multiprocessing.Value('i', True)
-    th = threading.Thread(
-      target = socket_serve,
-      kwargs = {
-        'read_queue'    : rq,
-        'write_queue'   : wq,
-        'status_bit'    : sb,
-        'listen_status' : rb,
-        'send_status'   : wb,
-      },
-    )
-    th.start()
-    return None, sb, (rq, rb), (wq, wb)
-  elif FLAGS.use_server == 'http':
-    th = threading.Thread(
-      target = http_serve,
-      kwargs = {
-        'read_queue'  : rq,
-        'write_queue' : wq,
-      },
-      daemon = True
-    )
-    th.start()
-    return None, None, (rq, None), (wq, None)
-  else:
-    raise ValueError(FLAGS.use_server)
+  th = threading.Thread(
+    target = http_serve,
+    kwargs = {
+      'read_queue'  : rq,
+      'write_queue' : wq,
+    },
+    daemon = True
+  )
+  th.start()
+  return p, rq, wq
