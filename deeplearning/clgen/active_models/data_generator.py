775bf775f02a1b7420e837c189551f6c6d770b52
fivosts <fivos_ts@hotmail.com>
2022-03-25 18:43:33 +0000

    Fix

--- deeplearning/clgen/active_models/data_generator.py
+++ deeplearning/clgen/active_models/data_generator.py
@@ -114,7 +114,7 @@ class DictPredictionDataloader(torch.utils.data.Dataset):
     Batch the whole dataset by keys and return it.
     """
     return {
-      'idx'              : np.asarray([x['idx'].numpy() for x in self.dataset])
+      'idx'              : np.asarray([x['idx'].numpy() for x in self.dataset]),
       'static_features'  : np.asarray([x['static_features'].numpy() for x in self.dataset]),
       'runtime_features' : np.asarray([x['runtime_features'].numpy() for x in self.dataset]),
       'input_ids'        : np.asarray([x['input_ids'].numpy() for x in self.dataset]),
