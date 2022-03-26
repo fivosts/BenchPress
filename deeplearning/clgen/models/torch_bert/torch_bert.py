6d284cd5d82d8439425ea950b858380189fee031
fivosts <fivos_ts@hotmail.com>
2022-03-26 02:24:17 +0000

    Fix

--- deeplearning/clgen/models/torch_bert/torch_bert.py
+++ deeplearning/clgen/models/torch_bert/torch_bert.py
@@ -395,6 +395,11 @@ class torchBert(backends.BackendBase):
       outputs['input_ids']         = list(outputs['input_ids'].cpu().numpy())
       outputs['masked_lm_lengths'] = list(outputs['masked_lm_lengths'].cpu().numpy())
 
+      outputs['generated_samples'] = list(self.torch.reshape(outputs['generated_samples'], (-1, outputs['generated_samples'].size(-1))).cpu().numpy())
+      outputs['sample_indices']    = list(self.torch.reshape(outputs['sample_indices'],    (-1, outputs['sample_indices'   ].size(-1))).cpu().numpy())
+      outputs['input_ids']         = list(self.torch.reshape(outputs['input_ids'],         (-1, outputs['input_ids'        ].size(-1))).cpu().numpy())
+      outputs['masked_lm_lengths'] = list(self.torch.reshape(outputs['masked_lm_lengths'], (-1, outputs['masked_lm_lengths'].size(-1))).cpu().numpy())
+
     else:
       raise NotImplementedError ("Haven't implemented live sampling.")
       inputs = self.to_device(inputs)
