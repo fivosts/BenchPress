af3aa197f7324a5e41d14389225edf0e393dac9e
fivosts <fivos_ts@hotmail.com>
2022-03-31 23:23:30 +0100

    Edit

--- deeplearning/clgen/active_models/committee/committee_database.py
+++ deeplearning/clgen/active_models/committee/committee_database.py
@@ -173,7 +173,7 @@ class CommitteeSamples(sqlutil.Database):
     """Return the most recent checkpointed current sample step."""
     if self.sample_count > 0:
       with self.get_session() as s:
-        return max(s.query(CommitteeSample.sample_epoch).all())
+        return max([x.sample_epoch for x in s.query(CommitteeSample).all()])
     else:
       return 0
 
