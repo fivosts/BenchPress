0ad99ddf59b97d1e893edbcfb192db70791e79f7
fivosts <fivos_ts@hotmail.com>
2022-03-31 16:55:39 +0100

    Fix

--- deeplearning/clgen/features/feature_sampler.py
+++ deeplearning/clgen/features/feature_sampler.py
@@ -325,8 +325,11 @@ class ActiveSampler(FeatureSampler):
     Update active learner with targetted generated samples by the language model.
     """
     upd_samples, upd_loader = self.active_learner.downstream_task.UpdateDataGenerator(target_samples, top_k, self.tokenizer)
-    self.active_learner.UpdateLearn(upd_loader)
-    self.active_learner.downstream_task.UpdateTrainDataset(upd_loader)
+    if len(upd_loader) > 0:
+      self.active_learner.UpdateLearn(upd_loader)
+      self.active_learner.downstream_task.UpdateTrainDataset(upd_loader)
+    else:
+      l.logger().info("Skipping empty update dataset.")
     return
 
   def step_generation(self, candidates: typing.List['ActiveSample']) -> None:
