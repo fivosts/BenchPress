e4d5111269163d76bc7ecece0fc743a49343d5f6
fivosts <fivos_ts@hotmail.com>
2022-04-17 23:29:37 +0300

    Stupid fix

--- deeplearning/clgen/models/torch_bert/data_generator.py
+++ deeplearning/clgen/models/torch_bert/data_generator.py
@@ -167,7 +167,7 @@ def IR_candidate_worker(sample                  : np.array,
                         ) -> ActiveSample:
   # sample, indices, input_ids, masked_lm_lengths = sample_out
   sample, sample_indices, input_ids, mlm_lengths, feed = sample
-  assert sample[0] != self.tokenizer.padToken, sample
+  assert sample[0] != tokenizer.padToken, sample
   try:
     code = tokenizer.ArrayToCode(sample, with_formatting = False)
     features = extractor.ExtractFeatures(code, [feature_space])[feature_space]
@@ -205,7 +205,7 @@ def text_candidate_worker(sample                  : np.array,
                           tokenizer               : typing.TypeVar('corpuses.tokenizers.TokenizerBase'),
                           ) -> ActiveSample:
   sample, sample_indices, input_ids, mlm_lengths, feed = sample
-  assert sample[0] != self.tokenizer.padToken, sample
+  assert sample[0] != tokenizer.padToken, sample
   try:
     code = tokenizer.ArrayToCode(sample, with_formatting = False)
     _ = opencl.Compile(code)
