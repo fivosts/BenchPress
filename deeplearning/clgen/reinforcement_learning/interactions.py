a0cb1001bd4d692f7d2f834abf06c2a703f96a8f
Foivos Tsimpourlas <foivos@fb.com>
2022-06-04 14:28:15 -0700

    Isolate action state representations to module

---
+++ deeplearning/clgen/reinforcement_learning/interactions.py
@@ -0,0 +1,39 @@
+"""
+A module containing all possible interactions between
+the environment and an agent.
+"""
+import typing
+import numpy as np
+
+ACTION_TYPE_SPACE = {
+  'ADD' : 0,
+  'REM' : 1,
+  'COMP': 2, 
+}
+
+class Action(typing.NamedTuple):
+  """
+  Agent action representation.
+  """
+  action_type         : int      # Your policy function picks the best action type.
+  action_type_logits  : np.array # This must be a distribution vector over action space.
+  action_index        : int      # Your policy function picks the best index to apply the policy.
+  action_index_logits : np.array # Distribution vector over size of input code (or max length).
+  token_type          : int      # Your policy function picks the best token.
+  token_type_logits   : np.array # Distribution vector over possible tokens.
+
+class State(typing.NamedTuple):
+  """
+  Environment's state representation.
+  """
+  target_features : typing.Dict[str, float]
+  code            : np.array
+
+class Reward(typing.NamedTuple):
+  """
+  Reward provided to agent as feedback.
+  """
+  action   : Action
+  value    : float
+  distance : float
+  comment  : str
