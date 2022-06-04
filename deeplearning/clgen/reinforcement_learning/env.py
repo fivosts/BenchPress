d99348b8177bb7e4dc6096f34718d3650ad0cff4
Foivos Tsimpourlas <foivos@fb.com>
2022-06-04 14:45:45 -0700

    Resolve name dependencies

--- deeplearning/clgen/reinforcement_learning/env.py
+++ deeplearning/clgen/reinforcement_learning/env.py
@@ -1,23 +1,12 @@
 """
 RL Environment for the task of targeted benchmark generation.
 """
-import pathlib
 import numpy as np
 
 from deeplearning.clgen.preprocessors import opencl
-from deeplearning.clgen.reinforcement_learning import agent
+from deeplearning.clgen.reinforcement_learning import interactions
 
 from absl import flags
-
-class Reward(typing.NamedTuple):
-  action   : agent.Action
-  value    : float
-  distance : float
-  comment  : str
-
-class State(typing.NamedTuple):
-  target_features : typing.Dict[str, float]
-  code            : np.array
 
 class Environment(object):
   """
@@ -29,14 +18,14 @@ class Environment(object):
   
   def compute_reward(self, action) -> Reward:
     """Compute an action's reward."""
-    if action.action_type == agent.ACTION_TYPE_SPACE['ADD'] or action.action_type == agent.ACTION_TYPE_SPACE['REM']:
+    if action.action_type == interactions.ACTION_TYPE_SPACE['ADD'] or action.action_type == interactions.ACTION_TYPE_SPACE['REM']:
       return Reward(
         action   = action,
         value    = 0.0,
         distance = None,
         comment  = "[ADD] action, reward is +0."
       )
-    elif action.action_type == agent.ACTION_TYPE_SPACE['COMP']:
+    elif action.action_type == interactions.ACTION_TYPE_SPACE['COMP']:
       source = self.tokenizer.ArrayToCode(self.current_state.code)
       try:
         _ = opencl.Compile(source)
@@ -52,7 +41,7 @@ class Environment(object):
           comment  = "[COMPILE] action failed and reward is -1.",
         )
       else:
-        dist = feature_sample.euclidean_distance(feats, self.current_state.target_features)
+        dist = feature_sampler.euclidean_distance(feats, self.current_state.target_features)
         if dist == 0:
           return Reward(
             action   = action,
