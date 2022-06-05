ea27a7fe60ef1a7baab05b2bbde4a1d8e0cb7590
Foivos Tsimpourlas <foivos@fb.com>
2022-06-04 18:43:22 -0700

    Extend environment

--- deeplearning/clgen/reinforcement_learning/env.py
+++ deeplearning/clgen/reinforcement_learning/env.py
@@ -75,15 +75,29 @@ class Environment(object):
     """
     Get the current state of the environment.
     """
-    raise NotImplementedError
+    return self.current_state
 
 class Memory(object):
   """
   Replay buffer of previous states and actions.
   """
   def __init__(self):
+    self.action_buffer = []
+    self.state_buffer  = []
+    self.reward_buffer = []
+    self.loadCheckpoint()
     return
   
-  def add(self, action, state, reward):
-    raise NotImplementedError("TODO")
+  def add(self, action, state, reward) -> None:
+    """Add single step to memory buffers."""
+    self.action_buffer.append(action)
+    self.state_buffer.append(state)
+    self.reward_buffer.append(reward)
     return
+  
+  def loadCheckpoint(self) -> None:
+    """Fetch memory's latest state."""
+    return
+  
+  def saveCheckpoint(self) -> None:
+    """Save Checkpoint state."""
