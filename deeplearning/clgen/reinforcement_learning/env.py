dcabb13ae52f2a3ab791b0aa16887787eff63ca1
Foivos Tsimpourlas <foivos@fb.com>
2022-06-20 10:50:29 -0700

    Remove memory module from env

--- deeplearning/clgen/reinforcement_learning/env.py
+++ deeplearning/clgen/reinforcement_learning/env.py
@@ -78,29 +78,3 @@ class Environment(object):
     Get the current state of the environment.
     """
     return self.current_state
-
-class Memory(object):
-  """
-  Replay buffer of previous states and actions.
-  """
-  def __init__(self):
-    self.action_buffer = []
-    self.state_buffer  = []
-    self.reward_buffer = []
-    self.loadCheckpoint()
-    return
-
-  def add(self, action, state, reward) -> None:
-    """Add single step to memory buffers."""
-    self.action_buffer.append(action)
-    self.state_buffer.append(state)
-    self.reward_buffer.append(reward)
-    return
-  
-  def loadCheckpoint(self) -> None:
-    """Fetch memory's latest state."""
-    return
-  
-  def saveCheckpoint(self) -> None:
-    """Save Checkpoint state."""
-    return
