075ac5c8ead1df54a004a64f33abc43ae0f00847
Foivos Tsimpourlas <foivos@fb.com>
2022-06-20 16:05:24 -0700

    Rename model, add Train call to agent

--- deeplearning/clgen/reinforcement_learning/agent.py
+++ deeplearning/clgen/reinforcement_learning/agent.py
@@ -50,7 +50,7 @@ class Agent(object):
     if environment.WORLD_RANK == 0:
       self.cache_path.mkdir(exists_ok = True, parents = True)
 
-    self.q_values = model.QValuesModel()
+    self.q_model = model.QValuesModel()
     self.policy   = Policy()
 
     self.loadCheckpoint()
@@ -61,14 +61,14 @@ class Agent(object):
     Agent collects the current state by the environment
     and picks the right action.
     """
-    action_type_logits  = self.q_values.predict_action_type(state)
+    action_type_logits  = self.q_model.predict_action_type(state)
     action_type         = self.policy.select_action_type(action_type_logits)
 
     if action_type != interactions.ACTION_SPACE['COMP']:
-      action_index_logits = self.q_values.predict_action_index(state, action_type)
+      action_index_logits = self.q_model.predict_action_index(state, action_type)
       action_index        = self.policy.select_action_index(action_index_logits)
       if action_type == interactions.ACTION_SPACE['ADD']:
-        token_logits        = self.q_values.predict_token(state)
+        token_logits        = self.q_model.predict_token(state)
         token               = self.policy.select_token(token_logits)
       else:
         token_logits      = None
@@ -88,10 +88,11 @@ class Agent(object):
       token_type_logits   = token_logits,
     )
 
-  def update_agent(self, input_ids) -> None:
+  def update_agent(self, input_ids: typing.Dict[str, torch.Tensor]) -> None:
     """
     Train the agent on the new episodes.
     """
+    self.q_model.Train(input_ids)
     raise NotImplementedError
     return
 
