fe05c66a7d9ad45a65afb30d4de8b33286602281
Foivos Tsimpourlas <foivos@fb.com>
2022-06-21 16:22:53 -0700

    Fix details

--- deeplearning/clgen/reinforcement_learning/model.py
+++ deeplearning/clgen/reinforcement_learning/model.py
@@ -12,13 +12,12 @@ from deeplearning.clgen.util import pytorch
 
 torch = pytorch.torch
 
-class StateEncoderDecoder(torch.nn.Module):
+class FeatureEncoder(torch.nn.Module):
   """Transformer-Encoder architecture for encoding states."""
   def __init__(self, config):
     super().__init__()
     ## Dimensional variables.
     self.encoder_embedding_size = config.feature_vocab_size
-    self.decoder_embedding_size = config.vocab_size
     ## Feature vector encoder.
     self.encoder_embedding = torch.nn.Embedding(
       num_embeddings = config.feature_vocab_size,
@@ -42,6 +41,27 @@ class StateEncoderDecoder(torch.nn.Module):
       num_layers    = config.num_hidden_layers,
       norm          = encoder_norm,
     )
+    return
+
+  def forward(self,
+              input_features                  : torch.LongTensor,
+              input_features_mask             : torch.ByteTensor,
+              input_features_key_padding_mask : torch.ByteTensor,
+              ) -> torch.Tensor:
+    ## Run the encoder transformer over the features.
+    enc_embed = self.encoder_embedding(input_features) * math.sqrt(self.encoder_embedding_size)
+    pos_enc_embed = self.encoder_pos_encoder(enc_embed)
+    encoded = self.encoder_transformer(
+      pos_enc_embed,
+      mask = input_features_mask,
+      src_key_padding_mask = input_features_key_padding_mask,
+    )
+    return encoded
+
+class SourceDecoder(torch.nn.Module):
+  """Source code decoder for action prediction."""
+  def __init__(self, config):
+    super().__init__()
     ## Source code decoder.
     self.decoder_embedding = model.BertEmbeddings(config)
     decoder_layers = torch.nn.TransformerDecoderLayer(
@@ -63,64 +83,22 @@ class StateEncoderDecoder(torch.nn.Module):
     return
 
   def forward(self,
-              input_ids                       : torch.LongTensor,
-              input_features                  : torch.LongTensor,
-              input_features_mask             : torch.ByteTensor,
-              input_features_key_padding_mask : torch.ByteTensor,
-              input_ids_mask                  : torch.ByteTensor,
-              input_ids_key_padding_mask      : torch.ByteTensor,
+              input_ids                  : torch.LongTensor,
+              encoded_features           : torch.FloatTensor,
+              input_ids_mask             : torch.ByteTensor,
+              input_ids_key_padding_mask : torch.ByteTensor,
               ) -> torch.Tensor:
-    ## Run the encoder transformer over the features.
-    enc_embed = self.encoder_embedding(input_features) * math.sqrt(self.encoder_embedding_size)
-    pos_enc_embed = self.encoder_pos_encoder(enc_embed)
-    encoded = self.encoder_transformer(
-      pos_enc_embed,
-      mask = input_features_mask,
-      src_key_padding_mask = input_features_key_padding_mask,
-    )
     ## Run the decoder over the source code.
-    dec_embed = self.decoder_embedding(input_ids) * math.sqrt(self.decoder_embedding_size)
-    pos_dec_embed = self.decoder_pos_encoder(dec_embed)
+    dec_embed = self.decoder_embedding(input_ids)
     decoded = self.decoder_transformer(
-      pos_dec_embed,
-      memory = encoded,
+      dec_embed,
+      memory = encoded_features,
       tgt_mask = input_ids_mask,
       # memory_mask = None, ??
       tgt_key_padding_mask = input_ids_key_padding_mask,
       # memory_key_padding_mask = None, ??
     )
     return decoded
-
-class SourceDecoder(torch.nn.Module):
-  """Source code decoder for action prediction."""
-  def __init__(self, config):
-    super().__init__()
-    self.decoder_embedding = torch.nn.Embedding(
-      num_embeddings = config.vocab_size,
-      embedding_dim  = config.hidden_size,
-      padding_idx    = config.pad_token_id,
-    )
-    self.encoder_pos_encoder = model.FeaturePositionalEncoding(config)
-    decoder_layers    = torch.nn.TransformerDecoderLayer(
-      d_model         = config.hidden_size,
-      nhead           = config.num_attention_heads,
-      dim_feedforward = config.intermediate_size,
-      dropout         = config.dropout_prob,
-      batch_first     = True
-    )
-    decoder_norm = torch.nn.LayerNorm(
-      config.hidden_size,
-      eps = config.layer_norm_eps
-    )
-    self.decoder_transformer = torch.nn.TransformerDecoder(
-      decoder_layer = decoder_layers,
-      num_layers    = config.num_hidden_layers,
-      norm          = decoder_norm,
-    )
-    return
-
-  def forward(self, input_ids):
-    return
 
 class ActionHead(torch.nn.Module):
   """Classification head for action prediction."""
@@ -143,7 +121,7 @@ class IndexHead(torch.nn.Module):
 class ActionQV(torch.nn.Module):
   """Deep Q-Values for Action type prediction."""
   def __init__(self, config):
-    self.feature_encoder = StateEncoderDecoder(config)
+    self.feature_encoder = FeatureEncoder(config)
     self.source_decoder  = SourceDecoder(config)
     self.action_head     = ActionHead(config)
     self.index_head      = IndexHead(config)
