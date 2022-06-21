7aad858682c1573965fe32011875dcf22d9fff5a
Foivos Tsimpourlas <foivos@fb.com>
2022-06-21 11:15:05 -0700

    Implement transformer encoder decoder

--- deeplearning/clgen/reinforcement_learning/model.py
+++ deeplearning/clgen/reinforcement_learning/model.py
@@ -5,36 +5,105 @@ import pathlib
 import typing
 
 from deeplearning.clgen.reinforcement_learning import interactions
+from deeplearning.clgen.models.torch_bert import model
 from deeplearning.clgen.util import environment
 from deeplearning.clgen.util import distrib
 from deeplearning.clgen.util import pytorch
 
 torch = pytorch.torch
 
-class ActionTypeQV(torch.nn.Module):
+class FeatureEncoder(torch.nn.Module):
+  """Transformer-Encoder architecture for target features."""
+  def __init__(self, config):
+    super().__init__()
+    self.encoder_embedding = torch.nn.Embedding(
+      num_embeddings = config.feature_vocab_size,
+      embedding_dim  = config.hidden_size,
+      padding_idx    = config.feature_pad_idx
+    )
+    self.encoder_pos_encoder = model.FeaturePositionalEncoding(config)
+    encoder_layers = torch.nn.TransformerEncoderLayer(
+      d_model         = config.hidden_size,
+      nhead           = config.num_attention_heads,
+      dim_feedforward = config.intermediate_size,
+      dropout         = config.dropout_prob,
+      batch_first     = True
+    )
+    encoder_norm = torch.nn.LayerNorm(
+      config.hidden_size,
+      eps = config.feature_layer_norm_eps
+    )
+    self.encoder_transformer = torch.nn.TransformerEncoder(
+      encoder_layer = encoder_layers,
+      num_layers    = config.num_hidden_layers,
+      norm          = encoder_norm,
+    )
+    return
+  
+  def forward(self, input_ids):
+
+    return
+
+class SourceDecoder(torch.nn.Module):
+  """Source code decoder for action prediction."""
+  def __init__(self, config):
+    super().__init__()
+    self.decoder_embedding = torch.nn.Embedding(
+      num_embeddings = config.vocab_size,
+      embedding_dim  = config.hidden_size,
+      padding_idx    = config.pad_token_id,
+    )
+    self.encoder_pos_encoder = model.FeaturePositionalEncoding(config)
+    decoder_layers    = torch.nn.TransformerDecoderLayer(
+      d_model         = config.hidden_size,
+      nhead           = config.num_attention_heads,
+      dim_feedforward = config.intermediate_size,
+      dropout         = config.dropout_prob,
+      batch_first     = True
+    )
+    decoder_norm = torch.nn.LayerNorm(
+      config.hidden_size,
+      eps = config.layer_norm_eps
+    )
+    self.decoder_transformer = torch.nn.TransformerDecoder(
+      decoder_layer = decoder_layers,
+      num_layers    = config.num_hidden_layers,
+      norm          = decoder_norm,
+    )
+    return
+
+  def forward(self, input_ids):
+    return
+
+class ActionHead(torch.nn.Module):
+  """Classification head for action prediction."""
+  def __init__(self, config):
+    super().__init__()
+    return
+
+  def forward(self, input_ids):
+    return
+
+class IndexHead(torch.nn.Module):
+  """Classification head for token index prediction."""
+  def __init__(self, config):
+    super().__init__()
+    return
+
+  def forward(self, input_ids):
+    return
+
+class ActionQV(torch.nn.Module):
   """Deep Q-Values for Action type prediction."""
   def __init__(self, config):
-    super(ActionTypeQV, self).__init__()
-    self.config = config
-    self.encoder_embedding = torch.nn.Embedding(
-      
-    )
+    self.feature_encoder = FeatureEncoder(config)
+    self.source_decoder  = SourceDecoder(config)
+    self.action_head     = ActionHead(config)
+    self.index_head      = IndexHead(config)
     return
   
   def forward(self, input_ids: typing.Dict[str, torch.Tensor]) -> typing.Dict[str, torch.Tensor]:
     """Action type forward function."""
-    raise NotImplementedError
-    return
-
-class ActionIndexQV(torch.nn.Module):
-  """Deep Q-Values for Action index prediction."""
-  def __init__(self, config):
-    super(ActionIndexQV, self).__init__()
-    self.config = config
-    return
-  
-  def forward(self, input_ids: typing.Dict[str, torch.Tensor]) -> typing.Dict[str, torch.Tensor]:
-    """Action index forward function."""
     raise NotImplementedError
     return
 
@@ -60,8 +129,7 @@ class QValuesModel(object):
       self.cache_path.mkdir(exists_ok = True, parents = True)
 
     self.config = config
-    self.action_type_qv  = ActionTypeQV(config)
-    self.action_index_qv = ActionIndexQV(config)
+    self.action_type_qv  = ActionQV(config)
     self.token_type_qv   = TokenTypeQV(config)
     self.loadCheckpoint()
     return
