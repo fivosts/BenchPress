d27ee07805dea291c92ee0cb754f5d51c47d0ad0
Foivos Tsimpourlas <foivos@fb.com>
2022-06-24 11:53:37 -0700

    Fix QV config input dict

--- deeplearning/clgen/reinforcement_learning/config.py
+++ deeplearning/clgen/reinforcement_learning/config.py
@@ -13,14 +13,14 @@ class QValuesConfig(object):
       'vocab_size'              : tokenizer.vocab_size,
       'feature_vocab_size'      : feature_tokenizer.vocab_size,
       'feature_pad_idx'         : feature_tokenizer.padToken,
-      'pad_idx'                 : tokenizer.pad_idx,
-      'hidden_dropout_prob'     : config.deep_qv.hidden_dropout_prob,
-      'hidden_size'             : config.deep_qv.hidden_size,
-      'num_attention_heads'     : config.deep_qv.num_attention_heads,
-      'intermediate_size'       : config.deep_qv.intermediate_size,
-      'num_hidden_layers'       : config.deep_qv.num_hidden_layers,
-      'layer_norm_eps'          : config.deep_qv.layer_norm_eps,
-      'max_position_embeddings' : config.deep_qv.max_position_embeddings,
+      'pad_idx'                 : tokenizer.padToken,
+      'hidden_dropout_prob'     : config.agent.action_qv.hidden_dropout_prob,
+      'hidden_size'             : config.agent.action_qv.hidden_size,
+      'num_attention_heads'     : config.agent.action_qv.num_attention_heads,
+      'intermediate_size'       : config.agent.action_qv.intermediate_size,
+      'num_hidden_layers'       : config.agent.action_qv.num_hidden_layers,
+      'layer_norm_eps'          : config.agent.action_qv.layer_norm_eps,
+      'max_position_embeddings' : config.agent.action_qv.max_position_embeddings,
 
     }
     return QValuesConfig(**dict)
