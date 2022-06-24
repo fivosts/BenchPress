df25efb1dee50552fa4f12d078c53593226a8498
Foivos Tsimpourlas <foivos@fb.com>
2022-06-24 14:44:58 -0700

    Create setup dataset for Corpus dataloader

--- deeplearning/clgen/reinforcement_learning/data_generator.py
+++ deeplearning/clgen/reinforcement_learning/data_generator.py
@@ -29,15 +29,37 @@ class CorpusFeatureLoader(torch.utils.Dataset):
   """
   Dataloading from language model's training corpus.
   """
-  def __init__(self, corpus: corpuses.Corpus, feature_tokenizer: tokenizers.FeatureTokenizer):
+  def __init__(self,
+               config: reinforcement_learning_pb2.RLModel,
+               corpus: corpuses.Corpus,
+               feature_tokenizer: tokenizers.FeatureTokenizer
+               ):
+    self.config = config
     self.data = corpus.GetTrainingFeatures()
     self.feature_tokenizer = feature_tokenizer
+    self.setup_dataset()
     return
 
   def __len__(self) -> int:
     return len(self.dataset)
   
   def __getitem__(self, idx: int) -> typing.Dict[str, torch.Tensor]:
+    return
+  
+  def setup_dataset(self) -> typing.List[typing.Dict[str, torch.Tensor]]:
+    """Process raw feature vectors to processed dataset."""
+    self.dataset = []
+    for dp in self.data:
+      feats = extractor.RawToDictFeats(dp)
+      for k, v in feats.items():
+        fvec = self.feature_tokenizer.TokenizeFeatureVector(v, k, self.config.feature_sequence_length)
+        self.dataset.append(
+          {
+            'input_features': torch.LongTensor(fvec),
+            'input_features_mask': torch.LongTensor(fvec != self.feature_tokenizer.padToken),
+            'input_features_key_padding_mask': None,
+          }
+        )
     return
 
 class RandomFeatureLoader(torch.utils.data.Dataset):
