53d4ad59da0dfb5d4022896abae9eec749955648
Foivos Tsimpourlas <foivos@fb.com>
2022-06-24 14:22:04 -0700

    extend classmethod for feature loaders

--- deeplearning/clgen/reinforcement_learning/data_generator.py
+++ deeplearning/clgen/reinforcement_learning/data_generator.py
@@ -6,12 +6,25 @@ import typing
 import pickle
 
 from deeplearning.clgen.reinforcement_learning import interactions
+from deeplearning.clgen.corpuses import tokenizers
+from deeplearning.clgen.models import language_models
 from deeplearning.clgen.proto import reinforcement_learning_pb2
 from deeplearning.clgen.util import environment
 from deeplearning.clgen.util import distrib
 from deeplearning.clgen.util import pytorch
 
 torch = pytorch.torch
+
+def from_config(config: reinforcement_learning_pb2.RLModel,
+                feature_tokenizer: tokenizers.FeatureTokenizer,
+                language_model: language_models.Model
+                ) -> "FeatureLoader":
+  """
+  Return the right torch dataloader based on configuration.
+  """
+  if config.HasField("train_set"):
+    return CorpusFeatureLoader(language_model.corpus, feature_tokenizer)
+  return
 
 class FeatureLoader(torch.utils.data.Dataset):
   """
