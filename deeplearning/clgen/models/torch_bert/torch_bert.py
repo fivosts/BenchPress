693850424f72183851e30db34edeb643400f59d4
Foivos Tsimpourlas <foivos@fb.com>
2022-06-30 15:03:53 -0700

    Fix bug

--- deeplearning/clgen/models/torch_bert/torch_bert.py
+++ deeplearning/clgen/models/torch_bert/torch_bert.py
@@ -321,7 +321,7 @@ class torchBert(backends.BackendBase):
     attrs['pad_token_id']            = pad_token_id
     attrs['vocab_size']              = vocab_size
     generic_config = config.BertConfig.from_dict(
-      attrs
+      attrs,
       xla_device         = self.torch_tpu_available,
       reward_compilation = -1,
       # This is hard-coded to True to allow compile sampler to be initialized. This does not prohibit proper re-train.
