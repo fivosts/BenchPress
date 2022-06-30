d9df1adf2c70fe56a37fc360e563b5de9443d90f
Foivos Tsimpourlas <foivos@fb.com>
2022-06-30 15:00:23 -0700

    Implement State To Tensor methods

--- deeplearning/clgen/reinforcement_learning/data_generator.py
+++ deeplearning/clgen/reinforcement_learning/data_generator.py
@@ -25,6 +25,47 @@ def from_config(config            : reinforcement_learning_pb2.RLModel,
     return RandomFeatureLoader(config, feature_tokenizer)
   return
 
+def StateToActionTensor(state: interactions.State, padToken: int, feat_padToken: int) -> typing.Dict[str, torch.Tensor]:
+  """
+  Pre-process state to tensor inputs for Action Deep QValues.
+  """
+  ids               = torch.LongTensor(state.encoded_code)
+  feat_ids          = torch.LongTensor(state.encoded_features)
+  ids_pad_mask      = ids      != padToken
+  feat_ids_pad_mask = feat_ids != feat_padToken
+  return {
+    'input_ids'                        : ids,
+    'target_features'                  : feat_ids,
+    'input_ids_key_padding_mask'       : ids_pad_mask,
+    'target_features_key_padding_mask' : feat_ids_pad_mask,
+  }
+
+def StateToTokenTensor(state         : interactions.State,
+                       mask_idx      : int,
+                       maskToken     : int,
+                       padToken      : int,
+                       feat_padToken : int,
+                       ) -> typing.Dict[str, torch.Tensor]:
+  """
+  Pre-process state to 
+  """
+  seq_len = len(state.encoded_code)
+  feat_seq_len = len(state.encoded_features)
+
+  masked_code = state.encoded_code[:mask_idx+1] + [maskToken] + state.encoded_code[mask_idx+1:]
+  masked_code = torch.LongTensor(masked_code[:seq_len])
+  enc_features = torch.LongTensor(state.encoded_features)
+
+  return {
+    'encoder_input_ids'      : masked_code,
+    'encoder_input_mask'     : masked_code != padToken,
+    'encoder_position_ids'   : torch.arange(seq_len, dtype = torch.int64),
+    'decoder_feature_ids'    : enc_features,
+    'decoder_feature_mask'   : enc_features != feat_padToken,
+    'decoder_position_ids'   : torch.arange(feat_seq_len, dtype = torch.int64),
+    'encoder_input_features' : enc_features,
+  }
+
 class CorpusFeatureLoader(torch.utils.data.Dataset):
   """
   Dataloading from language model's training corpus.
