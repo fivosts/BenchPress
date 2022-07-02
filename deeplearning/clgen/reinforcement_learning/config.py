83289d58d127af636509763300d8117974bba2bc
Foivos Tsimpourlas <foivos@fb.com>
2022-07-02 16:16:14 -0700

    Collect max position embeddings

--- deeplearning/clgen/reinforcement_learning/config.py
+++ deeplearning/clgen/reinforcement_learning/config.py
@@ -5,15 +5,17 @@ from deeplearning.clgen.proto import reinforcement_learning_pb2
 class QValuesConfig(object):
   @classmethod
   def from_config(cls,
-                  config            : reinforcement_learning_pb2.RLModel,
-                  tokenizer         : tokenizers.TokenizerBase,
-                  feature_tokenizer : tokenizers.FeatureTokenizer,
+                  config                  : reinforcement_learning_pb2.RLModel,
+                  max_position_embeddings : int,
+                  tokenizer               : tokenizers.TokenizerBase,
+                  feature_tokenizer       : tokenizers.FeatureTokenizer,
                   ) -> 'QValuesConfig':
     dict = {
       'vocab_size'                          : tokenizer.vocab_size,
       'feature_vocab_size'                  : feature_tokenizer.vocab_size,
       'feature_pad_idx'                     : feature_tokenizer.padToken,
       'pad_token_id'                        : tokenizer.padToken,
+      'max_position_embeddings'             : max_position_embeddings,
       'feature_sequence_length'             : config.agent.feature_tokenizer.feature_sequence_length,
       'action_hidden_dropout_prob'          : config.agent.action_qv.hidden_dropout_prob,
       'action_feature_dropout_prob'         : config.agent.action_qv.hidden_dropout_prob,
