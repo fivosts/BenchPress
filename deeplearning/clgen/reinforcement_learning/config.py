ecfdf13edddef87c2beea0058f4e2af9ae1b4cf6
Foivos Tsimpourlas <foivos@fb.com>
2022-07-12 13:32:28 -0700

    Variable namefix

--- deeplearning/clgen/reinforcement_learning/config.py
+++ deeplearning/clgen/reinforcement_learning/config.py
@@ -42,7 +42,7 @@ class QValuesConfig(object):
       'token_hidden_act'                    : config.agent.action_lm.hidden_act,
       'token_attention_probs_dropout_prob'  : config.agent.action_lm.attention_probs_dropout_prob,
       'token_type_vocab_size'               : config.agent.action_lm.type_vocab_size,
-      'token_initializer_range'             : config.agent.action_lm.initializer_range_micros / 10e6,
+      'token_initializer_range'             : config.agent.action_lm.token_temperature_micros / 10e6,
       'token_temperature'                   : config.agent.action_lm.token_temperature,
       'feature_encoder'                     : False,
     }
