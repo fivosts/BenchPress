3e8f87bc3f5f5e107370143a3ff0c0e00bfa3e5c
Foivos Tsimpourlas <foivos@fb.com>
2022-07-12 13:45:58 -0700

    Add PPO methods to agent

--- deeplearning/clgen/reinforcement_learning/agent.py
+++ deeplearning/clgen/reinforcement_learning/agent.py
@@ -76,7 +76,7 @@ class Agent(object):
       self.feature_tokenizer,
     )
     self.q_model = model.QValuesModel(
-      self.language_model, self.feature_tokenizer, self.qv_config, self.cache_path
+      self.language_model, self.feature_tokenizer, self.qv_config, self.cache_path, is_critic = False,
     )
     self.critic_model = model.QValuesModel(
       self.language_model, self.feature_tokenizer, self.qv_config, self.cache_path, is_critic = True
@@ -132,7 +132,7 @@ class Agent(object):
         # the performance function, but Adam minimizes the loss. So minimizing the negative
         # performance function maximizes it.
         actor_loss = (-torch.min(surr1, surr2)).mean()
-        critic_loss = torch.nn.MSELoss()(V, batch_rtgs)
+        critic_loss = torch.nn.MSELoss()(Val, batch_rtgs)
 
         raise NotImplementedError
         # Calculate gradients and perform backward propagation for actor network
@@ -146,13 +146,80 @@ class Agent(object):
         self.critic_optim.step()
     return
 
-  def rollout(self) -> typing.Tuple:
+  def rollout(self, env: env.Environment) -> typing.Tuple:
     """
-    Play a number of episodes and collect batched data.
+    Too many transformers references, I'm sorry. This is where we collect the batch of data
+    from simulation. Since this is an on-policy algorithm, we'll need to collect a fresh batch
+    of data each time we iterate the actor/critic networks.
+
+    Parameters:
+      None
+
+    Return:
+      batch_obs - the observations collected this batch. Shape: (number of timesteps, dimension of observation)
+      batch_acts - the actions collected this batch. Shape: (number of timesteps, dimension of action)
+      batch_log_probs - the log probabilities of each action taken this batch. Shape: (number of timesteps)
+      batch_rtgs - the Rewards-To-Go of each timestep in this batch. Shape: (number of timesteps)
+      batch_lens - the lengths of each episode this batch. Shape: (number of episodes)
     """
-    batch_states, batch_actions, batch_logits, batch_rtgs, batch_lens = [], [], [], [], []
-    raise NotImplementedError
-    return batch_states, batch_actions, batch_logits, batch_rtgs, batch_lens
+    # Batch data. For more details, check function header.
+    batch_obs       = []
+    batch_acts      = []
+    batch_log_probs = []
+    batch_rews      = []
+    batch_rtgs      = []
+    batch_lens      = []
+
+    # Episodic data. Keeps track of rewards per episode, will get cleared
+    # upon each new episode
+    ep_rews = []
+
+    t = 0 # Keeps track of how many timesteps we've run so far this batch
+
+    # Keep simulating until we've run more than or equal to specified timesteps per batch
+    while t < self.timesteps_per_batch:
+      ep_rews = [] # rewards collected per episode
+
+      # Reset the environment. sNote that obs is short for observation. 
+      obs = self.env.reset()
+      done = False
+
+      # Run an episode for a maximum of max_timesteps_per_episode timesteps
+      for ep_t in range(self.max_timesteps_per_episode):
+        t += 1 # Increment timesteps ran this batch so far
+
+        # Track observations in this batch
+        batch_obs.append(obs)
+
+        # Calculate action and make a step in the env. 
+        # Note that rew is short for reward.
+        action, log_prob = self.make_action(obs)
+        obs, rew, done, _ = env.step(action)
+
+        # Track recent reward, action, and action log probability
+        ep_rews.append(rew)
+        batch_acts.append(action)
+        batch_log_probs.append(log_prob)
+
+        # If the environment tells us the episode is terminated, break
+        if done:
+          break
+
+      # Track episodic lengths and rewards
+      batch_lens.append(ep_t + 1)
+      batch_rews.append(ep_rews)
+
+    # Reshape data as tensors in the shape specified in function description, before returning
+    batch_obs = torch.tensor(batch_obs, dtype=torch.float)
+    batch_acts = torch.tensor(batch_acts, dtype=torch.float)
+    batch_log_probs = torch.tensor(batch_log_probs, dtype=torch.float)
+    batch_rtgs = self.compute_rtgs(batch_rews)                                                              # ALG STEP 4
+
+    # Log the episodic returns and episodic lengths in this batch.
+    self.logger['batch_rews'] = batch_rews
+    self.logger['batch_lens'] = batch_lens
+
+    return batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens
 
   def evaluate_policy(self, states, actions) -> typing.Tuple[torch.Tensor, torch.Tensor]:
     """
@@ -219,6 +286,35 @@ class Agent(object):
       token_type_logits   = token_logits,
       comment             = comment,
     )
+
+  def compute_rtgs(self, batch_rews):
+    """
+    Compute the Reward-To-Go of each timestep in a batch given the rewards.
+
+    Parameters:
+      batch_rews - the rewards in a batch, Shape: (number of episodes, number of timesteps per episode)
+
+    Return:
+      batch_rtgs - the rewards to go, Shape: (number of timesteps in batch)
+    """
+    # The rewards-to-go (rtg) per episode per batch to return.
+    # The shape will be (num timesteps per episode)
+    batch_rtgs = []
+
+    # Iterate through each episode
+    for ep_rews in reversed(batch_rews):
+
+      discounted_reward = 0 # The discounted reward so far
+
+      # Iterate through all rewards in the episode. We go backwards for smoother calculation of each
+      # discounted return (think about why it would be harder starting from the beginning)
+      for rew in reversed(ep_rews):
+        discounted_reward = rew + discounted_reward * self.gamma
+        batch_rtgs.insert(0, discounted_reward)
+
+    # Convert the rewards-to-go into a tensor
+    batch_rtgs = torch.tensor(batch_rtgs, dtype=torch.float)
+    return batch_rtgs
 
   def update_agent(self, input_ids: typing.Dict[str, torch.Tensor]) -> None:
     """
