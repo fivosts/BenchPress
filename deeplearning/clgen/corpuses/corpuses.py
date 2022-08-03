12b512059ef01fa42754f5e7dcfec7bc918ac9e6
Foivos Tsimpourlas <foivos@fb.com>
2022-08-02 22:08:23 -0700

    Add header

--- deeplearning/clgen/corpuses/corpuses.py
+++ deeplearning/clgen/corpuses/corpuses.py
@@ -1,17 +1,14 @@
-# Copyright (c) 2016-2020 Chris Cummins.
+# Copyright (c) Foivos Tsimpourlas.
 #
-# clgen is free software: you can redistribute it and/or modify
+# BenchPress is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# clgen is distributed in the hope that it will be useful,
+# BenchPress is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with clgen.  If not, see <https://www.gnu.org/licenses/>.
 """This file defines the logic and training corpuses.
 
 A training corpus is a set of one or more "contentfiles", where each contentfile
