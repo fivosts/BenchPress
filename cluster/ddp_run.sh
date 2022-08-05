cdd117135dd7aaa415457d8a277de1060ef7735e
Foivos Tsimpourlas <foivos@fb.com>
2022-08-04 21:50:47 -0700

    Update ddp_run.sh

--- cluster/ddp_run.sh
+++ cluster/ddp_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./clgen --local_filesystem /disk/scratch/s1879742/tmp --config model_zoo/BERT/bert_base_pretrain.pbtxt --workspace_dir /disk/scratch/s1879742/workspace
+./benchpress --local_filesystem /disk/scratch/s1879742/tmp --config model_zoo/BERT/bert_base_pretrain.pbtxt --workspace_dir /disk/scratch/s1879742/workspace
