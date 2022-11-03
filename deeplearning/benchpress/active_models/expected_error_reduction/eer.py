201b6dc1e029412b93cabe9949e04e6162ce2169
fivosts <fivos_ts@hotmail.com>
2022-11-03 16:25:20 +0000

    #194: Implement EER init

--- deeplearning/benchpress/active_models/expected_error_reduction/eer.py
+++ deeplearning/benchpress/active_models/expected_error_reduction/eer.py
@@ -17,4 +17,46 @@ A neural architecture for CPU vs GPU device mapping prediction.
 
 This head is used for feature-less learning to target benchmarks.
 """
+import typing
+import pathlib
 
+from deeplearning.benchpress.active_models import backends
+
+class ExpectedErrorReduction(backends.BackendBase):
+
+  def __repr__(self):
+    return "ExpectedErrorReduction"
+
+  def __init__(self, *args, **kwargs):
+
+    super(ExpectedErrorReduction, self).__init__(*args, **kwargs):
+    from deeplearning.benchpress.util import pytorch
+    if not pytorch.initialized:
+      pytorch.initPytorch()
+
+    self.pytorch             = pytorch
+    self.torch               = pytorch.torch
+    self.torch_tpu_available = pytorch.torch_tpu_available
+
+    self.torch.manual_seed(self.config.random_seed)
+    self.torch.cuda.manual_seed_all(self.config.random_seed)
+
+    self.ckpt_path    = self.cache_path / "checkpoints"
+    self.sample_path  = self.cache_path / "samples"
+    self.logfile_path = self.cache_path / "logs"
+
+    self.validation_results_file = "val_results.txt"
+    self.validation_results_path = self.logfile_path / self.validation_results_file
+
+    self.committee    = None
+
+    self.is_validated = False
+    self.is_trained   = False
+
+    self.committee_samples = committee_database.CommitteeSamples(
+      url        = "sqlite:///{}".format(str(self.sample_path / "samples.db")),
+      must_exist = False,
+    )
+    self.sample_epoch = self.committee_samples.cur_sample_epoch
+    l.logger().info("Active ExpectedErrorReduction config initialized in {}".format(self.cache_path))
+    return
