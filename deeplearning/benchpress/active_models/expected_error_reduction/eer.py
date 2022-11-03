2a63bb9be4f2ce4e981b77c7d5815a366f4b957d
fivosts <fivos_ts@hotmail.com>
2022-11-03 16:32:17 +0000

    #194: Implement ModelConfig

--- deeplearning/benchpress/active_models/expected_error_reduction/eer.py
+++ deeplearning/benchpress/active_models/expected_error_reduction/eer.py
@@ -48,7 +48,7 @@ class ExpectedErrorReduction(backends.BackendBase):
     self.validation_results_file = "val_results.txt"
     self.validation_results_path = self.logfile_path / self.validation_results_file
 
-    self.committee    = None
+    self.model = None
 
     self.is_validated = False
     self.is_trained   = False
@@ -59,4 +59,59 @@ class ExpectedErrorReduction(backends.BackendBase):
     )
     self.sample_epoch = self.committee_samples.cur_sample_epoch
     l.logger().info("Active ExpectedErrorReduction config initialized in {}".format(self.cache_path))
+    return
+
+  def _ConfigModelParams(self,
+                         data_generator : 'torch.utils.data.Dataset' = None,
+                         is_sampling    : bool = False
+                         ) -> None:
+    """
+    Model parameter initialization.
+    """
+    if not self.model:
+      self.model_config = config.ModelConfig.FromConfig(
+        self.config.expected_error_reduction, 
+        self.downstream_task,
+        self.config.num_train_steps
+      )
+      training_opts = QueryByCommittee.TrainingOpts(
+        train_batch_size = self.model_config.batch_size,
+        learning_rate    = self.model_config.learning_rate,
+        num_warmup_steps = self.model_config.num_warmup_steps,
+        max_grad_norm    = self.model_config.max_grad_norm,
+        steps_per_epoch  = self.model_config.steps_per_epoch,
+        num_epochs       = self.model_config.num_epochs,
+        num_train_steps  = self.model_config.num_train_steps,
+      )
+      cm = models.MLP.FromConfig(idx, self.model_config)
+      if not is_sampling:
+        opt, lr_scheduler = optimizer.create_optimizer_and_scheduler(
+          model           = cm,
+          num_train_steps = training_opts.num_train_steps,
+          warmup_steps    = training_opts.num_warmup_steps,
+          learning_rate   = training_opts.learning_rate,
+        )
+      else:
+        opt, lr_scheduler = None, None
+      self.estimator = ExpectedErrorReduction.Estimator(
+          model          = cm,
+          data_generator = copy.deepcopy(data_generator),
+          optimizer      = opt,
+          scheduler      = lr_scheduler,
+          training_opts  = training_opts,
+          sha256         = self.model_config.sha256,
+          config         = self.model_config,
+          train_fn       = self.TrainNNMember if isinstance(cm, self.torch.nn.Module) else self.TrainUnsupervisedMember,
+          sample_fn      = self.SampleNNMember if isinstance(cm, self.torch.nn.Module) else self.SampleUnsupervisedMember,
+        )
+      (self.ckpt_path / self.model_config.sha256).mkdir(exist_ok = True, parents = True),
+      (self.logfile_path / self.model_config.sha256).mkdir(exist_ok = True, parents = True),
+      l.logger().info(self.GetShortSummary())
+    # for member in self.committee:
+    #   self.committee_samples.add_member(
+    #     member_id     = member.model.id,
+    #     member_name   = member.config.name,
+    #     type          = "supervised" if isinstance(member.model, self.torch.nn.Module) else "unsupervised",
+    #     configuration = member.config.config,
+    #   )
     return
