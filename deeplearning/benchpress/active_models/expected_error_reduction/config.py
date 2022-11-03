d11fc567eae70d61273594f7f7b4a9d5c6a5b2a1
fivosts <fivos_ts@hotmail.com>
2022-11-03 14:47:18 +0000

    #194: Implement config for eer

--- deeplearning/benchpress/active_models/expected_error_reduction/config.py
+++ deeplearning/benchpress/active_models/expected_error_reduction/config.py
@@ -23,7 +23,7 @@ from deeplearning.benchpress.proto import active_learning_pb2
 from deeplearning.benchpress.util import pbutil
 from deeplearning.benchpress.util import crypto
 
-def AssertConfigIsValid(config: active_learning_pb2.ActiveLearner.ExpectedErrorReduction) -> None:
+def AssertConfigIsValid(config: active_learning_pb2.ExpectedErrorReduction) -> None:
   """
   Parse proto description and check for validity.
   """
@@ -51,3 +51,128 @@ def AssertConfigIsValid(config: active_learning_pb2.ActiveLearner.ExpectedErrorR
       tl += 1
     assert tl > 0, "Model is empty. No layers found."
   return
+
+class ModelConfig(object):
+
+  model_type = "expected_error_reduction"
+
+  @classmethod
+  def FromConfig(cls,
+                 config: active_learning_pb2.ExpectedErrorReduction,
+                 downstream_task: downstream_tasks.DownstreamTask,
+                 num_train_steps: int,
+                 ) -> typing.List["ModelConfig"]:
+    return NNModelConfig(config.mlp, downstream_task, num_train_steps)
+
+  @property
+  def num_labels(self) -> int:
+    """
+    The number of output labels for classification models.
+    """
+    return self.downstream_task.output_size
+
+  @property
+  def num_features(self) -> int:
+    """
+    The number of input features to model committee.
+    """
+    return self.downstream_task.input_size
+
+  def __init__(self,
+               name        : str,
+               config          : typing.Union[active_learning_pb2.MLP, active_learning_pb2.KMeans],
+               downstream_task : downstream_tasks.DownstreamTask
+               ) -> "ModelConfig":
+    self.name            = name
+    self.config          = config
+    self.downstream_task = downstream_task
+    self.sha256          = crypto.sha256_str(str(config))
+
+    ## Placeholding initialization
+    self.num_train_steps  = None
+    self.num_warmup_steps = None
+    self.num_epochs       = None
+    self.steps_per_epoch  = None
+    self.batch_size       = None
+    self.learning_rate    = None
+    self.max_grad_norm    = None
+    self.layer_config     = None
+    return
+
+class NNModelConfig(ModelConfig):
+  """
+  NeuralNetwork-based architectural config.
+  """
+  def __init__(self,
+               config          : active_learning_pb2.MLP,
+               downstream_task : downstream_tasks.DownstreamTask,
+               num_train_steps : int
+               ) -> "ModelConfig":
+    super(NNModelConfig, self).__init__("MLP", config, downstream_task)
+
+    ## NN-specific attributes
+    self.num_train_steps  = (num_train_steps + config.batch_size) // config.batch_size
+    self.num_warmup_steps = config.num_warmup_steps
+    self.num_epochs       = 1
+    self.steps_per_epoch  = self.num_train_steps
+    self.batch_size       = config.batch_size
+
+    self.learning_rate    = config.initial_learning_rate_micros / 1e6
+    self.max_grad_norm    = 1.0
+
+    if len(self.config.layer) == 0:
+      raise ValueError("Layer list is empty for committee model")
+    if self.config.layer[0].HasField("linear"):
+      if self.config.layer[0].linear.in_features != self.downstream_task.input_size:
+        raise ValueError("Mismatch between committee member's input size {} and downstream task's input size {}".format(
+            self.config.layer[0].linear.in_features,
+            self.downstream_task.input_size
+          )
+        )
+    if self.config.layer[-1].HasField("linear"):
+      if self.config.layer[-1].linear.out_features != self.downstream_task.output_size:
+        raise ValueError("Mismatch between committee member's output size {} and downstream task's output size {}".format(
+            self.config.layer[-1].linear.out_features,
+            self.downstream_task.output_size
+          )
+        )
+    self.layer_config = []
+    for l in self.config.layer:
+      if l.HasField("embedding"):
+        self.layer_config.append((
+            'Embedding', {
+              'num_embeddings': l.embedding.num_embeddings,
+              'embedding_dim' : l.embedding.embedding_dim,
+              'padding_idx'   : l.embedding.padding_idx if l.embedding.HasField("padding_idx") else None
+            }
+          )
+        )
+      elif l.HasField("linear"):
+        self.layer_config.append((
+            'Linear', {
+              'in_features': l.linear.in_features,
+              'out_features': l.linear.out_features,
+            }
+          )
+        )
+      elif l.HasField("dropout"):
+        self.layer_config.append((
+            'Dropout', {
+              'p': l.dropout.p,
+            }
+          )
+        )
+      elif l.HasField("layer_norm"):
+        self.layer_config.append((
+            'LayerNorm', {
+              'normalized_shape': l.layer_norm.normalized_shape,
+              'eps': l.layer_norm.eps,
+            }
+          )
+        )
+      elif l.HasField("act_fn"):
+        self.layer_config.append((
+            l.act_fn.fn, {}
+          )
+        )
+    return
