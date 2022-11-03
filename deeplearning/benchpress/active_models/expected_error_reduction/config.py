160c1e1d4a09787a13a720d9000e06779f8dac0b
fivosts <fivos_ts@hotmail.com>
2022-11-03 14:27:48 +0000

    #194: Change headline

--- deeplearning/benchpress/active_models/expected_error_reduction/config.py
+++ deeplearning/benchpress/active_models/expected_error_reduction/config.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-A neural architecture for CPU vs GPU device mapping prediction.
-
-This head is used for feature-less learning to target benchmarks.
+Config setup for expected error reduction active learner.
 """
 
