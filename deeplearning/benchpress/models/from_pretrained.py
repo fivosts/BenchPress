ddc7974da26c0fe66ca37ac7352c16fb98233eff
fivosts <fivos_ts@hotmail.com>
2022-11-21 17:24:40 +0000

    Add warning message

--- deeplearning/benchpress/models/from_pretrained.py
+++ deeplearning/benchpress/models/from_pretrained.py
@@ -152,6 +152,8 @@ class PreTrainedModel(object):
       FLAGS.sample_indices_limit = sample_indices_limit
 
     self.language_model.Create()
+    if pretrained.language_model.backend.pytorch.num_gpus == 0:
+      l.logger().warn("No GPUs detected. This process is going to be *very* slow on the CPU.")
     if "[START]" in prompt or "[END]" in prompt:
       l.logger().error("Do not add [START] and [END] manually. They will be added automatically by the tokenizer.")
       return ""
