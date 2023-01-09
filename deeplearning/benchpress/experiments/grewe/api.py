a754c1bdce34be4cb02c016b74df2cb233ed3a16
fivosts <fivos_ts@hotmail.com>
2023-01-09 22:51:43 +0000

    Edit ranges

--- deeplearning/benchpress/experiments/grewe/api.py
+++ deeplearning/benchpress/experiments/grewe/api.py
@@ -161,8 +161,8 @@ def DriveSource(src        : str,
       return lst[mid] / 2
 
   # for gsize in tqdm.tqdm([2**6, 2**7, 2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**20], desc = "gsize", leave = False):
-  for gsize in tqdm.tqdm([2**4, 2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**20, 2**24, 2**28], desc = "gsize", leave = False):
-    for lsize in tqdm.tqdm([2**2, 2**3, 2**4, 2**5, 2**6, 2**7, 2**8], desc = "lsize", leave = False):
+  for gsize in tqdm.tqdm([2**1, 2**2, 2**4, 2**8, 2**10, 2**12, 2**14, 2**16, 2**18, 2**20], desc = "gsize", leave = False):
+    for lsize in tqdm.tqdm([2**1, 2**2, 2**3, 2**4, 2**5, 2**6, 2**7, 2**8], desc = "lsize", leave = False):
       if lsize > gsize:
         continue
 
