7cf4342184a8f7347e1e57ea0be4d8a3fb93a2ac
fivosts <fivos_ts@hotmail.com>
2023-02-11 17:00:06 +0000

    Plot distributions, implement evaluator

--- deeplearning/benchpress/experiments/turing/analysis.py
+++ deeplearning/benchpress/experiments/turing/analysis.py
@@ -19,6 +19,7 @@ import pathlib
 
 from deeplearning.benchpress.experiments import public
 from deeplearning.benchpress.experiments.turing import db
+from deeplearning.benchpress.util import distributions
 from deeplearning.benchpress.util import plotter
 
 @public.evaluator
@@ -26,15 +27,80 @@ def HumanLikenessAnalysis(**kwargs) -> None:
   """
   Analyse Human or AI experiment.
   """
-  data = kwargs.get("human_likeness_db")
-  path = pathlib.Path(data).resolve()
+  workspace = kwargs.get("workspace_path")
+  str_path = kwargs.get("human_likeness_db")
+  path = pathlib.Path(str_path).resolve()
   if not path.exists():
     raise FileNotFoundError(path)
   
-  db = db.TuringDB(url = "sqlite:///{}".format(path), must_exist = True)
+  data = db.TuringDB(url = "sqlite:///{}".format(path), must_exist = True)
 
   """
-  Perform a set of analysis.
+  Evaluate the results from the database.
   """
+  """
+  1. Get distribution of scores per dataset (distribute score per user / per dataset.)
+    a. Get average score per dataset group.
+    b. Get confidence interval.
+  """
+  prediction_distr = data.get_prediction_distr()
+  labels = {
+    "engineer": {
+      "human": ([], []),
+      "AI"   : ([], [])
+    },
+    "non-engineer": {
+      "human": ([], []),
+      "AI"   : ([], [])
+    }
+  }
+  for label in labels.keys():
+    for dset, values in prediction_distr[label].items():
+      if values["predictions"]["human"] > 0:
+        labels[label]["human"][0] += dset
+        labels[label]["human"][1] += values["predictions"]["human"]
+      if values["predictions"]["robot"] > 0:
+        labels[label]["AI"][0] += dset
+        labels[label]["AI"][1] += values["predictions"]["robot"]
+    plotter.GrouppedBars(
+      labels[label],
+      plot_name = "Engineers_scores_per_set",
+      path = workspace,
+    )
+  plotter.GrouppedBars(
+    {
+      key: {
+        "human": labels["engineer"][key]["human"] + labels["non-engineer"][key]["human"],
+        "AI": labels["engineer"][key]["AI"] + labels["non-engineer"][key]["AI"], 
+      }
+      for key in labels["engineer"].keys()
+    },
+    plot_name = "Total_scores_per_set",
+    path = workspace,
+  )
+  unique_datasets = set(prediction_distr["engineer"].keys()).update(set(prediction_distr["non-engineer"].keys()))
+  ## Distributions
+  user_prediction_distr = data.get_user_prediction_distr()
+  distrs = {
+    "engineer": {},
+    "non-engineer": {},
+  }
+  for label in labels.keys():
+    for dset in unique_datasets:
+      distrs[label][dset] = distributions.GenericDistribution(
+        [
+          x[dset]["predictions"]["human"] / x[dset]["predictions"]["robot"]
+          for x in user_prediction_distr[label] if dset in x
+        ],
+        log_path = workspace,
+        set_name = "{}_{}_distrib".format(label, dset)
+      )
+      distrs[label][dset].plot()
 
+  """
+  2. Conditioned probabilities:
+    a. Score distribution on robots, given score on human.
+    b. Score distribution on human, given score on robots.
+    c. Score distribution on human and robots, given total ratio of human/robot selections.
+  """
   return
